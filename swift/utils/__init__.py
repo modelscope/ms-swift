b65aab92f650e61325ca7e592f25251ff1dd4282
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2023-08-25 09:59:00 +0800

    fix bug

--- swift/utils/__init__.py
+++ swift/utils/__init__.py
@@ -3,4 +3,4 @@
 from .logger import get_logger
 from .torch_utils import (add_version_to_work_dir, get_seed, is_master,
                           is_on_same_device, parse_args, print_model_info,
-                          seed_everything, show_freeze_layers)
+                          seed_everything)
