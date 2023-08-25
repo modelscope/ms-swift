2eeb510d5859f2c61ec53985b9ea8698214df689
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2023-08-26 01:50:50 +0800

    update utils

--- swift/utils/torch_utils.py
+++ swift/utils/torch_utils.py
@@ -9,6 +9,7 @@ from typing import List, Optional, Tuple, Type, TypeVar
 
 import numpy as np
 import torch
+import torch.distributed as dist
 from numpy.random import RandomState
 from torch.nn import Module
 from transformers import HfArgumentParser
@@ -113,3 +114,52 @@ def parse_args(class_type: Type[_T],
         argv, return_remaining_strings=True)
     logger.info(f'args: {args}')
     return args, remaining_args
+
+
+def get_dist_setting() -> Tuple[int, int, int, int]:
+    """return rank, local_rank, world_size"""
+    rank = int(os.getenv('RANK', -1))
+    local_rank = int(os.getenv('LOCAL_RANK', -1))
+    world_size = int(os.getenv('WORLD_SIZE', 1))
+    local_world_size = int(os.getenv('LOCAL_WORLD_SIZE', 1))
+    return rank, local_rank, world_size, local_world_size
+
+
+def is_dist():
+    """Determine if the training is distributed"""
+    rank, local_rank, _, _ = get_dist_setting()
+    return rank >= 0 and local_rank >= 0
+
+
+def show_layers(model: Module, max_lines: Optional[int] = 20) -> None:
+    named_p = list(model.named_parameters())
+    for i, (n, p) in enumerate(named_p):
+        if max_lines is not None and i >= max_lines:
+            logger.info('...')
+            break
+        logger.info(
+            f'[{n}]: requires_grad={p.requires_grad}, dtype={p.dtype}, device={p.device}'
+        )
+
+
+def broadcast_string(string: Optional[str], buffer_size: int = 100) -> str:
+    """String broadcasting in case of DDP
+    string: main rank: str
+        other rank: None
+    return: all rank: str
+    """
+    assert dist.is_initialized()
+    rank, local_rank, _, _ = get_dist_setting()
+    assert rank >= 0
+    if rank == 0:
+        assert string is not None
+        tensor = torch.tensor(
+            [ord(c) for c in string] + [0] * (buffer_size - len(string)),
+            dtype=torch.int64,
+            device=local_rank)
+    else:
+        tensor = torch.zeros(buffer_size, dtype=torch.int64, device=local_rank)
+    dist.broadcast(tensor, 0)
+    first_zero = (tensor == 0).nonzero()[0].item()
+    res = tensor.tolist()[:first_zero]
+    return ''.join([chr(x) for x in res])
