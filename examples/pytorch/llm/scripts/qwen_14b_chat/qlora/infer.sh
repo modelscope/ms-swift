4ace12170e65e1d65e839ead2c605d27810ec47d
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2023-10-04 11:06:37 +0800

    update sh

---
+++ examples/pytorch/llm/scripts/qwen_14b_chat/qlora/infer.sh
@@ -0,0 +1,21 @@
+# If you want to merge LoRA weight and save it, you need to set `--merge_lora_and_save true`.
+PYTHONPATH=../../.. \
+CUDA_VISIBLE_DEVICES=0 \
+python src/llm_infer.py \
+    --model_type qwen-14b-chat \
+    --sft_type lora \
+    --template_type chatml \
+    --dtype bf16 \
+    --ckpt_dir "output/qwen-14b-chat/vx_xxx/checkpoint-xxx" \
+    --eval_human false \
+    --dataset dureader-robust-zh \
+    --max_length 2048 \
+    --quantization_bit 4 \
+    --bnb_4bit_comp_dtype bf16 \
+    --use_flash_attn false \
+    --max_new_tokens 1024 \
+    --temperature 0.9 \
+    --top_k 20 \
+    --top_p 0.9 \
+    --do_sample true \
+    --merge_lora_and_save false \
