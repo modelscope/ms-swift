748ac23c01b49ebaabfdfe8dbf39c193898ba1b0
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2023-10-18 11:35:02 +0800

    update

--- examples/pytorch/llm/src/llm_infer.py
+++ examples/pytorch/llm/src/llm_infer.py
@@ -18,6 +18,7 @@ logger = get_logger()
 def merge_lora(args: InferArguments) -> None:
     assert args.sft_type == 'lora'
     assert not args.model_type.endswith('int4'), 'int4 model is not supported'
+    assert not args.model_type.endswith('int8'), 'int8 model is not supported'
     # ### Loading Model and Tokenizer
     model, tokenizer = get_model_tokenizer(
         args.model_type, torch_dtype=args.torch_dtype, device_map='cpu')
