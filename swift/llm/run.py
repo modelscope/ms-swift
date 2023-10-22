eb167a79f1fbdccb27624b86a6a5c56622ce5eef
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2023-10-22 15:16:08 +0800

    update run

--- swift/llm/run.py
+++ swift/llm/run.py
@@ -1,28 +1,31 @@
-from typing import List, Optional
+from typing import Callable, List, Optional, Type, TypeVar
 
 from swift.llm import InferArguments, SftArguments, llm_infer, llm_sft
 from swift.utils import get_logger, parse_args
 
 logger = get_logger()
 
-
-def sft_main(argv: Optional[List[str]] = None) -> str:
-    args, remaining_argv = parse_args(SftArguments, argv)
-    args.init_argument()
-    if len(remaining_argv) > 0:
-        if args.ignore_args_error:
-            logger.warning(f'remaining_argv: {remaining_argv}')
-        else:
-            raise ValueError(f'remaining_argv: {remaining_argv}')
-    return llm_sft(args)
+_TArgsClass = TypeVar('_TArgsClass')
+_T = TypeVar('_T')
 
 
-def infer_main(argv: Optional[List[str]] = None) -> None:
-    args, remaining_argv = parse_args(InferArguments, argv)
-    args.init_argument()
-    if len(remaining_argv) > 0:
-        if args.ignore_args_error:
-            logger.warning(f'remaining_argv: {remaining_argv}')
-        else:
-            raise ValueError(f'remaining_argv: {remaining_argv}')
-    llm_infer(args)
+def get_main(
+        args_class: Type[_TArgsClass],
+        llm_x: Callable[[_TArgsClass],
+                        _T]) -> Callable[[Optional[List[str]]], _T]:
+
+    def x_main(argv: Optional[List[str]] = None) -> _T:
+        args, remaining_argv = parse_args(args_class, argv)
+        args.init_argument()
+        if len(remaining_argv) > 0:
+            if args.ignore_args_error:
+                logger.warning(f'remaining_argv: {remaining_argv}')
+            else:
+                raise ValueError(f'remaining_argv: {remaining_argv}')
+        return llm_x(args)
+
+    return x_main
+
+
+sft_main = get_main(SftArguments, llm_sft)
+infer_main = get_main(InferArguments, llm_infer)
