24a71e429c4233e1c7f9c81702da0e5b6c9ca265
tastelikefeet <58414341+tastelikefeet@users.noreply.github.com>
2023-10-27 11:20:05 +0800

    Support ROME (#121)

--- swift/tuners/mapping.py
+++ swift/tuners/mapping.py
@@ -5,6 +5,7 @@ from .longlora.longlora import LongLoRA, LongLoRAConfig
 from .lora import LoRA, LoRAConfig
 from .prompt import Prompt, PromptConfig
 from .restuning import ResTuning, ResTuningConfig
+from .rome import Rome, RomeConfig
 from .side import Side, SideConfig
 
 
@@ -14,6 +15,7 @@ class SwiftTuners:
     LORA = 'LORA'
     SIDE = 'SIDE'
     RESTUNING = 'RESTUNING'
+    ROME = 'ROME'
     LONGLORA = 'longlora'
 
 
@@ -23,5 +25,6 @@ SWIFT_MAPPING = {
     SwiftTuners.LORA: (LoRAConfig, LoRA),
     SwiftTuners.SIDE: (SideConfig, Side),
     SwiftTuners.RESTUNING: (ResTuningConfig, ResTuning),
+    SwiftTuners.ROME: (RomeConfig, Rome),
     SwiftTuners.LONGLORA: (LongLoRAConfig, LongLoRA),
 }
