1f4eb8890da99f6fc92cbd111be75d29b1af9d45
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2023-10-30 22:01:29 +0800

    lint test

--- tests/utils/test_llm_utils.py
+++ tests/utils/test_llm_utils.py
@@ -1,9 +1,8 @@
 import os
 import unittest
 
-from swift.llm import (MODEL_MAPPING, ModelType,
-                       get_model_tokenizer, get_template, inference,
-                       inference_stream)
+from swift.llm import (MODEL_MAPPING, ModelType, get_model_tokenizer,
+                       get_template, inference, inference_stream)
 from swift.utils import lower_bound, seed_everything
 
 
