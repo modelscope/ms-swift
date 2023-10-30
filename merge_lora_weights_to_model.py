1f39625e02ae83b97c82bf7e91b34c255941dc5c
tastelikefeet <yuze.zyz@alibaba-inc.com>
2023-10-30 20:55:20 +0800

    fix

--- merge_lora_weights_to_model.py
+++ merge_lora_weights_to_model.py
@@ -9,9 +9,9 @@ from swift.utils import parse_args
 
 
 def convert_ckpt(args):
-    assert args.model_type is not None and args.ckpt_dir is not None
-    if not os.path.exists(args.model_type):
-        args.model_type = snapshot_download(args.model_type)
+    assert args.model_id_or_path is not None and args.ckpt_dir is not None
+    if not os.path.exists(args.model_id_or_path):
+        args.model_id_or_path = snapshot_download(args.model_id_or_path)
 
     model, tokenizer = get_model_tokenizer(
         args.model_type, torch_dtype=args.torch_dtype, device_map='cpu')
@@ -19,7 +19,7 @@ def convert_ckpt(args):
     # ### Preparing LoRA
     model = Swift.from_pretrained(model, args.ckpt_dir, inference_mode=True)
     Swift.merge_and_unload(model)
-    model.save_pretrained(args.ckpt_dir + '-merged')
+    model.model.save_pretrained(args.ckpt_dir + '-merged')
     tokenizer.save_pretrained(args.ckpt_dir + '-merged')
     for fname in os.listdir(args.ckpt_dir):
         if fname in {'generation_config.json'}:
