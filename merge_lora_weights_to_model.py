62c52af6a4013956ccaef13b9b5a449c8d82079f
yuze.zyz <yuze.zyz@alibaba-inc.com>
2023-10-30 20:14:22 +0800

    update doc

---
+++ merge_lora_weights_to_model.py
@@ -0,0 +1,35 @@
+import os.path
+import shutil
+
+from modelscope import snapshot_download
+
+from swift import Swift
+from swift.llm import get_model_tokenizer, InferArguments
+from swift.utils import parse_args
+
+
+def convert_ckpt(args):
+    assert args.model_type is not None and args.ckpt_dir is not None
+    if not os.path.exists(args.model_type):
+        args.model_type = snapshot_download(args.model_type)
+
+    model, tokenizer = get_model_tokenizer(
+        args.model_type, torch_dtype=args.torch_dtype, device_map='cpu')
+
+    # ### Preparing LoRA
+    model = Swift.from_pretrained(model, args.ckpt_dir, inference_mode=True)
+    Swift.merge_and_unload(model)
+    model.save_pretrained(args.ckpt_dir + '-merged')
+    tokenizer.save_pretrained(args.ckpt_dir + '-merged')
+    for fname in os.listdir(args.ckpt_dir):
+        if fname in {'generation_config.json'}:
+            src_path = os.path.join(args.ckpt_dir, fname)
+            tgt_path = os.path.join(args.ckpt_dir + '-merged', fname)
+            shutil.copy(src_path, tgt_path)
+    print(f'model saved to : {args.ckpt_dir + "-merged"}')
+
+
+if __name__ == '__main__':
+    args, remaining_argv = parse_args(InferArguments, None)
+    args.init_argument()
+    convert_ckpt(args)
