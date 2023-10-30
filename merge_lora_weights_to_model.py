4804f62e2487354baea2fcb4446e41d58f90f4a3
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2023-10-30 21:54:12 +0800

    fix bug

--- merge_lora_weights_to_model.py
+++ merge_lora_weights_to_model.py
@@ -4,5 +4,4 @@ from swift.utils import parse_args
 
 if __name__ == '__main__':
     args, remaining_argv = parse_args(InferArguments, None)
-    args.init_argument()
     merge_lora(args, replace_if_exists=True)
