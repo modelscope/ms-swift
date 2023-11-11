743352714a0c3f9069ca6fe03ad9bd3905d65791
yuze.zyz <yuze.zyz@alibaba-inc.com>
2023-11-11 21:30:12 +0800

    update

--- examples/pytorch/llm/scripts/chatglm3_6b_32k/lora_ddp_ds/sft.sh
+++ examples/pytorch/llm/scripts/chatglm3_6b_32k/lora_ddp_ds/sft.sh
@@ -1,13 +1,10 @@
 # Experimental environment: 2 * 3090
 # 2 * 20GB GPU memory
-nproc_per_node=2
+nproc_per_node=1
 
 PYTHONPATH=../../.. \
 CUDA_VISIBLE_DEVICES=0,1 \
-torchrun \
-    --nproc_per_node=$nproc_per_node \
-    --master_port 29500 \
-    llm_sft.py \
+python llm_sft.py \
     --model_id_or_path ZhipuAI/chatglm3-6b-32k \
     --model_revision master \
     --sft_type lora \
@@ -16,20 +13,20 @@ torchrun \
     --dtype bf16 \
     --output_dir output \
     --ddp_backend nccl \
-    --dataset damo-agent-mini-zh \
+    --dataset dureader-robust-zh \
     --train_dataset_sample -1 \
     --num_train_epochs 1 \
-    --max_length 4096 \
+    --max_length 512 \
     --check_dataset_strategy warning \
     --lora_rank 8 \
     --lora_alpha 32 \
     --lora_dropout_p 0.05 \
     --lora_target_modules AUTO \
     --gradient_checkpointing true \
-    --batch_size 1 \
+    --batch_size 16 \
     --weight_decay 0.01 \
     --learning_rate 1e-4 \
-    --gradient_accumulation_steps $(expr 16 / $nproc_per_node) \
+    --gradient_accumulation_steps 1 \
     --max_grad_norm 0.5 \
     --warmup_ratio 0.03 \
     --eval_steps 100 \
