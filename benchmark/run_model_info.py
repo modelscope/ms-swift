3150989912ce90b7698b7158cf28e446866e2cb5
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2023-11-17 20:07:05 +0800

    update model_info benchmark

--- benchmark/run_model_info.py
+++ benchmark/run_model_info.py
@@ -14,12 +14,14 @@ def get_model_name_list() -> List[str]:
 
 def format_markdown(model_info: Dict[Tuple, List[str]]) -> str:
     text_list = []
-    md_format = '|{model_list}|{lora_target_modules}|{template}|{requires}|'
+    md_format = '|{model_list}|{lora_target_modules}|{template}|{support_flash_attn}|{requires}|'
+    mapping = {True: '&#x2714;', False: '&#x2718;'}
     for k, v in model_info.items():
         model_list = ', '.join(v)
         lora_target_modules = ', '.join(k[0])
         template = k[1]
-        requires = ', '.join(k[2])
+        support_flash_attn = mapping[k[2]]
+        requires = ', '.join(k[3])
         if len(requires) == 0:
             requires = '-'
         text_list.append(
@@ -27,6 +29,7 @@ def format_markdown(model_info: Dict[Tuple, List[str]]) -> str:
                 model_list=model_list,
                 lora_target_modules=lora_target_modules,
                 template=template,
+                support_flash_attn=support_flash_attn,
                 requires=requires))
     return '\n'.join(text_list)
 
@@ -35,16 +38,18 @@ def write_model_info_table(fpath: str) -> None:
     model_name_list = get_model_name_list()
     with open(fpath, 'w') as f:
         f.write(
-            """| Model List | Default Lora Target Modules | Default Template | Requires |
-| ---------  | --------------------------- | ---------------- | -------- |
+            """| Model List | Default Lora Target Modules | Default Template | Support Flash Attn | Requires |
+| ---------  | --------------------------- | ---------------- | ------------------ | -------- |
 """)
     res = {}
     for model_name in model_name_list:
         model_info = MODEL_MAPPING[model_name]
         lora_target_modules = model_info['lora_target_modules']
         template = model_info['template']
+        support_flash_attn = model_info['support_flash_attn']
         requires = model_info['requires']
-        key = (tuple(lora_target_modules), template, tuple(requires))
+        key = (tuple(lora_target_modules), template, support_flash_attn,
+               tuple(requires))
         if key not in res:
             res[key] = []
         res[key].append(model_name)
