20d2e195ccf6dd30cd94b864ed801cc68977a6ee
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2023-11-21 00:02:33 +0800

    update

--- swift/llm/utils/utils.py
+++ swift/llm/utils/utils.py
@@ -350,7 +350,7 @@ def inference_stream(
     stream_config = StreamGenerationConfig(
         **generation_config.to_dict(), do_stream=True)
     if stream_config.max_new_tokens is not None:
-        stream_config.max_length = 20  # fix max_length, max_new_tokens bug
+        stream_config.max_length = 20  # fix max_length, max_new_tokens warning
     stream_config.do_sample = True  # avoid is_greedy_gen_mode = True
     gen = model.generate_stream(
         input_ids=input_ids,
@@ -395,7 +395,7 @@ def inference(model: PreTrainedModel,
     if stream:
         streamer = TextStreamer(tokenizer, skip_prompt=True)
     if generation_config.max_new_tokens is not None:
-        generation_config.max_length = 20  # fix max_length, max_new_tokens bug
+        generation_config.max_length = 20  # fix max_length, max_new_tokens warning
     generate_ids = model.generate(
         input_ids=input_ids,
         attention_mask=attention_mask,
