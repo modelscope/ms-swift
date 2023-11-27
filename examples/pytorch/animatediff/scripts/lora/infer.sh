749f0ce85f84cf4cb7dce72d42e944202141f71e
tastelikefeet <yuze.zyz@alibaba-inc.com>
2023-11-27 19:13:33 +0800

    fix

--- examples/pytorch/animatediff/scripts/lora/infer.sh
+++ examples/pytorch/animatediff/scripts/lora/infer.sh
@@ -1,10 +1,10 @@
-# Experimental environment: 3090
-# 10GB GPU memory
+# Experimental environment: A100
+# 18GB GPU memory
 PYTHONPATH=../../.. \
 CUDA_VISIBLE_DEVICES=0 \
 python animatediff_infer.py \
   --model_id_or_path wyj123456/Realistic_Vision_V5.1_noVAE \
-  --sft_type full \
   --motion_adapter_id_or_path Shanghai_AI_Laboratory/animatediff-motion-adapter-v1-5-2 \
-  --ckpt_dir /mnt/workspace/yzhao/tastelikefeet/swift/examples/pytorch/animatediff/output/ad-2023-11-25T15-13-58/checkpoints/iter-150000 \
+  --sft_type lora \
+  --ckpt_dir /output/path/like/checkpoints/iter-xxx \
   --eval_human true  \
--- examples/pytorch/animatediff/scripts/lora/sft.sh
+++ examples/pytorch/animatediff/scripts/lora/sft.sh
@@ -1,14 +1,16 @@
-# Experimental environment: 3090
-# 10GB GPU memory
+# Experimental environment: A100
+# 20GB GPU memory
 PYTHONPATH=../../.. \
-CUDA_VISIBLE_DEVICES=0,1,2,3 \
-torchrun --nproc_per_node=4 animatediff_sft.py \
+CUDA_VISIBLE_DEVICES=0 USE_PEFT_BACKEND=1 \
+python animatediff_sft.py \
   --model_id_or_path wyj123456/Realistic_Vision_V5.1_noVAE \
   --csv_path /mnt/workspace/yzhao/tastelikefeet/webvid/results_2M_train.csv \
   --video_folder /mnt/workspace/yzhao/tastelikefeet/webvid/videos2 \
-  --sft_type full \
+  --motion_adapter_id_or_path Shanghai_AI_Laboratory/animatediff-motion-adapter-v1-5-2 \
+  --sft_type lora \
   --lr_scheduler_type constant \
   --trainable_modules .*motion_modules.* \
-  --batch_size 4 \
-  --eval_steps 10000 \
+  --batch_size 1 \
+  --eval_steps 200 \
+  --dataset_sample_size 10000 \
   --gradient_accumulation_steps 16 \
