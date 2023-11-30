3c56bd91094506992a62fe2b5abb8ad29551713b
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2023-11-30 17:03:14 +0800

    update citest

--- tests/llm/test_run.py
+++ tests/llm/test_run.py
@@ -62,39 +62,26 @@ class TestRun(unittest.TestCase):
             # ignore citest error in github
             output_dir = self.tmp_dir
             return
+        losses = []
         for tuner_backend in ['swift', 'peft']:
-            best_model_checkpoint = sft_main([
-                '--model_type',
-                ModelType.qwen_7b_chat_int4,
-                '--eval_steps',
-                '5',
-                '--tuner_backend',
-                tuner_backend,
-                '--train_dataset_sample',
-                '200',
-                '--predict_with_generate',
-                'true',
-                '--dataset',
-                DatasetName.leetcode_python_en,
-                '--output_dir',
-                output_dir,
-                '--use_flash_attn',
-                'false',
-                '--gradient_checkpointing',
-                'true',
-                '--max_new_tokens',
-                '100',
-            ])['best_model_checkpoint']
+            output = sft_main([
+                '--model_type', ModelType.qwen_7b_chat_int4, '--eval_steps',
+                '5', '--tuner_backend', tuner_backend,
+                '--train_dataset_sample', '200', '--predict_with_generate',
+                'true', '--dataset', DatasetName.leetcode_python_en,
+                '--output_dir', output_dir, '--gradient_checkpointing', 'true',
+                '--max_new_tokens', '100', '--use_flash_attn', 'true'
+            ])
+            best_model_checkpoint = output['best_model_checkpoint']
             print(f'best_model_checkpoint: {best_model_checkpoint}')
             torch.cuda.empty_cache()
             infer_main([
-                '--ckpt_dir',
-                best_model_checkpoint,
-                '--show_dataset_sample',
-                '5',
-                '--max_new_tokens',
-                '100',
+                '--ckpt_dir', best_model_checkpoint, '--show_dataset_sample',
+                '2', '--max_new_tokens', '100', '--use_flash_attn', 'true'
             ])
+            loss = output['log_history'][-1]['train_loss']
+            losses.append(loss)
+        self.assertTrue(abs(losses[0] - losses[1]) < 1e-4)
 
     def test_run_3(self):
         output_dir = 'output'
