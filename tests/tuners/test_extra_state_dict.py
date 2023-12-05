6426afb643d08d95f34034722a3cb84eaeaa0946
yuze.zyz <yuze.zyz@alibaba-inc.com>
2023-12-05 22:11:33 +0800

    fix

--- tests/tuners/test_extra_state_dict.py
+++ tests/tuners/test_extra_state_dict.py
@@ -1,9 +1,10 @@
 import math
 import os.path
-import unittest
-import tempfile
-import torch
 import shutil
+import tempfile
+import unittest
+
+import torch
 from modelscope import Model, Preprocessor
 from torch import nn
 
@@ -27,36 +28,57 @@ class TestExtraStateDict(unittest.TestCase):
         model = Model.from_pretrained(
             'damo/nlp_structbert_sentence-similarity_chinese-base')
         lora_config = LoRAConfig(target_modules=['query', 'key', 'value'])
-        model = Swift.prepare_model(model, lora_config, extra_state_keys=['classifier.*'])
+        model = Swift.prepare_model(
+            model, lora_config, extra_state_keys=['classifier.*'])
         model.save_pretrained(self.tmp_dir)
-        self.assertTrue(os.path.isfile(os.path.join(self.tmp_dir, 'adapter_model.bin')))
-        state_dict = torch.load(os.path.join(self.tmp_dir, 'adapter_model.bin'))
+        self.assertTrue(
+            os.path.isfile(os.path.join(self.tmp_dir, 'adapter_model.bin')))
+        state_dict = torch.load(
+            os.path.join(self.tmp_dir, 'adapter_model.bin'))
         self.assertTrue(any('classifier' in key for key in state_dict))
-        state_dict['classifier.weight'] = torch.ones_like(state_dict['classifier.weight']) * 2.0
+        state_dict['classifier.weight'] = torch.ones_like(
+            state_dict['classifier.weight']) * 2.0
         with open(os.path.join(self.tmp_dir, 'adapter_model.bin'), 'wb') as f:
             torch.save(state_dict, f)
         model = Model.from_pretrained(
             'damo/nlp_structbert_sentence-similarity_chinese-base')
         model = Swift.from_pretrained(model, self.tmp_dir)
-        self.assertTrue(torch.allclose(state_dict['classifier.weight'], model.base_model.classifier.weight))
+        self.assertTrue(
+            torch.allclose(state_dict['classifier.weight'],
+                           model.base_model.classifier.weight))
 
     def test_swift_modules_to_save(self):
         model = Model.from_pretrained(
             'damo/nlp_structbert_sentence-similarity_chinese-base')
-        lora_config = LoRAConfig(target_modules=['query', 'key', 'value'], modules_to_save=['classifier'])
-        lora_config2 = LoRAConfig(target_modules=['query', 'key', 'value'], modules_to_save=['classifier'])
-        model = Swift.prepare_model(model, {'lora1': lora_config, 'lora2': lora_config2})
+        lora_config = LoRAConfig(
+            target_modules=['query', 'key', 'value'],
+            modules_to_save=['classifier'])
+        lora_config2 = LoRAConfig(
+            target_modules=['query', 'key', 'value'],
+            modules_to_save=['classifier'])
+        model = Swift.prepare_model(model, {
+            'lora1': lora_config,
+            'lora2': lora_config2
+        })
         model.set_active_adapters('lora1')
         model.set_active_adapters('lora2')
         self.assertTrue(isinstance(model.classifier, ModulesToSaveWrapper))
         self.assertTrue(model.classifier.active_adapter == 'lora2')
         model.save_pretrained(self.tmp_dir)
-        state_dict = torch.load(os.path.join(self.tmp_dir, 'lora2', 'adapter_model.bin'))
+        state_dict = torch.load(
+            os.path.join(self.tmp_dir, 'lora2', 'adapter_model.bin'))
         self.assertTrue(any('classifier' in key for key in state_dict))
-        state_dict['classifier.weight'] = torch.ones_like(state_dict['classifier.weight']) * 2.0
-        with open(os.path.join(self.tmp_dir, 'lora2', 'adapter_model.bin'), 'wb') as f:
+        state_dict['classifier.weight'] = torch.ones_like(
+            state_dict['classifier.weight']) * 2.0
+        with open(
+                os.path.join(self.tmp_dir, 'lora2', 'adapter_model.bin'),
+                'wb') as f:
             torch.save(state_dict, f)
         model = Model.from_pretrained(
             'damo/nlp_structbert_sentence-similarity_chinese-base')
-        model = Swift.from_pretrained(model, self.tmp_dir, adapter_name='lora2')
-        self.assertTrue(torch.allclose(state_dict['classifier.weight'], model.base_model.classifier.modules_to_save['lora2'].weight))
+        model = Swift.from_pretrained(
+            model, self.tmp_dir, adapter_name='lora2')
+        self.assertTrue(
+            torch.allclose(
+                state_dict['classifier.weight'],
+                model.base_model.classifier.modules_to_save['lora2'].weight))
