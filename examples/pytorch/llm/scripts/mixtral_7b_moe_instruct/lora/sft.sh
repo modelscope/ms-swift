c3a9c7f257cb2f2c55c0bd6f191ce4e606ed7912
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2023-12-14 12:18:27 +0800

    update mixtral

--- examples/pytorch/llm/scripts/mixtral_7b_moe_instruct/lora/sft.sh
+++ examples/pytorch/llm/scripts/mixtral_7b_moe_instruct/lora/sft.sh
@@ -9,10 +9,9 @@ python llm_sft.py \
     --tuner_backend swift \
     --dtype AUTO \
     --output_dir output \
-    --ddp_backend nccl \
     --dataset dureader-robust-zh \
     --train_dataset_sample -1 \
-    --num_train_epochs 2 \
+    --num_train_epochs 1 \
     --max_length 2048 \
     --check_dataset_strategy warning \
     --lora_rank 8 \
@@ -25,9 +24,9 @@ python llm_sft.py \
     --gradient_accumulation_steps 16 \
     --max_grad_norm 0.5 \
     --warmup_ratio 0.03 \
-    --eval_steps 300 \
-    --save_steps 300 \
+    --eval_steps 100 \
+    --save_steps 100 \
     --save_total_limit 2 \
     --logging_steps 10 \
     --only_save_model true \
-    --gradient_checkpointing false
+    --gradient_checkpointing true
