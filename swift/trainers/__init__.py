8a275ff504b7b1e48a196b8d49d2128957894c12
tastelikefeet <58414341+tastelikefeet@users.noreply.github.com>
2023-12-29 21:51:28 +0800

    Fix import (#266)

--- swift/trainers/__init__.py
+++ swift/trainers/__init__.py
@@ -1,21 +1,24 @@
 # Copyright (c) Alibaba, Inc. and its affiliates.
 from typing import TYPE_CHECKING
 
-from transformers.trainer_utils import (EvaluationStrategy, FSDPOption,
-                                        HPSearchBackend, HubStrategy,
-                                        IntervalStrategy, SchedulerType)
-
 from swift.utils.import_utils import _LazyModule
 
 if TYPE_CHECKING:
     from .arguments import Seq2SeqTrainingArguments, TrainingArguments
     from .dpo_trainers import DPOTrainer
     from .trainers import Seq2SeqTrainer, Trainer
+    from .utils import EvaluationStrategy, FSDPOption, HPSearchBackend, HubStrategy, \
+        IntervalStrategy, SchedulerType, ShardedDDPOption
 else:
     _import_structure = {
         'arguments': ['Seq2SeqTrainingArguments', 'TrainingArguments'],
         'dpo_trainers': ['DPOTrainer'],
         'trainers': ['Seq2SeqTrainer', 'Trainer'],
+        'utils': [
+            'EvaluationStrategy', 'FSDPOption', 'HPSearchBackend',
+            'HubStrategy', 'IntervalStrategy', 'SchedulerType',
+            'ShardedDDPOption'
+        ]
     }
 
     import sys
@@ -27,9 +30,3 @@ else:
         module_spec=__spec__,
         extra_objects={},
     )
-
-try:
-    # https://github.com/huggingface/transformers/pull/25702
-    from transformers.trainer_utils import ShardedDDPOption
-except ImportError:
-    pass
