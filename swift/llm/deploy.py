c01f531298140db8c57234ad67c3db04107801b2
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2024-01-04 11:57:48 +0800

    update

--- swift/llm/deploy.py
+++ swift/llm/deploy.py
@@ -1,17 +1,19 @@
-import time
-import uuid
-from dataclasses import dataclass, field
 from http import HTTPStatus
-from typing import Dict, List, Literal, Optional, Union
+from typing import List, Optional, Union
 
 import uvicorn
 from fastapi import FastAPI, Request
-from fastapi.responses import JSONResponse, Response
+from fastapi.responses import JSONResponse
 
 from swift.utils import seed_everything
 from .infer import merge_lora, prepare_model_template
 from .utils import (DeployArguments, VllmGenerationConfig, messages_to_history,
                     prepare_vllm_engine_template)
+from .utils.protocol import (ChatCompletionRequest, ChatCompletionResponse,
+                             ChatCompletionResponseChoice, ChatMessage,
+                             CompletionRequest, CompletionResponse,
+                             CompletionResponseChoice, Model, ModelList,
+                             UsageInfo, random_uuid)
 
 app = FastAPI()
 model = None
@@ -23,111 +25,6 @@ def create_error_response(status_code: Union[int, str, HTTPStatus],
                           message: str) -> JSONResponse:
     status_code = int(status_code)
     return JSONResponse({'message': message, 'object': 'error'}, status_code)
-
-
-def random_uuid() -> str:
-    return str(uuid.uuid4().hex)
-
-
-@dataclass
-class Model:
-    id: str  # model_type
-
-
-@dataclass
-class ModelList:
-    data: List[Model]
-    object: str = 'list'
-
-
-@dataclass
-class CompletionRequest:
-    model: str
-    prompt: str
-    #
-    n: int = 1
-    max_tokens: Optional[int] = None
-    temperature: float = 1.
-    top_k: int = -1
-    top_p: float = 1.
-    repetition_penalty: float = 1.
-    num_beams: int = 1
-    #
-    seed: Optional[int] = None
-    stop: Optional[List[str]] = None
-    stream: bool = False
-    #
-    best_of: Optional[int] = None
-    presence_penalty: float = 0.
-    frequency_penalty: float = 0.
-
-
-@dataclass
-class ChatCompletionRequest:
-    model: str
-    messages: List[Dict[str, str]]
-    #
-    n: int = 1
-    max_tokens: Optional[int] = None
-    temperature: float = 1.
-    top_k: int = -1
-    top_p: float = 1.
-    repetition_penalty: float = 1.
-    num_beams: int = 1
-    #
-    seed: Optional[int] = None
-    stop: List[str] = field(default_factory=list)
-    stream: bool = False
-    best_of: Optional[int] = None
-    presence_penalty: float = 0.
-    frequency_penalty: float = 0.
-
-
-@dataclass
-class UsageInfo:
-    prompt_tokens: int = 0
-    completion_tokens: int = 0
-    total_tokens: int = 0
-
-
-@dataclass
-class ChatMessage:
-    role: Literal['system', 'user', 'assistant']
-    content: str
-
-
-@dataclass
-class ChatCompletionResponseChoice:
-    index: int
-    message: ChatMessage
-    finish_reason: Literal['stop', 'length']
-
-
-@dataclass
-class CompletionResponseChoice:
-    index: int
-    text: str
-    finish_reason: Literal['stop', 'length']
-
-
-@dataclass
-class ChatCompletionResponse:
-    model: str
-    choices: List[ChatCompletionResponseChoice]
-    usage: UsageInfo
-    id: str = field(default_factory=lambda: f'chatcmpl-{random_uuid()}')
-    object: str = 'chat.completion'
-    created: int = field(default_factory=lambda: int(time.time()))
-
-
-@dataclass
-class CompletionResponse:
-    model: str
-    choices: List[CompletionResponseChoice]
-    usage: UsageInfo
-    id: str = field(default_factory=lambda: f'cmpl-{random_uuid()}')
-    object: str = 'text_completion'
-    created: int = field(default_factory=lambda: int(time.time()))
 
 
 @app.get('/v1/models')
---
+++ swift/llm/utils/protocol.py
@@ -0,0 +1,109 @@
+import time
+import uuid
+from dataclasses import dataclass, field
+from typing import Dict, List, Literal, Optional
+
+
+def random_uuid() -> str:
+    return str(uuid.uuid4().hex)
+
+
+@dataclass
+class Model:
+    id: str  # model_type
+
+
+@dataclass
+class ModelList:
+    data: List[Model]
+    object: str = 'list'
+
+
+@dataclass
+class CompletionRequest:
+    model: str
+    prompt: str
+    #
+    n: int = 1
+    max_tokens: Optional[int] = None
+    temperature: float = 1.
+    top_k: int = -1
+    top_p: float = 1.
+    repetition_penalty: float = 1.
+    num_beams: int = 1
+    #
+    seed: Optional[int] = None
+    stop: Optional[List[str]] = None
+    stream: bool = False
+    #
+    best_of: Optional[int] = None
+    presence_penalty: float = 0.
+    frequency_penalty: float = 0.
+
+
+@dataclass
+class ChatCompletionRequest:
+    model: str
+    messages: List[Dict[str, str]]
+    #
+    n: int = 1
+    max_tokens: Optional[int] = None
+    temperature: float = 1.
+    top_k: int = -1
+    top_p: float = 1.
+    repetition_penalty: float = 1.
+    num_beams: int = 1
+    #
+    seed: Optional[int] = None
+    stop: List[str] = field(default_factory=list)
+    stream: bool = False
+    best_of: Optional[int] = None
+    presence_penalty: float = 0.
+    frequency_penalty: float = 0.
+
+
+@dataclass
+class UsageInfo:
+    prompt_tokens: int = 0
+    completion_tokens: int = 0
+    total_tokens: int = 0
+
+
+@dataclass
+class ChatMessage:
+    role: Literal['system', 'user', 'assistant']
+    content: str
+
+
+@dataclass
+class ChatCompletionResponseChoice:
+    index: int
+    message: ChatMessage
+    finish_reason: Literal['stop', 'length']
+
+
+@dataclass
+class CompletionResponseChoice:
+    index: int
+    text: str
+    finish_reason: Literal['stop', 'length']
+
+
+@dataclass
+class ChatCompletionResponse:
+    model: str
+    choices: List[ChatCompletionResponseChoice]
+    usage: UsageInfo
+    id: str = field(default_factory=lambda: f'chatcmpl-{random_uuid()}')
+    object: str = 'chat.completion'
+    created: int = field(default_factory=lambda: int(time.time()))
+
+
+@dataclass
+class CompletionResponse:
+    model: str
+    choices: List[CompletionResponseChoice]
+    usage: UsageInfo
+    id: str = field(default_factory=lambda: f'cmpl-{random_uuid()}')
+    object: str = 'text_completion'
+    created: int = field(default_factory=lambda: int(time.time()))
