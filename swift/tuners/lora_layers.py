32c0eea9c9013c7b09dae8406fcf66f1952befe2
yuze.zyz <yuze.zyz@alibaba-inc.com>
2024-01-05 18:09:20 +0800

    fix

--- swift/tuners/lora_layers.py
+++ swift/tuners/lora_layers.py
@@ -489,6 +489,7 @@ class LoraModel(_LoraModel):
         elif lora_config.use_merged_linear:
             new_module = MergedLinear(
                 adapter_name,
+                current_key,
                 target,
                 bias=bias,
                 enable_lora=lora_config.enable_lora,
