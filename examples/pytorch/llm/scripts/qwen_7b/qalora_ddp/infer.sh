695bcbe42a273dc1f0226cb9262bb51fd64e4476
yuze.zyz <yuze.zyz@alibaba-inc.com>
2024-01-09 17:48:30 +0800

    add script

---
+++ examples/pytorch/llm/scripts/qwen_7b/qalora_ddp/infer.sh
@@ -0,0 +1,14 @@
+# Experimental environment: A10
+PYTHONPATH=../../.. \
+CUDA_VISIBLE_DEVICES=0 \
+python llm_infer.py \
+    --ckpt_dir "output/qwen-7b/vx_xxx/checkpoint-xxx" \
+    --load_dataset_config true \
+    --max_length 4096 \
+    --use_flash_attn false \
+    --max_new_tokens 2048 \
+    --temperature 0.7 \
+    --top_p 0.7 \
+    --repetition_penalty 1.05 \
+    --do_sample true \
+    --merge_lora_and_save false \
