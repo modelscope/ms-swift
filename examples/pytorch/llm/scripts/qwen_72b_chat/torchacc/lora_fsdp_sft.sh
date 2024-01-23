d967e5b455137c949fc8b455931d25170e113560
baole.abl <baole.abl@alibaba-inc.com>
2024-01-23 16:42:08 +0800

    update script

--- examples/pytorch/llm/scripts/qwen_72b_chat/torchacc/lora_fsdp_sft.sh
+++ examples/pytorch/llm/scripts/qwen_72b_chat/torchacc/lora_fsdp_sft.sh
@@ -1,5 +1,5 @@
 export USE_TORCHACC=1
-export XLA_FLAGS='--xla_multiheap_size_constraint_per_heap=4294967296 --xla_disable_hlo_passes=rematerialization,all-gather-combiner,all-reduce-combiner,reduce-scatter-combiner'
+export XLA_FLAGS='--xla_multiheap_size_constraint_per_heap=4294967296 --xla_disable_hlo_passes=all-gather-combiner,all-reduce-combiner,reduce-scatter-combiner'
 export XLA_IR_SHAPE_CACHE_SIZE=100000000
 export XLA_ALLOCATOR_FRACTION=0.97
 
