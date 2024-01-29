bfc11518304bfe72e63817e133ed3c44559ba02a
yuze.zyz <yuze.zyz@alibaba-inc.com>
2024-01-29 17:29:11 +0800

    update script

--- examples/pytorch/llm/scripts/chatglm3_6b_32k/qlora/infer.sh
+++ examples/pytorch/llm/scripts/chatglm3_6b_32k/qlora/infer.sh
@@ -2,12 +2,14 @@
 PYTHONPATH=../../.. \
 CUDA_VISIBLE_DEVICES=0 \
 python llm_infer.py \
-    --ckpt_dir "output/chatglm3-6b-32k/vx_xxx/checkpoint-xxx" \
+    --ckpt_dir "output/chatglm3-6b-32k/v4-20240125-235528/checkpoint-11278" \
     --load_dataset_config true \
     --max_length 4096 \
     --max_new_tokens 2048 \
     --temperature 0.1 \
     --top_p 0.7 \
+    --eval_human true \
     --repetition_penalty 1. \
     --do_sample true \
-    --merge_lora_and_save false \
+    --infer_backend pt \
+    --merge_lora_and_save false
--- examples/pytorch/llm/scripts/chatglm3_6b_32k/qlora/sft.sh
+++ examples/pytorch/llm/scripts/chatglm3_6b_32k/qlora/sft.sh
@@ -1,38 +1,43 @@
 # Experimental environment: A10, 3090
 # 10GB GPU memory
+nproc_per_node=4
+
 PYTHONPATH=../../.. \
-CUDA_VISIBLE_DEVICES=0 \
-python llm_sft.py \
+torchrun \
+    --nproc_per_node=$nproc_per_node \
+    --master_port 29500 \
+    llm_sft.py \
     --model_id_or_path ZhipuAI/chatglm3-6b-32k \
     --model_revision master \
-    --sft_type lora \
+    --sft_type full \
+    --train_dataset_mix_ratio 2.0 \
     --tuner_backend swift \
-    --template_type chatglm3 \
     --dtype AUTO \
     --output_dir output \
-    --dataset agent-instruct-all-en \
+    --dataset ms-agent \
     --train_dataset_sample -1 \
-    --num_train_epochs 1 \
-    --max_length 4096 \
+    --num_train_epochs 2 \
+    --max_length 2048 \
     --check_dataset_strategy warning \
-    --quantization_bit 4 \
-    --bnb_4bit_comp_dtype AUTO \
     --lora_rank 8 \
     --lora_alpha 32 \
     --lora_dropout_p 0.05 \
-    --lora_target_modules DEFAULT \
+    --lora_target_modules ALL \
+    --self_cognition_sample 3000 \
+    --model_name 小灰灰 \
+    --model_author 陶白白 \
     --gradient_checkpointing true \
     --batch_size 1 \
     --weight_decay 0.01 \
     --learning_rate 1e-4 \
-    --gradient_accumulation_steps 16 \
+    --gradient_accumulation_steps $(expr 16 / $nproc_per_node) \
     --max_grad_norm 0.5 \
     --warmup_ratio 0.03 \
-    --eval_steps 100 \
-    --save_steps 100 \
+    --eval_steps 500 \
+    --save_steps 500 \
     --save_total_limit 2 \
     --logging_steps 10 \
     --push_to_hub false \
     --hub_model_id chatglm3-6b-32k-qlora \
     --hub_private_repo true \
-    --hub_token 'your-sdk-token' \
+    --hub_token 'your-sdk-token'
