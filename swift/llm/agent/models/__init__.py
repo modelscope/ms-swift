a0bc0ce8bba1b9fbac1693f4c156c2d84542dacd
yuze.zyz <yuze.zyz@alibaba-inc.com>
2024-01-29 23:25:50 +0800

    support qwen

--- swift/llm/agent/models/__init__.py
+++ swift/llm/agent/models/__init__.py
@@ -1,2 +1,4 @@
 from .chatglm3 import forward as ChatGLM3Forward
 from .internlm2 import forward as InternLMForward
+from .qwen import forward as QwenForward
+
---
+++ swift/llm/agent/models/qwen.py
@@ -0,0 +1,78 @@
+""" PyTorch ChatGLM model. """
+
+from typing import Optional, Tuple, Union, List
+
+import torch.utils.checkpoint
+from torch.nn import CrossEntropyLoss
+from transformers.modeling_outputs import (
+    CausalLMOutputWithPast,
+)
+
+
+def forward(
+        self,
+        input_ids: Optional[torch.LongTensor] = None,
+        past_key_values: Optional[Tuple[Tuple[torch.Tensor]]] = None,
+        attention_mask: Optional[torch.FloatTensor] = None,
+        token_type_ids: Optional[torch.LongTensor] = None,
+        position_ids: Optional[torch.LongTensor] = None,
+        head_mask: Optional[torch.FloatTensor] = None,
+        inputs_embeds: Optional[torch.FloatTensor] = None,
+        encoder_hidden_states: Optional[torch.Tensor] = None,
+        encoder_attention_mask: Optional[torch.FloatTensor] = None,
+        labels: Optional[torch.LongTensor] = None,
+        use_cache: Optional[bool] = None,
+        output_attentions: Optional[bool] = None,
+        output_hidden_states: Optional[bool] = None,
+        return_dict: Optional[bool] = None,
+        **kwargs,
+) -> Union[Tuple, CausalLMOutputWithPast]:
+    return_dict = (
+        return_dict if return_dict is not None else self.config.use_return_dict
+    )
+
+    transformer_outputs = self.transformer(
+        input_ids,
+        past_key_values=past_key_values,
+        attention_mask=attention_mask,
+        token_type_ids=token_type_ids,
+        position_ids=position_ids,
+        head_mask=head_mask,
+        inputs_embeds=inputs_embeds,
+        encoder_hidden_states=encoder_hidden_states,
+        encoder_attention_mask=encoder_attention_mask,
+        use_cache=use_cache,
+        output_attentions=output_attentions,
+        output_hidden_states=output_hidden_states,
+        return_dict=return_dict,
+    )
+    hidden_states = transformer_outputs[0]
+
+    lm_logits = self.lm_head(hidden_states)
+
+    loss = None
+    if labels is not None:
+        labels = labels.to(lm_logits.device)
+        shift_logits = lm_logits[..., :-1, :].contiguous()
+        shift_labels = labels[..., 1:].contiguous()
+        loss_fct = CrossEntropyLoss(reduction='none')
+        loss = loss_fct(
+            shift_logits.view(-1, shift_logits.size(-1)), shift_labels.view(-1)
+        )
+        loss_scale = kwargs.pop('loss_scale', None)
+        if loss_scale is not None:
+            loss_scale = loss_scale[..., 1:].contiguous().view(-1)
+            loss = loss_scale * loss
+        loss = loss.mean()
+
+    if not return_dict:
+        output = (lm_logits,) + transformer_outputs[1:]
+        return ((loss,) + output) if loss is not None else output
+
+    return CausalLMOutputWithPast(
+        loss=loss,
+        logits=lm_logits,
+        past_key_values=transformer_outputs.past_key_values,
+        hidden_states=transformer_outputs.hidden_states,
+        attentions=transformer_outputs.attentions,
+    )
