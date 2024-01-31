b8ebc4710024eac17368af860d981cf48750049f
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2024-02-01 02:01:06 +0800

    update openbmb sh

---
+++ examples/pytorch/llm/scripts/openbmb_minicpm_2b_chat/full_ddp/infer.sh
@@ -0,0 +1,9 @@
+# Experimental environment: 3090
+CUDA_VISIBLE_DEVICES=0 \
+swift infer \
+    --ckpt_dir "output/openbmb-minicpm-2b-chat/vx_xxx/checkpoint-xxx" \
+    --load_dataset_config true \
+    --max_length 2048 \
+    --use_flash_attn false \
+    --max_new_tokens 2048 \
+    --do_sample false \
