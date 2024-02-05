b9c869915cea866cfab31aa9e9d9397f5b6fc1dd
yuze.zyz <yuze.zyz@alibaba-inc.com>
2024-02-05 18:19:30 +0800

    fix

--- swift/llm/agent/utils.py
+++ swift/llm/agent/utils.py
@@ -57,7 +57,8 @@ def split_agent_parts_by(text: str, delimiters: List[str]):
 
 
 def calculate_loss_scale(response: str,
-                         use_loss_scale=False) -> Tuple[List[str], List[float]]:
+                         use_loss_scale=False
+                         ) -> Tuple[List[str], List[float]]:
     """Calculate the loss scale by splitting the agent response.
 
     This algorithm comes from paper: https://arxiv.org/pdf/2309.00986.pdf
