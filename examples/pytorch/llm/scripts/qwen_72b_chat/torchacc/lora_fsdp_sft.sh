b140759856df19f562dbbde0d45d69cc3052d1bd
Baole Ai <baole.abl@alibaba-inc.com>
2024-02-06 14:24:07 +0800

    Enhance TorchAcc support for dynamic sequence. (#382)

--- examples/pytorch/llm/scripts/qwen_72b_chat/torchacc/lora_fsdp_sft.sh
+++ examples/pytorch/llm/scripts/qwen_72b_chat/torchacc/lora_fsdp_sft.sh
@@ -1,14 +1,15 @@
-# Experimental environment: 2 * A800
+# Experimental environment: 4 * A800
 # 80GB GPU memory
 # Note: TorchAcc is currently only available internally.
 
 export USE_TORCHACC=1
 export XLA_FLAGS='--xla_multiheap_size_constraint_per_heap=4831838208 --xla_disable_hlo_passes=all-gather-combiner,all-reduce-combiner,reduce-scatter-combiner'
 export XLA_IR_SHAPE_CACHE_SIZE=100000000
-export XLA_ALLOCATOR_FRACTION=0.97
+export XLA_ALLOCATOR_FRACTION=0.95
+export XLA_EXPERIMENTAL=nonzero:masked_select
 
-NPROC_PER_NODE=2 \
-CUDA_VISIBLE_DEVICES=0,1 \
+NPROC_PER_NODE=4 \
+CUDA_VISIBLE_DEVICES=0,1,2,3 \
 swift sft \
 	--model_type qwen-72b-chat \
   --model_layer_cls_name QWenBlock \
@@ -17,7 +18,7 @@ swift sft \
   --output_dir output \
   --num_train_epochs 1 \
   --max_length 1024 \
-  --batch_size 1 \
+  --batch_size 6 \
   --use_flash_attn true \
   --gradient_accumulation_steps 1 \
   --gradient_checkpointing no \
--- swift/llm/sft.py
+++ swift/llm/sft.py
@@ -214,6 +214,14 @@ def llm_sft(args: SftArguments) -> Dict[str, Union[str, Any]]:
     padding_to = args.max_length if args.sft_type == 'longlora' else None
     data_collator = partial(template.data_collator, padding_to=padding_to,
                             bucket_sizes=bucket_sizes)
+    trian_batch_size = args.batch_size
+    eval_batch_size = args.eval_batch_size
+    if use_torchacc():
+        trian_batch_size *= world_size
+        eval_batch_size *= world_size
+    training_args.per_device_train_batch_size = trian_batch_size
+    training_args.per_device_eval_batch_size = eval_batch_size
+    training_args.group_by_length=use_torchacc()
 
     # Trainer
     logger.info(f'training_args: {training_args}')
