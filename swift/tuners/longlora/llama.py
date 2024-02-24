344a2002dcc4296cc00ede865c9c7bc30314fbab
tastelikefeet <yuze.zyz@alibaba-inc.com>
2024-02-24 23:32:13 +0800

    wip

--- swift/tuners/longlora/llama.py
+++ swift/tuners/longlora/llama.py
@@ -17,9 +17,40 @@ from swift.utils import get_logger
 logger = get_logger()
 
 
-def _preprocess_qkv(attn_module, query_states, key_states, value_states, attention_mask):
-    if attn_module.is_training():
+def _preprocess_qkv_fa2(attn_module, query_states, key_states, value_states, attention_mask):
+    if attn_module.training:
         bsz, q_len = query_states.shape[:2]
+        group_size = int(q_len * attn_module.config.group_size_ratio)
+        if q_len % group_size != 0:
+            raise ValueError(
+                f'The sequence length {q_len} should'
+                f'be able to be splitted by the group_ratio {attn_module.config.group_size_ratio}'
+            )
+
+        num_group = q_len // group_size
+
+        def shift(qkv, bsz, q_len, group_size, num_heads, head_dim):
+            qkv[:, :, num_heads // 2:] = qkv[:, :, num_heads // 2:].roll(
+                -group_size // 2, dims=1)
+            qkv = qkv.reshape(bsz * num_group, group_size, num_heads, head_dim)
+            return qkv
+
+        query_states = shift(query_states, bsz, q_len, group_size,
+                             attn_module.num_heads, attn_module.head_dim)
+        key_states = shift(key_states, bsz, q_len, group_size, attn_module.num_heads,
+                           attn_module.head_dim)
+        value_states = shift(value_states, bsz, q_len, group_size,
+                             attn_module.num_heads, attn_module.head_dim)
+        if attention_mask is not None:
+            attention_mask = attention_mask[:, :group_size].repeat(
+                num_group, 1)
+
+    return query_states, key_states, value_states, attention_mask
+
+
+def _preprocess_qkv(attn_module, query_states, key_states, value_states, attention_mask):
+    if attn_module.training:
+        bsz, _, q_len = query_states.shape[:3]
         group_size = int(q_len * attn_module.config.group_size_ratio)
         if q_len % group_size != 0:
             raise ValueError(
@@ -32,10 +63,9 @@ def _preprocess_qkv(attn_module, query_states, key_states, value_states, attenti
         def shift(qkv, bsz, q_len, group_size, num_heads, head_dim):
             qkv[:, num_heads // 2:] = qkv[:, num_heads // 2:].roll(
                 -group_size // 2, dims=2)
-            qkv = qkv.transpose(1, 2).reshape(bsz * (q_len // group_size),
-                                              group_size, num_heads,
-                                              head_dim).transpose(1, 2)
-            return qkv
+            qkv = qkv.transpose(1, 2)
+            qkv = qkv.reshape(bsz * num_group, group_size, num_heads, head_dim)
+            return qkv.transpose(1, 2)
 
         query_states = shift(query_states, bsz, q_len, group_size,
                              attn_module.num_heads, attn_module.head_dim)
@@ -45,16 +75,30 @@ def _preprocess_qkv(attn_module, query_states, key_states, value_states, attenti
                              attn_module.num_heads, attn_module.head_dim)
         if attention_mask is not None:
             attention_mask = attention_mask[:, :, :group_size, :
-                                                               group_size].repeat(
-                num_group, 1, 1, 1)
+                                group_size].repeat(
+                                    num_group, 1, 1, 1)
 
     return query_states, key_states, value_states, attention_mask
 
 
-def _postprocess_qkv(attn_module, attn_output):
+def _postprocess_qkv(attn_module, attn_output, q_len):
     if attn_module.training:
-        q_len = attn_output.shape[1]
         group_size = int(q_len * attn_module.config.group_size_ratio)
+        attn_output = attn_output.transpose(1, 2)
+        attn_output = attn_output.reshape(-1, q_len, attn_module.num_heads,
+                                    attn_module.head_dim)
+        # shift back
+        attn_output[:, :, attn_module.num_heads
+                    // 2:] = attn_output[:, :, attn_module.num_heads // 2:].roll(
+                        group_size // 2, dims=1)
+    return attn_output.transpose(1, 2)
+
+
+def _postprocess_qkv_fa2(attn_module, attn_output, q_len):
+    if attn_module.training:
+        group_size = int(q_len * attn_module.config.group_size_ratio)
+        attn_output = attn_output.reshape(-1, q_len, attn_module.num_heads,
+                                    attn_module.head_dim)
         # shift back
         attn_output[:, :, attn_module.num_heads
                     // 2:] = attn_output[:, :, attn_module.num_heads // 2:].roll(
@@ -121,23 +165,19 @@ def eager_forward(
     attn_weights = torch.matmul(query_states, key_states.transpose(2, 3)) / math.sqrt(self.head_dim)
 
     if attention_mask is not None:  # no matter the length, we just slice it
-        if cache_position is not None:
+        if cache_position is not None and not self.training:
             causal_mask = attention_mask[:, :, cache_position, : key_states.shape[-2]]
-        attn_weights = attn_weights + causal_mask
+            attn_weights = attn_weights + causal_mask
+        else:
+            attn_weights = attn_weights + attention_mask
 
     # upcast attention to fp32
     attn_weights = nn.functional.softmax(attn_weights, dim=-1, dtype=torch.float32).to(query_states.dtype)
     attn_weights = nn.functional.dropout(attn_weights, p=self.attention_dropout, training=self.training)
     attn_output = torch.matmul(attn_weights, value_states)
 
-    if attn_output.size() != (bsz, self.num_heads, q_len, self.head_dim):
-        raise ValueError(
-            f"`attn_output` should be of size {(bsz, self.num_heads, q_len, self.head_dim)}, but is"
-            f" {attn_output.size()}"
-        )
-
     # patch position unrolling
-    attn_output = _postprocess_qkv(self, attn_output)
+    attn_output = _postprocess_qkv(self, attn_output, q_len)
 
     attn_output = attn_output.transpose(1, 2).contiguous()
 
@@ -228,15 +268,15 @@ def fa2_forward(
         value_states = value_states.to(target_dtype)
 
     # patch position rolling
-    query_states, key_states, value_states, attention_mask = _preprocess_qkv(self,
+    query_states, key_states, value_states, attention_mask = _preprocess_qkv_fa2(self,
         query_states, key_states, value_states, attention_mask)
 
     attn_output = self._flash_attention_forward(
-        query_states, key_states, value_states, attention_mask, q_len, dropout=dropout_rate
+        query_states, key_states, value_states, attention_mask, query_states.shape[1], dropout=dropout_rate
     )
 
     # patch position unrolling
-    attn_output = _postprocess_qkv(self, attn_output)
+    attn_output = _postprocess_qkv_fa2(self, attn_output, q_len)
 
     attn_output = attn_output.reshape(bsz, q_len, self.hidden_size).contiguous()
     attn_output = self.o_proj(attn_output)
@@ -310,8 +350,8 @@ def sdpa_forward(
         value_states = value_states.contiguous()
 
     # patch position rolling
-    query_states, key_states, value_states, attention_mask = _preprocess_qkv(self,
-        query_states, key_states, value_states, attention_mask)
+    query_states, key_states, value_states, causal_mask = _preprocess_qkv(self,
+        query_states, key_states, value_states, causal_mask)
 
     attn_output = torch.nn.functional.scaled_dot_product_attention(
         query_states,
@@ -322,7 +362,7 @@ def sdpa_forward(
     )
 
     # patch position unrolling
-    attn_output = _postprocess_qkv(self, attn_output)
+    attn_output = _postprocess_qkv(self, attn_output, q_len)
 
     attn_output = attn_output.transpose(1, 2).contiguous()
     attn_output = attn_output.view(bsz, q_len, self.hidden_size)
