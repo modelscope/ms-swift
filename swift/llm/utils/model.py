627308246e165bdfe6791453387a4229c515567a
yuze.zyz <yuze.zyz@alibaba-inc.com>
2024-03-05 21:36:19 +0800

    fix comments

--- swift/llm/utils/model.py
+++ swift/llm/utils/model.py
@@ -76,10 +76,10 @@ class ModelType:
     qwen1half_72b_chat_awq = 'qwen1half-72b-chat-awq'
 
     # aqlm llama model
-    llama2_7b_aqlm_2bit_1x16 = 'llama2-7b-aqlm-2bit-1x16-hf'
+    llama2_7b_aqlm_2bit_1x16 = 'llama2-7b-aqlm-2bit-1x16'
 
     # aqlm mixtral model
-    mixtral_moe_7b_aqlm_2bit_1x16 = 'mixtral-moe-7b-aqlm-2bit-1x16-hf'
+    mixtral_moe_7b_aqlm_2bit_1x16 = 'mixtral-moe-7b-aqlm-2bit-1x16'
 
     # qwen1.5 gptq
     qwen1half_0_5b_chat_int4 = 'qwen1half-0_5b-chat-int4'
