2ab28b5fda92e4ebb9a760eb673417c7b24491f7
yuze.zyz <yuze.zyz@alibaba-inc.com>
2024-03-05 20:43:43 +0800

    fix throw

--- swift/llm/utils/model.py
+++ swift/llm/utils/model.py
@@ -435,11 +435,11 @@ def get_model_tokenizer_from_repo(model_dir: str,
             try:
                 from awq.utils.packing_utils import dequantize_gemm
                 import awq_ext  # with CUDA kernels (AutoAWQ_kernels)
-            except:
+            except ImportError as e:
                 raise ImportError(
                     'You are training awq models, remember installing awq_ext by '
                     '`git clone https://github.com/casper-hansen/AutoAWQ_kernels '
-                    '&& cd AutoAWQ_kernels && pip install -e .`')
+                    '&& cd AutoAWQ_kernels && pip install -e .`') from e
         # import aqlm
         # context = aqlm.optimize_for_training
         with context:
