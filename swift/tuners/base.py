d1b7b4ab3fbeb44cbfd43f2996137a9f083cbde3
yuze.zyz <yuze.zyz@alibaba-inc.com>
2024-03-05 11:59:19 +0800

    revert peft format

--- swift/tuners/base.py
+++ swift/tuners/base.py
@@ -496,7 +496,7 @@ class SwiftModel(nn.Module):
             safe_serialization (`bool`): Use safe tensors to save the weights, default False.
             adapter_name(`Union[str, List[str]]`): The adapters to be saved, default is `None` to save all.
         """
-        peft_format = kwargs.pop('peft_format', True)
+        peft_format = kwargs.pop('peft_format', False)
         if os.path.isfile(save_directory):
             raise ValueError(
                 f'Provided path ({save_directory}) should be a directory, not a file'
