5666d2ec68dd78124c14322b4a0a95a704a0c113
yuze.zyz <yuze.zyz@alibaba-inc.com>
2024-03-08 23:45:49 +0800

    scripts

---
+++ examples/pytorch/llm/scripts/qwen1half_7b_chat_awq/lora/infer.sh
@@ -0,0 +1,14 @@
+# Experimental environment: V100, A10, 3090
+PYTHONPATH=../../.. \
+CUDA_VISIBLE_DEVICES=0 \
+python llm_infer.py \
+    --ckpt_dir "output/qwen1half-7b-chat-awq/vx-xxx/checkpoint-xxx" \
+    --load_dataset_config true \
+    --max_length 2048 \
+    --use_flash_attn false \
+    --max_new_tokens 2048 \
+    --temperature 0.1 \
+    --top_p 0.7 \
+    --repetition_penalty 1. \
+    --do_sample true \
+    --merge_lora false \
