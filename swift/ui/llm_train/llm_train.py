0e6e23ea71b823e27db2a9b05a5bc0d9278bc8b1
tastelikefeet <58414341+tastelikefeet@users.noreply.github.com>
2024-03-11 19:04:38 +0800

    fix hyper parameters (#529)
    (cherry picked from commit b99ae52c506851797dcad072633b409843ad4ff7)

--- swift/ui/llm_train/llm_train.py
+++ swift/ui/llm_train/llm_train.py
@@ -315,7 +315,7 @@ class LLMTrain(BaseUI):
             else:
                 params += f'--{e} "{kwargs[e]}" '
         params += f'--add_output_dir_suffix False --output_dir {sft_args.output_dir} ' \
-                  f'--logging_dir {sft_args.logging_dir}'
+                  f'--logging_dir {sft_args.logging_dir} '
         for key, param in more_params.items():
             params += f'--{key} "{param}" '
         ddp_param = ''
