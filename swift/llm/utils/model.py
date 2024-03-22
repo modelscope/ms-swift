9965488484e505766dee044a30978333b77d16c5
jinghan <jinghan@U-Y092T109-2224.local>
2024-03-22 11:24:26 +0800

    fix

--- swift/llm/utils/model.py
+++ swift/llm/utils/model.py
@@ -2670,7 +2670,7 @@ def get_model_tokenizer_llava(model_dir: str,
     LoRATM.llama2,
     TemplateType.default_generation,
     torch_dtype=torch.bfloat16,
-    support_flash_attn=True,
+    # support_flash_attn=True,
     support_vllm=True)
 @register_model(
     ModelType.telechat_7b_int4,
