dc3fedffe7082dd812e536c464b82a1031a07d8e
jinghan <jinghan@U-Y092T109-2224.local>
2024-03-24 00:41:51 +0800

    fix

--- swift/llm/utils/template.py
+++ swift/llm/utils/template.py
@@ -1195,7 +1195,7 @@ register_template(TemplateType.gemma, gemma_template)
 
 register_template(
     TemplateType.telechat,
-    Template(['<_start>'], ['<_user>{{QUERY}}<_bot>\n'], None, ['<_end>']))
+    Template(['<_start>'], ['<_user>{{QUERY}}<_bot>\n'], None, ['</s>']))
 
 
 def get_template(
