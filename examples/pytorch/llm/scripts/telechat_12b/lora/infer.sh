3ed3149d945c435c50a8215f8472415a97f685ce
jinghan <jinghan@U-Y092T109-2224.local>
2024-03-24 22:30:01 +0800

    update

--- examples/pytorch/llm/scripts/telechat_12b/lora/infer.sh
+++ examples/pytorch/llm/scripts/telechat_12b/lora/infer.sh
@@ -2,7 +2,7 @@
 PYTHONPATH=../../.. \
 CUDA_VISIBLE_DEVICES=0,1 \
 python llm_infer.py \
-    --ckpt_dir "output/telechat-12b/vx-xxx/checkpoint-xxx" \
+    --ckpt_dir "/mnt/workspace/hujinghan.hjh/hjh/swift/examples/pytorch/llm/output/telechat-12b/v3-20240322-172202/checkpoint-2498" \
     --load_dataset_config true \
     --max_length 2048 \
     --use_flash_attn true \
