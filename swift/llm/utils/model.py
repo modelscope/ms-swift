249b236ffa29f1d16318906b2fca7c4cd7f92608
jinghan <jinghan@U-Y092T109-2224.local>
2024-03-28 11:07:51 +0800

    fix

--- swift/llm/utils/model.py
+++ swift/llm/utils/model.py
@@ -2349,6 +2349,12 @@ def get_model_tokenizer_codellama(model_dir: str,
     support_vllm=True,
     support_gradient_checkpointing=False,
     tags=['coding'])
+@register_model(
+    ModelType.telechat_12b,
+    'TeleAI/TeleChat-12B',
+    LoRATM.telechat,
+    TemplateType.telechat,
+    support_flash_attn=True)
 def get_model_tokenizer_phi(model_dir: str,
                             torch_dtype: Dtype,
                             model_kwargs: Dict[str, Any],
@@ -2668,28 +2674,6 @@ def get_model_tokenizer_llava(model_dir: str,
     _patch_llava(model)
     return model, tokenizer
 
-@register_model(
-    ModelType.telechat_12b,
-    'TeleAI/TeleChat-12B',
-    LoRATM.telechat,
-    TemplateType.telechat,
-    support_flash_attn=True)
-def get_model_tokenizer_telechat(model_dir: str,
-                            torch_dtype: Dtype,
-                            model_kwargs: Dict[str, Any],
-                            load_model: bool = True,
-                            **kwargs):
-    model_config = AutoConfig.from_pretrained(
-        model_dir, trust_remote_code=True)
-    use_flash_attn = kwargs.pop('use_flash_attn', False)
-    model_config.flash_attn = use_flash_attn
-    return get_model_tokenizer_from_repo(
-        model_dir,
-        torch_dtype,
-        model_kwargs,
-        load_model,
-        model_config=model_config,
-        **kwargs)
 
 def fix_transformers_upgrade(module: PreTrainedModel) -> None:
     # from 4.35, transformers changes its arguments of _set_gradient_checkpointing
