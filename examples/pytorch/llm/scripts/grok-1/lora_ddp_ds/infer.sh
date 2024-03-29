b2e34fb7142854226e89c925731d570e1bbb0a5f
yuze.zyz <yuze.zyz@alibaba-inc.com>
2024-03-29 16:51:09 +0800

    align scripts

--- examples/pytorch/llm/scripts/grok-1/lora_ddp_ds/infer.sh
+++ examples/pytorch/llm/scripts/grok-1/lora_ddp_ds/infer.sh
@@ -2,8 +2,8 @@
 # Memory cost: 8 * 80G
 PYTHONPATH=../../.. \
 python llm_infer.py \
-	--ckpt_dir output/grok-1/vxx-xxxx-xxxx/checkpoint-xxx \
-	--dtype bf16 \
+    --ckpt_dir output/grok-1/vxx-xxxx-xxxx/checkpoint-xxx \
+    --dtype bf16 \
     --load_dataset_config true \
     --max_new_tokens 64 \
     --do_sample true \
