536f508d1bc852ce045397eb3b3e36fa75e36e87
jinghan <jinghan@U-Y092T109-2224.local>
2024-03-31 22:39:32 +0800

    lora module & scripts

--- examples/pytorch/llm/scripts/dbrx-instruct/lora_mp/sft.sh
+++ examples/pytorch/llm/scripts/dbrx-instruct/lora_mp/sft.sh
@@ -1,32 +1,30 @@
 # Experimental environment: 4 * A100
-# 4 * 55GB GPU memory
-nproc_per_node=4
-
+# 4 * 74GB GPU memory
 CUDA_VISIBLE_DEVICES=0,1,2,3 \
-NPROC_PER_NODE=$nproc_per_node \
-MASTER_PORT=29500 \
 swift sft \
-    --model_id_or_path dbrx-instruct \
+    --model_type dbrx-instruct \
     --model_revision master \
     --sft_type lora \
     --tuner_backend swift \
     --template_type qwen \
-    --dtype AUTO \
+    --dtype bf16 \
     --output_dir output \
     --ddp_backend nccl \
     --dataset blossom-math-zh \
     --train_dataset_sample -1 \
-    --num_train_epochs 5 \
-    --max_length 2048 \
+    --num_train_epochs 1 \
+    --max_length 1024 \
     --check_dataset_strategy warning \
     --lora_rank 8 \
     --lora_alpha 32 \
     --lora_dropout_p 0.05 \
     --lora_target_modules ALL \
+    --lora_dtype bf16 \
+    --gradient_checkpointing false \
     --batch_size 1 \
     --weight_decay 0.1 \
     --learning_rate 1e-4 \
-    --gradient_accumulation_steps $(expr 16 / $nproc_per_node) \
+    --gradient_accumulation_steps 16 \
     --max_grad_norm 0.5 \
     --warmup_ratio 0.03 \
     --eval_steps 100 \
@@ -34,4 +32,3 @@ swift sft \
     --save_total_limit 2 \
     --logging_steps 10 \
     --use_flash_attn true \
-    --deepspeed default-zero3 \
