b1b40f2eb75371959f8319c1485bdb587c756560
yuze.zyz <yuze.zyz@alibaba-inc.com>
2024-04-10 15:24:08 +0800

    fix

--- examples/pytorch/llm/scripts/qwen_7b_chat/lora/eval.sh
+++ examples/pytorch/llm/scripts/qwen_7b_chat/lora/eval.sh
@@ -3,4 +3,5 @@ swift eval \
     --ckpt_dir "output/qwen-7b-chat/vx-xxx/checkpoint-xxx" \
     --eval_dataset arc \
     --eval_limit 10 \
+    --infer_backend pt \
     --custom_eval_config eval_example/custom_config.json
