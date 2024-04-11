5349662e1a09f8e02d938fabfff291b85227e5fd
jinghan <jinghan@U-Y092T109-2224.local>
2024-04-11 15:19:01 +0800

    fix

--- examples/pytorch/llm/scripts/c4ai_command_r_plus/lora_mp/infer.sh
+++ examples/pytorch/llm/scripts/c4ai_command_r_plus/lora_mp/infer.sh
@@ -3,6 +3,7 @@
 CUDA_VISIBLE_DEVICES=0,1,2,3 \
 swift infer \
     --ckpt_dir "output/c4ai-command-r-plus/vx-xxx/checkpoint-xx" \
+    --load_dataset_config true \
     --load_args_from_ckpt_dir true \
     --temperature 0.3 \
     --top_p 0.7 \
--- swift/llm/utils/model.py
+++ swift/llm/utils/model.py
@@ -455,22 +455,22 @@ def register_model(
     TemplateType.mengzi,
     support_vllm=True,
     support_flash_attn=True)
-# @register_model(
-#     ModelType.c4ai_command_r_v01,
-#     'AI-ModelScope/c4ai-command-r-v01',
-#     LoRATM.llama2,
-#     TemplateType.c4ai,
-#     requires=['transformers>=4.39.1'],
-#     support_vllm=True,
-#     support_flash_attn=True)
-# @register_model(
-#     ModelType.c4ai_command_r_plus,
-#     'AI-ModelScope/c4ai-command-r-plus',
-#     LoRATM.llama2,
-#     TemplateType.c4ai,
-#     requires=['transformers>4.39'],
-#     support_vllm=True,
-#     support_flash_attn=True)
+@register_model(
+    ModelType.c4ai_command_r_v01,
+    'AI-ModelScope/c4ai-command-r-v01',
+    LoRATM.llama2,
+    TemplateType.c4ai,
+    requires=['transformers>=4.39.1'],
+    support_vllm=False,
+    support_flash_attn=True)
+@register_model(
+    ModelType.c4ai_command_r_plus,
+    'AI-ModelScope/c4ai-command-r-plus',
+    LoRATM.llama2,
+    TemplateType.c4ai,
+    requires=['transformers>4.39'],
+    support_vllm=False,
+    support_flash_attn=True)
 def get_model_tokenizer_from_repo(model_dir: str,
                                   torch_dtype: Optional[Dtype],
                                   model_kwargs: Dict[str, Any],
@@ -500,53 +500,6 @@ def get_model_tokenizer_from_repo(model_dir: str,
                 config=model_config,
                 torch_dtype=torch_dtype,
                 trust_remote_code=True,
-                **model_kwargs)
-    return model, tokenizer
-
-
-@register_model(
-    ModelType.c4ai_command_r_v01,
-    'AI-ModelScope/c4ai-command-r-v01',
-    LoRATM.llama2,
-    TemplateType.c4ai,
-    requires=['transformers>=4.39.1'],
-    support_vllm=False,
-    support_flash_attn=True)
-@register_model(
-    ModelType.c4ai_command_r_plus,
-    'AI-ModelScope/c4ai-command-r-plus',
-    LoRATM.llama2,
-    TemplateType.c4ai,
-    requires=['transformers>4.39'],
-    support_vllm=False,
-    support_flash_attn=True)
-def get_model_tokenizer_c4ai(model_dir: str,
-                             torch_dtype: Optional[Dtype],
-                             model_kwargs: Dict[str, Any],
-                             load_model: bool = True,
-                             model_config=None,
-                             tokenizer=None,
-                             automodel_class=AutoModelForCausalLM,
-                             **kwargs):
-    if model_config is None:
-        model_config = AutoConfig.from_pretrained(
-            model_dir, trust_remote_code=True)
-    if torch_dtype is not None:
-        model_config.torch_dtype = torch_dtype
-    if tokenizer is None:
-        tokenizer = AutoTokenizer.from_pretrained(
-            model_dir, trust_remote_code=True, use_fast=False)
-    eos_token = kwargs.get('eos_token')
-    if eos_token is not None:
-        tokenizer.eos_token = eos_token
-    model = None
-    context = kwargs.get('context', nullcontext())
-    if load_model:
-        with context:
-            model = automodel_class.from_pretrained(
-                model_dir,
-                config=model_config,
-                torch_dtype=torch_dtype,
                 **model_kwargs)
     return model, tokenizer
 
