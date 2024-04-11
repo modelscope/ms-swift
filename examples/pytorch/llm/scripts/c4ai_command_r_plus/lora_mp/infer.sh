377443a1918026f48f6a6026a2b1505411f46c4d
jinghan <jinghan@U-Y092T109-2224.local>
2024-04-11 14:54:58 +0800

    fix script

--- examples/pytorch/llm/scripts/c4ai_command_r_plus/lora_mp/infer.sh
+++ examples/pytorch/llm/scripts/c4ai_command_r_plus/lora_mp/infer.sh
@@ -4,11 +4,8 @@ CUDA_VISIBLE_DEVICES=0,1,2,3 \
 swift infer \
     --ckpt_dir "output/c4ai-command-r-plus/vx-xxx/checkpoint-xx" \
     --load_args_from_ckpt_dir true \
-    --eval_human true \
     --temperature 0.3 \
     --top_p 0.7 \
     --repetition_penalty 1. \
     --do_sample true \
     --merge_lora false \
-    --load_dataset_config true \
-    --eval_human false \
