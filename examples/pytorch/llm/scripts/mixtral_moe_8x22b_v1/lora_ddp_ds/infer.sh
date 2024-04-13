1b50302c3b35212ed6b0591dde70196425fc6330
jinghan <jinghan@U-Y092T109-2224.local>
2024-04-13 18:07:53 +0800

    readme

--- examples/pytorch/llm/scripts/mixtral_moe_8x22b_v1/lora_ddp_ds/infer.sh
+++ examples/pytorch/llm/scripts/mixtral_moe_8x22b_v1/lora_ddp_ds/infer.sh
@@ -1,9 +1,8 @@
 # Experimental environment: A100
-# 2 * 45GB GPU memory
 PYTHONPATH=../../.. \
 CUDA_VISIBLE_DEVICES=0,1,2,3 \
 python llm_infer.py \
-    --ckpt_dir "output/mixtral-moe-7b/vx-xxx/checkpoint-xxx" \
+    --ckpt_dir "output/mixtral-8x22b-v0.1/vx-xxx/checkpoint-xxx" \
     --load_dataset_config true \
     --use_flash_attn true \
     --max_new_tokens 2048 \
