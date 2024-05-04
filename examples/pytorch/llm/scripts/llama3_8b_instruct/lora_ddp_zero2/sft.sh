09c2df18042411f0b15aaf6eaf89e8306ad96ee4
tastelikefeet <yuze.zyz@alibaba-inc.com>
2024-05-04 20:58:52 +0800

    add temp file

--- examples/pytorch/llm/scripts/llama3_8b_instruct/lora_ddp_zero2/sft.sh
+++ examples/pytorch/llm/scripts/llama3_8b_instruct/lora_ddp_zero2/sft.sh
@@ -14,10 +14,10 @@ swift sft \
     --dtype AUTO \
     --output_dir output \
     --ddp_backend nccl \
-    --dataset blossom-math-zh \
+    --dataset ms-agent \
     --train_dataset_sample -1 \
-    --num_train_epochs 5 \
-    --max_length 2048 \
+    --num_train_epochs 1 \
+    --max_length 4096 \
     --check_dataset_strategy warning \
     --lora_rank 8 \
     --lora_alpha 32 \
@@ -27,12 +27,13 @@ swift sft \
     --batch_size 1 \
     --weight_decay 0.1 \
     --learning_rate 1e-4 \
-    --gradient_accumulation_steps $(expr 16 / $nproc_per_node) \
+    --gradient_accumulation_steps 1 \
     --max_grad_norm 0.5 \
     --warmup_ratio 0.03 \
-    --eval_steps 100 \
-    --save_steps 100 \
+    --eval_steps 10000 \
+    --save_steps 10000 \
     --save_total_limit 2 \
     --logging_steps 10 \
-    --deepspeed default-zero2 \
     --save_only_model true \
+    --sequence_parallel_size 2 \
+    --pack_to_max_length false \
