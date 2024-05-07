b98aa5adec1da032976baf68e73b9d4e3e0922c6
jinghan <jinghan@U-Y092T109-2224.local>
2024-05-07 17:13:15 +0800

    fix

--- swift/llm/utils/model.py
+++ swift/llm/utils/model.py
@@ -2405,10 +2405,10 @@ def get_model_tokenizer_deepseek2(model_dir: str,
     model_config = AutoConfig.from_pretrained(model_dir, trust_remote_code=True)
     use_flash_attn = kwargs.pop('use_flash_attn', False)
     model_config._attn_implementation = 'flash_attention_2' if use_flash_attn else 'eager'
-    model.generation_config.pad_token_id = model.generation_config.eos_token_id
 
     model, tokenizer = get_model_tokenizer_from_repo(
         model_dir, torch_dtype, model_kwargs, load_model, model_config=model_config, **kwargs)
+    model.generation_config.pad_token_id = model.generation_config.eos_token_id
     tokenizer.eos_token = '<｜end▁of▁sentence｜>'
     tokenizer.bos_token = '<｜begin▁of▁sentence｜>'
     return model, tokenizer
