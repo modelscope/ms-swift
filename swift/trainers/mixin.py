d91d8098358514044967c919f26ee738a1bed4ec
huangjintao <huangjintao@163.com>
2024-06-11 14:54:46 +0800

    fix npu

--- swift/trainers/mixin.py
+++ swift/trainers/mixin.py
@@ -29,7 +29,6 @@ from transformers.trainer_utils import EvalPrediction
 from transformers.training_args import TrainingArguments
 from transformers.utils import is_torch_npu_available
 
-
 from swift.hub import Repository
 from swift.hub.check_model import check_local_model_is_latest
 from swift.torchacc_utils import (save_ta_ddp_checkpoint, save_ta_fsdp_checkpoint, ta_load_optimizer_and_scheduler,
