2f0765620b33ffcdb8f01bfd2765062d5bd2e7c5
huangjintao <huangjintao@163.com>
2024-06-12 21:27:38 +0800

    update

--- swift/llm/utils/template.py
+++ swift/llm/utils/template.py
@@ -107,19 +107,26 @@ Context = Union[str, List[int]]
 class StopWordsCriteria(StoppingCriteria):
     # The returned sentence includes stop words.
     def __init__(self, tokenizer: PreTrainedTokenizerBase, stop_words: StopWords, **tokenizer_kwargs) -> None:
-        self.stop_words: List[List[int]] = []
-        for stop_word in stop_words:
-            if isinstance(stop_word, str):
-                stop_word = tokenizer.encode(stop_word, add_special_tokens=False, **tokenizer_kwargs)
-            self.stop_words.append(stop_word)
+        self.tokenizer = tokenizer
+        self.stop_words = stop_words
+        self.tokenizer_kwargs = tokenizer_kwargs
         self.start_idx = -1
 
     def __call__(self, input_ids: Tensor, scores: Tensor) -> bool:
         if self.start_idx == -1:
             self.start_idx = len(input_ids[0]) - 1
-        for stop_word in self.stop_words:
-            if len(stop_word) > 0 and input_ids[0].tolist()[-len(stop_word):] == stop_word:
-                return True
+        tokenizer = self.tokenizer
+        stop_words = self.stop_words
+        # [-20:]: Assuming the end tokens do not exceed 20 tokens, 
+        #   to avoid input_ids being too long and affecting efficiency.
+        text = tokenizer.decode(input_ids[0, self.start_idx:][-20:], **self.tokenizer_kwargs)
+        for stop_word in stop_words:
+            if isinstance(stop_word, str):
+                if stop_word in text:
+                    return True
+            else:  # list
+                if len(stop_word) > 0 and input_ids[0].tolist()[-len(stop_word):] == stop_word:
+                    return True
         return False
 
 
