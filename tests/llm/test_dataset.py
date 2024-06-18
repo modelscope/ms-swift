b5493dfdc0b3433b6dacafd66988540f51961fd7
tastelikefeet <yuze.zyz@alibaba-inc.com>
2024-06-18 13:42:06 +0800

    fix

--- tests/llm/test_dataset.py
+++ tests/llm/test_dataset.py
@@ -8,25 +8,14 @@ from swift.llm import DatasetName, get_dataset
 
 class TestDataset(unittest.TestCase):
 
-    @unittest.skip('fix citest')
-    def test_dataset(self):
-        train_dataset, val_dataset = get_dataset([DatasetName.leetcode_python_en, DatasetName.blossom_math_zh])
-        assert isinstance(train_dataset, HfDataset) and val_dataset is None
-        totol_len = 12359
-        assert len(train_dataset) == totol_len
-
-        train_dataset, val_dataset = get_dataset([DatasetName.leetcode_python_en, DatasetName.blossom_math_zh], 0.01)
-        assert isinstance(train_dataset, HfDataset) and isinstance(train_dataset, HfDataset)
-        assert len(train_dataset) + len(val_dataset) == totol_len
-
     def test_load_v_dataset(self):
         if not __name__ == '__main__':
             # ignore citest error in github
             return
 
-        for ds in ['m3it', 'mantis_instruct', 'llava-med-zh-instruct']:
+        for ds in ['m3it#1000', 'mantis-instruct#1000', 'llava-med-zh-instruct#1000']:
             ds = get_dataset(ds)
-            assert len(ds)
+            assert len(ds[0]) > 800
 
 
 if __name__ == '__main__':
