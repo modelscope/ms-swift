632fd07fc791c625eee8b8a94d2c7ebc6967b8c1
wangang.wa <wangang.wa@alibaba-inc.com>
2024-06-18 11:34:24 +0800

    cache the compiled results and remove some xla flags

--- examples/pytorch/llm/scripts/torchacc/chatglm3_6b/acc_lora_dp_sft.sh
+++ examples/pytorch/llm/scripts/torchacc/chatglm3_6b/acc_lora_dp_sft.sh
@@ -3,10 +3,12 @@
 # Note: TorchAcc is currently only available internally.
 # torchacc dp
 export USE_TORCHACC=1
-export XLA_FLAGS='--xla_gpu_force_compilation_parallelism=32 --xla_multiheap_size_constraint_per_heap=4831838208 --xla_disable_hlo_passes=all-gather-combiner,all-reduce-combiner,reduce-scatter-combiner,gpu-convert-async-collectives-to-sync,rematerialization'
 export XLA_IR_SHAPE_CACHE_SIZE=100000000
 export XLA_ALLOCATOR_FRACTION=0.95
 export XLA_EXPERIMENTAL=nonzero:masked_select
+
+export XLA_PERSISTENT_CACHE_PATH=./output/compiled_cache/chatglm3-6b
+mkdir -p $XLA_PERSISTENT_CACHE_PATH
 
 
 NPROC_PER_NODE=2 \
