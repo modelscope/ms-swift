4433e2e5f01cc65a6f13c698df346f07cdb5f951
hjh0119 <hjhcs@zju.edu.cn>
2024-06-19 20:11:54 +0800

    update

--- swift/llm/utils/argument.py
+++ swift/llm/utils/argument.py
@@ -1425,7 +1425,6 @@ class RLHFArguments(SftArguments):
         self.set_default_beta()
         self.set_default_loss_type()
         self.set_default_config()
-        self.set_default_loss_type()
         self.check_loss_type()
 
     def set_default_beta(self):
