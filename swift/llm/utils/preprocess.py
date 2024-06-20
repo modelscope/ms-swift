e20262c3b6eea4cfd8f5689267da0108c5c7f0d2
yuze.zyz <yuze.zyz@alibaba-inc.com>
2024-06-20 22:26:24 +0800

    revert code

--- swift/llm/utils/preprocess.py
+++ swift/llm/utils/preprocess.py
@@ -101,7 +101,7 @@ class AlpacaPreprocessor(MediaMixin, RowPreprocessMixin):
         inp: Optional[str] = d.get('input', None)
         h, output = d.pop('history', None), d['output']
         sys = d.pop('system', None)
-        tool = d.pop('tools')
+        tool = d.pop('tools', None)
         if output is None:
             return self.empty_row
         if inp is None or len(inp) == 0:
