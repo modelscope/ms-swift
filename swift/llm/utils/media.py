5247dcdac9631d2389eff2c7a78cd136600af93c
huangjintao <huangjintao@163.com>
2024-06-24 10:28:46 +0800

    refactor template

--- swift/llm/utils/media.py
+++ swift/llm/utils/media.py
@@ -1,7 +1,7 @@
 import os
 import shutil
 import time
-from typing import List, Literal, Optional, Union, Any, Dict
+from typing import Any, Dict, List, Literal, Optional, Union
 
 import numpy as np
 
