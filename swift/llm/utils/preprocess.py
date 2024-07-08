462696ee2151304d7bad1f95f7d4de7bfb606f0b
yuze.zyz <yuze.zyz@alibaba-inc.com>
2024-07-08 14:35:51 +0800

    fix ui, and support DATASET_ENABLE_CACHE variable

--- swift/llm/utils/preprocess.py
+++ swift/llm/utils/preprocess.py
@@ -1,14 +1,17 @@
 # Copyright (c) Alibaba, Inc. and its affiliates.
 import ast
+import os
 from typing import Any, Callable, Dict, List, Literal, Optional, Union
 
 from datasets import Dataset as HfDataset
 from tqdm import tqdm
+from transformers.utils import strtobool
 
 from .media import MediaTag
 from .template import History
 
 PreprocessFunc = Callable[[HfDataset], HfDataset]
+dataset_enable_cache = strtobool(os.environ.get('DATASET_ENABLE_CACHE', 'False'))
 
 
 def _reduce_columns(cls: type) -> type:
@@ -163,7 +166,8 @@ class AlpacaPreprocessor(MediaMixin, RowPreprocessMixin):
 
     def __call__(self, dataset: HfDataset) -> HfDataset:
         dataset = dataset.map(
-            self.preprocess, load_from_cache_file=False).filter(lambda row: row.get('response') is not None)
+            self.preprocess,
+            load_from_cache_file=dataset_enable_cache).filter(lambda row: row.get('response') is not None)
         if self.media_type and isinstance(self.media_key, str) and self.media_key != self.media_name:
             dataset = dataset.rename_columns({self.media_key: self.media_name})
         return dataset
@@ -257,7 +261,8 @@ class ConversationsPreprocessor(MediaMixin, RowPreprocessMixin):
 
     def __call__(self, dataset: HfDataset) -> HfDataset:
         dataset = dataset.map(
-            self.preprocess, load_from_cache_file=False).filter(lambda row: row.get('response') is not None)
+            self.preprocess,
+            load_from_cache_file=dataset_enable_cache).filter(lambda row: row.get('response') is not None)
         if self.media_type and isinstance(self.media_key, str) and self.media_key != self.media_name:
             dataset = dataset.rename_columns({self.media_key: self.media_name})
         return dataset
@@ -310,7 +315,8 @@ class ListPreprocessor(MediaMixin, RowPreprocessMixin):
         return row
 
     def __call__(self, dataset: HfDataset):
-        dataset = dataset.map(self.preprocess, load_from_cache_file=False).filter(lambda d: d.get('response'))
+        dataset = dataset.map(
+            self.preprocess, load_from_cache_file=dataset_enable_cache).filter(lambda d: d.get('response'))
         if self.media_type and isinstance(self.media_key, str) and self.media_key != self.media_name:
             dataset = dataset.rename_columns({self.media_key: self.media_name})
         return dataset
