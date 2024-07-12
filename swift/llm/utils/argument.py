dc4c39489b06554d499775d747ab16294899b941
tastelikefeet <yuze.zyz@alibaba-inc.com>
2024-07-13 00:15:28 +0800

    fix

--- swift/llm/utils/argument.py
+++ swift/llm/utils/argument.py
@@ -635,7 +635,7 @@ class SftArguments(ArgumentsBase):
     custom_dataset_info: Optional[str] = None  # .json
 
     device_map_config_path: Optional[str] = None
-    device_max_memory: Optional[List] = None
+    device_max_memory: List[str] = field(default_factory=list)
 
     # generation config
     max_new_tokens: int = 2048
