28aa70ec9240797dc2efcff8e89a0512a2cdc964
yuze.zyz <yuze.zyz@alibaba-inc.com>
2024-07-13 00:57:18 +0800

    lint

--- swift/llm/sft.py
+++ swift/llm/sft.py
@@ -63,7 +63,10 @@ def llm_sft(args: SftArguments) -> Dict[str, Union[str, Any]]:
     if args.device_max_memory:
         n_gpu = torch.cuda.device_count()
         assert len(args.device_max_memory) == n_gpu / local_world_size
-        model_kwargs['max_memory'] = {i: mem for i, mem in zip(list(range(local_rank, n_gpu, local_world_size)), args.device_max_memory)}
+        model_kwargs['max_memory'] = {
+            i: mem
+            for i, mem in zip(list(range(local_rank, n_gpu, local_world_size)), args.device_max_memory)
+        }
 
     if args.quant_method == 'hqq':
         from transformers import HqqConfig
