b16b508d560d464748357683f5b9cc483d1fd40e
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2024-07-17 13:45:16 +0800

    update

--- swift/llm/utils/utils.py
+++ swift/llm/utils/utils.py
@@ -938,8 +938,6 @@ def is_lmdeploy_available():
 def is_xtuner_available():
     return importlib.util.find_spec('xtuner') is not None
 
-def is_megatron_available():
-    return importlib.util.find_spec('megatron') is not None
 
 def is_megatron_available():
     return importlib.util.find_spec('megatron') is not None
