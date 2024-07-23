a426a82fb65c67764fb20b0ad9e1cd2f4077520e
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2024-07-24 02:15:13 +0800

    update

--- swift/llm/megatron/convert.py
+++ swift/llm/megatron/convert.py
@@ -5,9 +5,42 @@ from typing import Any, Dict, Optional
 
 import torch
 
-from .model import model_provider
-
 _convert_mapping = {'qwen2': 'qwen'}
+
+
+def model_provider(pre_process=True, post_process=True):
+    from megatron.training import get_args
+    from megatron.training.arguments import core_transformer_config_from_args
+    from megatron_patch.model.qwen2.transformer_config import Qwen2TransformerConfig
+    from megatron_patch.model.qwen2.layer_specs import (get_gpt_layer_local_spec,
+                                                        get_gpt_layer_with_transformer_engine_spec)
+    from megatron_patch.model.qwen2.model import GPTModel
+
+    args = get_args()
+    config = core_transformer_config_from_args(args, Qwen2TransformerConfig)
+    use_te = args.transformer_impl == 'transformer_engine'
+
+    if use_te:
+        transformer_layer_spec = get_gpt_layer_with_transformer_engine_spec(args.num_experts, args.moe_grouped_gemm,
+                                                                            args.qk_layernorm)
+    else:
+        transformer_layer_spec = get_gpt_layer_local_spec(args.num_experts, args.moe_grouped_gemm, args.qk_layernorm)
+
+    model = GPTModel(
+        config=config,
+        transformer_layer_spec=transformer_layer_spec,
+        vocab_size=args.padded_vocab_size,
+        max_sequence_length=args.max_position_embeddings,
+        pre_process=pre_process,
+        post_process=post_process,
+        fp16_lm_cross_entropy=args.fp16_lm_cross_entropy,
+        parallel_output=True,
+        share_embeddings_and_output_weights=not args.untie_embeddings_and_output_weights,
+        position_embedding_type=args.position_embedding_type,
+        rotary_percent=args.rotary_percent,
+        rotary_base=args.rotary_base,
+        seq_len_interpolation_factor=args.rotary_seq_len_interpolation_factor)
+    return model
 
 
 def convert_hf_to_megatron(
--- swift/llm/sft.py
+++ swift/llm/sft.py
@@ -1,6 +1,5 @@
 # Copyright (c) Alibaba, Inc. and its affiliates.
 import os
-import sys
 from functools import partial
 from typing import Any, Dict, Optional, Tuple
 
@@ -15,10 +14,9 @@ from transformers.utils import is_torch_npu_available
 from swift.torchacc_utils import patch_acc_model
 from swift.trainers import Seq2SeqTrainer
 from swift.trainers.utils import can_return_loss, find_labels
-from swift.utils import (append_to_jsonl, check_json_format, compute_acc_metrics, compute_nlg_metrics, get_dist_setting,
-                         get_logger, get_main, get_model_info, is_ddp_plus_mp, is_dist, is_local_master, is_master,
-                         plot_images, preprocess_logits_for_metrics, safe_ddp_context, seed_everything, show_layers,
-                         subprocess_run, use_torchacc)
+from swift.utils import (append_to_jsonl, check_json_format, compute_acc_metrics, compute_nlg_metrics, get_logger,
+                         get_main, get_model_info, is_ddp_plus_mp, is_dist, is_master, plot_images,
+                         preprocess_logits_for_metrics, seed_everything, show_layers, subprocess_run, use_torchacc)
 from .accelerator import ta_accelerate
 from .tuner import prepare_model
 from .utils import (LazyLLMDataset, SftArguments, Template, dataset_map, get_dataset, get_model_tokenizer, get_template,
@@ -57,7 +55,6 @@ def llm_sft_megatron(args: SftArguments) -> Dict[str, Any]:
         f'Please run `CUDA_VISIBLE_DEVICES=0 swift export --model_type {args.model_type} --tp {args.tp} --pp {args.pp} '
         f'--megatron_output_dir {args.resume_from_checkpoint} --to_megatron true` '
         'to convert the weights to Megatron format.')
-    from swift.llm import get_model_tokenizer
     from swift.llm.megatron import (MegatronArguments, get_model_seires, patch_megatron, model_provider, forward_step,
                                     train_valid_test_datasets_provider as _train_valid_test_datasets_provider)
     from megatron.core.enums import ModelType
