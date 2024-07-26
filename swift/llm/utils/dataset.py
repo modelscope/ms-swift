52203358ccb0ea56abfb1256fc561e5d0b7af90c
hjh0119 <hjhcs@zju.edu.cn>
2024-07-26 22:06:20 +0800

    fix

--- swift/llm/utils/dataset.py
+++ swift/llm/utils/dataset.py
@@ -49,7 +49,7 @@ def _remove_useless_columns(dataset: HfDataset) -> HfDataset:
     for k in dataset.features.keys():
         if k in {
                 'query', 'query_role', 'response', 'rejected_response', 'system', 'history', 'history_roles', 'images',
-                'objects', 'videos', 'audios', 'tools'
+                'objects', 'videos', 'audios', 'tools', 'label'
         }:
             k_list.append(k)
     dataset = dataset.select_columns(k_list)
