cdc2c80f2e62ac13065f8615d34bdcd22b22a4ac
Jintao <huangjintao.hjt@alibaba-inc.com>
2024-07-28 16:01:48 +0800

    fix pretrain dataset (#1518)

--- swift/llm/utils/template.py
+++ swift/llm/utils/template.py
@@ -242,6 +242,7 @@ class Template:
                        model=None,
                        **kwargs) -> None:
         assert self._is_init is False, 'The template has been initialized.'
+        self.is_multimodal = getattr(tokenizer, 'is_multimodal', None)
         self._is_init = True
         self.tokenizer = tokenizer
         # if default_system is None. not change self.default_system
@@ -348,7 +349,7 @@ class Template:
 
         # Parse <img></img> format images and merged into images key
         images_path = None
-        if example.get('query'):
+        if self.is_multimodal in {True, None}:  # If False, do not perform replace_img_tag
             example['query'], example['history'], images_path = replace_img_tag(
                 example.get('query'), history, self.image_placeholder)
         if images_path:
