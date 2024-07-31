93aa43c98e721bc13e9d8418b7bf15eb3a5f2d1c
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2024-07-31 17:23:49 +0800

    support quant_policy

--- swift/llm/utils/argument.py
+++ swift/llm/utils/argument.py
@@ -1243,6 +1243,7 @@ class InferArguments(ArgumentsBase):
     # lmdeploy
     tp: int = 1
     cache_max_entry_count: float = 0.8
+    quant_policy: int = 0  # e.g. 4, 8
     vision_batch_size: int = 1  # max_batch_size in VisionConfig
 
     # compatibility. (Deprecated)
