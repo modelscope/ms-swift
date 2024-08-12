c2005e5a444a021e3ccb99b1f215c657bf153788
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2024-08-12 15:42:22 +0800

    update

--- swift/llm/utils/template.py
+++ swift/llm/utils/template.py
@@ -19,8 +19,8 @@ from transformers.dynamic_module_utils import get_class_from_dynamic_module
 from swift.llm.agent.utils import calculate_loss_scale, get_tools_prompt
 from swift.torchacc_utils import pad_and_split_batch
 from swift.utils import get_dist_setting, get_logger, upper_bound, use_torchacc
-from .vision_utils import (load_audio_qwen, load_batch, load_image, transform_image, load_video_cogvlm2,
-                           load_video_internvl, load_video_llava, load_video_minicpmv, rescale_image)
+from .vision_utils import (load_audio_qwen, load_batch, load_image, load_video_cogvlm2, load_video_internvl,
+                           load_video_llava, load_video_minicpmv, rescale_image, transform_image)
 
 logger = get_logger()
 
@@ -599,7 +599,7 @@ class Template:
             return ['<audio>']
 
     def replace_tag_lmdeploy(self, media_type: Literal['image', 'video', 'audio'], index: int,
-                    example: Dict[str, Any]) -> List[Context]:
+                             example: Dict[str, Any]) -> List[Context]:
         if media_type == 'image':
             return [[-100]]
 
