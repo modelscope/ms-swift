1850898bb5d886ad01c39b63cffbee80cbde1ccc
DaozeZhang <zdz408@126.com>
2024-08-19 20:19:17 +0800

    update

--- swift/llm/utils/template.py
+++ swift/llm/utils/template.py
@@ -47,7 +47,6 @@ class TemplateType:
     baichuan = 'baichuan'
     chatglm2 = 'chatglm2'
     chatglm3 = 'chatglm3'
-    longwriter_glm4 = 'longwriter-glm4'
     codegeex4 = 'codegeex4'
     llama = 'llama'  # llama2
     llama3 = 'llama3'
@@ -1326,10 +1325,6 @@ register_template(
     TemplateType.chatglm3,
     GLMTemplate([], ['<|user|>\n{{QUERY}}<|assistant|>\n'], [], ['<|user|>'], None, ['<|system|>\n{{SYSTEM}}']))
 
-register_template(
-    TemplateType.longwriter_glm4,
-    GLMTemplate([], ['<|user|>\n{{QUERY}}<|assistant|>\n'], [], ['<|user|>'], None, ['<|system|>\n{{SYSTEM}}']))
-
 codegeex4_system = '你是一位智能编程助手，你叫CodeGeeX。你会为用户回答关于编程、代码、计算机方面的任何问题，并提供格式规范、可以执行、准确安全的代码，并在必要时提供详细的解释。'
 
 register_template(
@@ -1363,11 +1358,9 @@ register_template(
     Template(['<s>[INST] '], ['{{QUERY}} [/INST]'], ['</s><s>[INST] '], ['</s>'], LLAMA_DEFAULT_SYSTEM,
              ['<s>[INST] <<SYS>>\n{{SYSTEM}}\n<</SYS>>\n\n']))
 
-LONGWRITER_DEFAULT_SYSTEM = ('你是由智谱AI开发的长文本生成模型，可以在一分钟内生成超过10,000个字！')
-
 register_template(
     TemplateType.longwriter_llama3,
-    Template(['[INST]'], ['{{QUERY}}[/INST]'], ['[INST]'], [''], LONGWRITER_DEFAULT_SYSTEM,
+    Template(['[INST]'], ['{{QUERY}}[/INST]'], ['[INST]'], ['<|end_of_text|>'], None,
              ['<<SYS>>\n{{SYSTEM}}\n<</SYS>>\n\n']))
 
 register_template(TemplateType.mistral_nemo,
