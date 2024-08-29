d920794fc81a5416bc59f5309e2f9229e616cc4d
tastelikefeet <yuze.zyz@alibaba-inc.com>
2024-08-29 20:21:45 +0800

    fix

--- swift/trainers/mixin.py
+++ swift/trainers/mixin.py
@@ -334,7 +334,7 @@ class SwiftMixin:
         if isinstance(model, PeftModel):
             config = model.peft_config.get('default', {})
             init_lora_weights = getattr(config, 'init_lora_weights', '')
-            if 'pissa' in init_lora_weights or 'olora' in init_lora_weights:
+            if isinstance(init_lora_weights, str) and ('pissa' in init_lora_weights or 'olora' in init_lora_weights):
                 config.init_lora_weights = True
                 model.save_pretrained(os.path.join(output_dir, 'initial_model'))
                 config.init_lora_weights = init_lora_weights
@@ -344,7 +344,7 @@ class SwiftMixin:
         if isinstance(model, PeftModel):
             config = model.peft_config.get('default', {})
             init_lora_weights = getattr(config, 'init_lora_weights', '')
-            if 'pissa' in init_lora_weights or 'olora' in init_lora_weights:
+            if isinstance(init_lora_weights, str) and ('pissa' in init_lora_weights or 'olora' in init_lora_weights):
                 model.save_pretrained(
                     os.path.join(output_dir, 'converted'),
                     path_initial_model_for_weight_conversion=os.path.join(os.path.dirname(output_dir), 'initial_model'),
