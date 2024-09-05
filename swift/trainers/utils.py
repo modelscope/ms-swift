943c42ed59a533df14193e0969e723c3eceb029b
hjh0119 <hjhcs@zju.edu.cn>
2024-09-05 16:32:07 +0800

    remove padding for image and pixel value

--- swift/trainers/utils.py
+++ swift/trainers/utils.py
@@ -365,7 +365,7 @@ def patch_datacollator():
         def new_call(self, features: List[Dict[str, Any]]) -> Dict[str, Any]:
             padded_batch = {}
             for k in features[0].keys():
-                if k.endswith(('_input_ids', '_attention_mask', '_labels', '_pixel_values', '_images')):
+                if k.endswith(('_input_ids', '_attention_mask', '_labels')):
                     if self.is_encoder_decoder:
                         to_pad = [torch.LongTensor(ex[k]) for ex in features]
 
@@ -382,8 +382,6 @@ def patch_datacollator():
                         elif k.startswith(('chosen', 'rejected', 'completion')) or ('decoder' in k):
                             padding_value = self.label_pad_token_id
                         # patch here
-                        elif k.endswith('_pixel_values'):
-                            padding_value = 0
                         else:
                             raise ValueError(f"Unexpected key in batch '{k}'")
                         padded_batch[k] = pad_sequence(to_pad, batch_first=True, padding_value=padding_value)
@@ -400,8 +398,6 @@ def patch_datacollator():
                         elif k.endswith('_labels'):
                             padding_value = self.label_pad_token_id
                         elif k.endswith('_attention_mask'):
-                            padding_value = 0
-                        elif k.endswith(('_pixel_values', '_images')):
                             padding_value = 0
                         else:
                             raise ValueError(f"Unexpected key in batch '{k}'")
