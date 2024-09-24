c36b0b465ea9ca0acb2e0d1073a3cee12151a775
huangjintao.hjt <huangjintao.hjt@alibaba-inc.com>
2024-09-24 13:40:59 +0800

    fix

--- swift/llm/utils/vllm_utils.py
+++ swift/llm/utils/vllm_utils.py
@@ -304,14 +304,14 @@ def add_vllm_request(llm_engine: Union[LLMEngine, AsyncLLMEngine], inputs: Dict[
         if mm_data:
             llm_inputs['multi_modal_data'] = mm_data
         if llm_engine.__class__.__name__ == 'LLMEngine':
-            result_generator = llm_engine.generate(llm_inputs, generation_config, request_id, **kwargs)
-        else:
             result_generator = llm_engine.add_request(request_id, llm_inputs, generation_config, **kwargs)
+        else:
+            result_generator = llm_engine.generate(llm_inputs, generation_config, request_id, **kwargs)
     else:
         if llm_engine.__class__.__name__ == 'LLMEngine':
-            result_generator = llm_engine.generate(None, generation_config, request_id, input_ids, **kwargs)
-        else:
             result_generator = llm_engine.add_request(request_id, None, generation_config, input_ids, **kwargs)
+        else:
+            result_generator = llm_engine.generate(None, generation_config, request_id, input_ids, **kwargs)
     return result_generator
 
 
