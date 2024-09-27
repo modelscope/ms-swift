3cf3f9f3f6e39724e2a3d3da4fc14a19f884de16
tastelikefeet <58414341+tastelikefeet@users.noreply.github.com>
2024-09-27 11:24:21 +0800

    fix ui (#2140)

--- swift/ui/llm_train/llm_train.py
+++ swift/ui/llm_train/llm_train.py
@@ -371,11 +371,13 @@ class LLMTrain(BaseUI):
             })
         params = ''
 
+        sep = f'{cls.quote} {cls.quote}'
         for e in kwargs:
             if isinstance(kwargs[e], list):
-                params += f'--{e} {" ".join(kwargs[e])} '
+                params += f'--{e} {cls.quote}{sep.join(kwargs[e])}{cls.quote} '
             elif e in kwargs_is_list and kwargs_is_list[e]:
-                params += f'--{e} {kwargs[e]} '
+                all_args = [arg for arg in kwargs[e].split(' ') if arg.strip()]
+                params += f'--{e} {cls.quote}{sep.join(all_args)}{cls.quote} '
             else:
                 params += f'--{e} {cls.quote}{kwargs[e]}{cls.quote} '
         params += more_params_cmd + ' '
