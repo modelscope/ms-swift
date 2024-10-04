6fba2b7a0253d0e431c586da90f784f5210dd5bc
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2024-10-05 00:09:18 +0800

    update

--- swift/llm/argument/export_args.py
+++ swift/llm/argument/export_args.py
@@ -15,18 +15,19 @@ logger = get_logger()
 @dataclass
 class ExportArguments(InferArguments):
     to_peft_format: bool = False
-    to_ollama: bool = False
-    ollama_output_dir: Optional[str] = None
-    gguf_file: Optional[str] = None
-
     # awq: 4; gptq: 2, 3, 4, 8
     quant_bits: int = 0  # e.g. 4
     quant_method: Literal['awq', 'gptq', 'bnb'] = 'awq'
     quant_n_samples: int = 256
-    quant_seqlen: int = 2048
+    quant_seqlen: Optional[int] = None
     quant_device_map: Optional[str] = None  # e.g. 'cpu', 'auto'
     quant_output_dir: Optional[str] = None
     quant_batch_size: int = 1
+
+    # ollama
+    to_ollama: bool = False
+    ollama_output_dir: Optional[str] = None
+    gguf_file: Optional[str] = None
 
     # push to ms hub
     push_to_hub: bool = False
@@ -43,17 +44,13 @@ class ExportArguments(InferArguments):
     tp: int = 1
     pp: int = 1
 
-    # The parameter has been defined in InferArguments.
-    # merge_lora, hub_token
-
     def __post_init__(self):
         super().__post_init__()
         if self.quant_seqlen is None:
             self.quant_seqlen = self.max_length
         if self.quant_bits > 0:
             if len(self.dataset) == 0:
-                self.dataset = ['alpaca-zh#10000', 'alpaca-en#10000']
-                logger.info(f'Setting args.dataset: {self.dataset}')
+                raise ValueError(f'self.dataset: {self.dataset}, Please input the quant dataset.')
             if self.quant_output_dir is None:
                 if self.ckpt_dir is None:
                     self.quant_output_dir = f'{self.model_type}-{self.quant_method}-int{self.quant_bits}'
@@ -63,7 +60,8 @@ class ExportArguments(InferArguments):
                                                          f'{ckpt_name}-{self.quant_method}-int{self.quant_bits}')
                 self.quant_output_dir = self.check_path_validity(self.quant_output_dir)
                 logger.info(f'Setting args.quant_output_dir: {self.quant_output_dir}')
-            assert not os.path.exists(self.quant_output_dir), f'args.quant_output_dir: {self.quant_output_dir}'
+            assert not os.path.exists(
+                self.quant_output_dir), (f'args.quant_output_dir: {self.quant_output_dir} already exists.')
         elif self.to_ollama:
             assert self.train_type in ['full'] + adapters_can_be_merged()
             if self.train_type != 'full':
@@ -74,7 +72,7 @@ class ExportArguments(InferArguments):
             assert not os.path.exists(
                 self.ollama_output_dir), f'Please make sure your output dir does not exists: {self.ollama_output_dir}'
         elif self.to_megatron or self.to_hf:
-            self.quant_method = None
+            self.quant_method = None  # fix parameter conflict
             os.environ['RANK'] = '0'
             os.environ['LOCAL_RANK'] = '0'
             os.environ['WORLD_SIZE'] = '1'
@@ -83,16 +81,16 @@ class ExportArguments(InferArguments):
             os.environ['MASTER_PORT'] = os.environ.get('MASTER_PORT', '29500')
             assert is_dist(), 'Please start in distributed mode.'
             dist.init_process_group(backend='nccl')
-        if self.to_megatron:
-            if self.megatron_output_dir is None:
-                self.megatron_output_dir = f'{self.model_type}-tp{self.tp}-pp{self.pp}'
-            self.megatron_output_dir = self.check_path_validity(self.megatron_output_dir)
-            logger.info(f'Setting args.megatron_output_dir: {self.megatron_output_dir}')
-        if self.to_hf:
-            if self.hf_output_dir is None:
-                self.hf_output_dir = os.path.join(self.ckpt_dir, f'{self.model_type}-hf')
-            self.hf_output_dir = self.check_path_validity(self.hf_output_dir)
-            logger.info(f'Setting args.hf_output_dir: {self.hf_output_dir}')
+            if self.to_megatron:
+                if self.megatron_output_dir is None:
+                    self.megatron_output_dir = f'{self.model_type}-tp{self.tp}-pp{self.pp}'
+                self.megatron_output_dir = self.check_path_validity(self.megatron_output_dir)
+                logger.info(f'Setting args.megatron_output_dir: {self.megatron_output_dir}')
+            if self.to_hf:
+                if self.hf_output_dir is None:
+                    self.hf_output_dir = os.path.join(self.ckpt_dir, f'{self.model_type}-hf')
+                self.hf_output_dir = self.check_path_validity(self.hf_output_dir)
+                logger.info(f'Setting args.hf_output_dir: {self.hf_output_dir}')
 
     def select_dtype(self) -> None:
         if self.quant_bits > 0 and self.dtype == 'auto':
