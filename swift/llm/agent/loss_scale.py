aa390ac0d47774bf93abbc8de9e2ae775e0704c7
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2024-10-06 12:34:02 +0800

    update

--- swift/llm/agent/loss_scale.py
+++ swift/llm/agent/loss_scale.py
@@ -9,7 +9,7 @@ from .utils import calculate_loss_scale
 def agentflan_loss_scale(query: str, response: str):
     loss_scale_config_path = 'agentflan.json'
     path = os.path.dirname(os.path.abspath(__file__))
-    config_path = os.path.join(path, '../agent', loss_scale_config_path)
+    config_path = os.path.join(path, loss_scale_config_path)
     with open(config_path, 'r') as json_file:
         loss_scale_map = json.load(json_file)
     query_loss_scale_map = loss_scale_map['query']
@@ -20,7 +20,7 @@ def agentflan_loss_scale(query: str, response: str):
 def react_loss_scale(query: str, response: str):
     loss_scale_config_path = 'default_loss_scale_config.json'
     path = os.path.dirname(os.path.abspath(__file__))
-    config_path = os.path.join(path, '../agent', loss_scale_config_path)
+    config_path = os.path.join(path,  loss_scale_config_path)
     with open(config_path, 'r') as json_file:
         loss_scale_map = json.load(json_file)
     return calculate_loss_scale(query, response, loss_scale_map)
@@ -29,7 +29,7 @@ def react_loss_scale(query: str, response: str):
 def alpha_umi_loss_scale(query: str, response: str):
     loss_scale_config_path = 'alpha_umi_loss_scale_config.json'
     path = os.path.dirname(os.path.abspath(__file__))
-    config_path = os.path.join(path, '../agent', loss_scale_config_path)
+    config_path = os.path.join(path, loss_scale_config_path)
     with open(config_path, 'r') as json_file:
         loss_scale_map = json.load(json_file)
     return calculate_loss_scale(query, response, loss_scale_map)
