f132e5380af0960aa164130b454bddd2864730a8
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2024-10-08 10:40:20 +0800

    update

--- swift/llm/template/qwen.py
+++ swift/llm/template/qwen.py
@@ -1,5 +1,13 @@
 # Copyright (c) Alibaba, Inc. and its affiliates.
+from .constant import TemplateType
+from .register import register_template
+from .base import Template
+
 
 DEFAULT_SYSTEM = 'You are a helpful assistant.'
+
+register_template(TemplateType.qwen, QwenTemplate())
+register_template(TemplateType.qwen2_5, Qwen2_5Template())
+
 
 
