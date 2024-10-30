d585e0a3d9b8b7ddf022e211e136f0d3864c6748
huangjintao.hjt <huangjintao.hjt@alibaba-inc.com>
2024-10-30 21:23:03 +0800

    fix internvl2 eval

--- swift/llm/utils/model.py
+++ swift/llm/utils/model.py
@@ -1926,6 +1926,16 @@ def get_model_tokenizer_chatglm(model_dir: str,
     support_lmdeploy=True,
     requires=['transformers>=4.42'],
     hf_model_id='THUDM/glm-4-9b-chat-1m')
+@register_model(
+    ModelType.longwriter_glm4_9b,
+    'ZhipuAI/LongWriter-glm4-9b',
+    LoRATM.chatglm,
+    TemplateType.chatglm4,
+    support_flash_attn=True,
+    support_vllm=True,
+    support_lmdeploy=True,
+    requires=['transformers>=4.42'],
+    hf_model_id='THUDM/LongWriter-glm4-9b')
 def get_model_tokenizer_glm4(model_dir: str,
                              torch_dtype: torch.dtype,
                              model_kwargs: Dict[str, Any],
@@ -1937,24 +1947,11 @@ def get_model_tokenizer_glm4(model_dir: str,
         model_config._attn_implementation = 'flash_attention_2'
     elif use_flash_attn is False:
         model_config._attn_implementation = 'eager'
-    return get_model_tokenizer_chatglm(
+    model, tokenizer = get_model_tokenizer_chatglm(
         model_dir, torch_dtype, model_kwargs, load_model, model_config=model_config, **kwargs)
-
-
-@register_model(
-    ModelType.longwriter_glm4_9b,
-    'ZhipuAI/LongWriter-glm4-9b',
-    LoRATM.chatglm,
-    TemplateType.chatglm4,
-    support_flash_attn=True,
-    support_vllm=True,
-    support_lmdeploy=True,
-    requires=['transformers>=4.42'],
-    hf_model_id='THUDM/LongWriter-glm4-9b')
-def get_model_tokenizer_longwriter_glm4(*args, **kwargs):
-    model, tokenizer = get_model_tokenizer_glm4(*args, **kwargs)
-    for k in tokenizer.special_tokens.keys():
-        tokenizer.add_tokens(k)
+    if len(tokenizer.encode('<|user|>', add_special_tokens=False)) > 1:
+        for k in tokenizer.special_tokens.keys():
+            tokenizer.add_tokens(k)
     return model, tokenizer
 
 
@@ -4804,6 +4801,7 @@ def get_model_tokenizer_internvl(model_dir: str,
         _use_submodel_func(model, 'language_model', func_list)
         embedding = model.language_model.get_input_embeddings()
         embedding.register_forward_hook(_clone_hook)
+        model.config.keys_to_ignore_at_inference = ['past_key_values']  # fix prediction_step
 
     return model, tokenizer
 
