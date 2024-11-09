496f622a5cd64bdbc9f2ef0ffbd49edade69857e
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2024-11-09 11:37:42 +0800

    add citest

--- tests/general/test_dataset.py
+++ tests/general/test_dataset.py
@@ -1,21 +1,32 @@
+from typing import List
+
 from swift.llm import load_dataset
 
 
-def test_alpaca():
-
-    dataset = load_dataset(['AI-ModelScope/alpaca-gpt4-data-zh#1000', 'AI-ModelScope/alpaca-gpt4-data-en#200'],
-                           split_dataset_ratio=1,
-                           num_proc=1)
+def _test_dataset(datasets: List[str]):
+    dataset = load_dataset(datasets, num_proc=1)
     print(f'dataset[0]: {dataset[0]}')
     print(f'dataset[1]: {dataset[1]}')
+
+
+def test_alpaca():
+    _test_dataset(['AI-ModelScope/alpaca-gpt4-data-zh#1000', 'AI-ModelScope/alpaca-gpt4-data-en#200'])
 
 
 def test_coco():
-    dataset = load_dataset(['modelscope/coco_2014_caption:val'], split_dataset_ratio=0, num_proc=1)
-    print(f'dataset[0]: {dataset[0]}')
-    print(f'dataset[1]: {dataset[1]}')
+    _test_dataset(['modelscope/coco_2014_caption:val'])
+
+
+def test_llava_instruct():
+    _test_dataset(['AI-ModelScope/LLaVA-Instruct-150K'])
+
+
+def test_ms_bench():
+    _test_dataset(['iic/ms_bench'])
 
 
 if __name__ == '__main__':
     # test_alpaca()
-    test_coco()
+    # test_coco()
+    test_llava_instruct()
+    # test_ms_bench()
