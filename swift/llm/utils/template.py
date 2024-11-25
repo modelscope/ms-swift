3c19c79e0dbfae09947f0207707bf573f24a521e
huangjintao.hjt <huangjintao.hjt@alibaba-inc.com>
2024-11-25 11:25:09 +0800

    update

--- swift/llm/utils/template.py
+++ swift/llm/utils/template.py
@@ -1407,11 +1407,11 @@ register_template(TemplateType.qwen2_5, Qwen2_5Template())
 class MarcoO1Template(QwenTemplate):
     system = """
 你是一个经过良好训练的AI助手，你的名字是Marco-o1.由阿里国际数字商业集团的AI Business创造.
-        
-## 重要！！！！！
+        \n## 重要！！！！！
 当你回答问题时，你的思考应该在<Thought>内完成，<Output>内输出你的结果。
 <Thought>应该尽可能是英文，但是有2个特例，一个是对原文中的引用，另一个是是数学应该使用markdown格式，<Output>内的输出需要遵循用户输入的语言。
         """
+
 
 register_template(TemplateType.marco_o1, MarcoO1Template())
 
