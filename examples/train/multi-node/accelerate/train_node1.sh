c692fca3a25bb2611a393b503e5e393d8abf7313
tastelikefeet <yuze.zyz@alibaba-inc.com>
2024-11-27 16:59:58 +0800

    add multi node

---
+++ examples/train/multi-node/accelerate/train_node1.sh
@@ -0,0 +1,19 @@
+CUDA_VISIBLE_DEVICES=0,1 \
+accelerate launch --config_file ./examples/train/multi-node/accelerate/multi_node.yaml --machine_rank 0 \
+  swift/cli/sft.py \
+      --model Qwen/Qwen2-7B-Instruct \
+      --train_type lora \
+      --torch_dtype bfloat16 \
+      --dataset swift/self-cognition#1000 \
+      --num_train_epochs 1 \
+      --lora_rank 8 \
+      --lora_alpha 32 \
+      --learning_rate 1e-4 \
+      --gradient_accumulation_steps 16 \
+      --gradient_checkpointing_kwargs "{\"use_reentrant\": false}" \
+      --eval_steps 100 \
+      --save_steps 100 \
+      --save_total_limit 2 \
+      --logging_steps 5 \
+      --model_author swift \
+      --model_name swift-robot
---
+++ examples/train/multi-node/swift/train_node2.sh
@@ -0,0 +1,22 @@
+CUDA_VISIBLE_DEVICES=2,3 \
+NNODES=2 \
+NODE_RANK=1 \
+MASTER_ADDR=127.0.0.1 \
+NPROC_PER_NODE=2 \
+swift sft \
+      --model Qwen/Qwen2-7B-Instruct \
+      --train_type lora \
+      --torch_dtype bfloat16 \
+      --dataset swift/self-cognition#1000 \
+      --num_train_epochs 1 \
+      --lora_rank 8 \
+      --lora_alpha 32 \
+      --learning_rate 1e-4 \
+      --gradient_accumulation_steps 16 \
+      --gradient_checkpointing_kwargs "{\"use_reentrant\": false}" \
+      --eval_steps 100 \
+      --save_steps 100 \
+      --save_total_limit 2 \
+      --logging_steps 5 \
+      --model_author swift \
+      --model_name swift-robot
