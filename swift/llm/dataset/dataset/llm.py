55f9739f16ac85323dbe1502ea4cdcbe542b12e2
yuze.zyz <yuze.zyz@alibaba-inc.com>
2024-11-27 20:53:49 +0800

    add hc3 cls dataset

--- swift/llm/dataset/dataset/llm.py
+++ swift/llm/dataset/dataset/llm.py
@@ -405,6 +405,80 @@ Output:"""
     return HfDataset.from_list(messages)
 
 
+def _preprocess_hc3_cls(dataset: DATASET_TYPE, **kwargs) -> DATASET_TYPE:
+    prompt = """Classification Task: Are the following responses from a human or from ChatGPT?
+Question: {question}
+Answer: {answer}
+Category: 0 for Human, 1 for ChatGPT
+Output:"""
+    if isinstance(dataset, IterableDataset):
+
+        def generate_example(dataset):
+            for example in dataset:
+                question = example['question']
+                for h in example['human_answers']:
+                    yield {
+                        'messages': [{
+                            'role': 'user',
+                            'content': prompt.format(question=question, answer=h)
+                        }],
+                        'label': 0,
+                    }
+                for c in example['chatgpt_answers']:
+                    yield {
+                        'messages': [{
+                            'role': 'user',
+                            'content': prompt.format(question=question, answer=c)
+                        }],
+                        'label': 1,
+                    }
+
+        return IterableDataset.from_generator(generate_example, gen_kwargs={'dataset': dataset})
+
+    messages = []
+    for d in dataset:
+        question = d['question']
+        for h in d['human_answers']:
+            messages.append({
+                'messages': [{
+                    'role': 'user',
+                    'content': prompt.format(question=question, answer=h)
+                }],
+                'label': 0,
+            })
+        for c in d['chatgpt_answers']:
+            messages.append({
+                'messages': [{
+                    'role': 'user',
+                    'content': prompt.format(question=question, answer=c)
+                }],
+                'label': 1,
+            })
+    return HfDataset.from_list(messages)
+
+
+hc3_subset_names = ['baike', 'open_qa', 'nlpcc_dbqa', 'finance', 'medicine', 'law', 'psychology']
+hc3_subsets: List[SubsetDataset] = []
+for hc3_subset_name in hc3_subset_names:
+    hc3_subsets.append(SubsetDataset(
+        name=hc3_subset_name,
+        subset=hc3_subset_name,
+        preprocess_func=_preprocess_hc3,
+    ))
+    hc3_subsets.append(SubsetDataset(
+        name=f'{hc3_subset_name}_cls',
+        subset=hc3_subset_name,
+        preprocess_func=_preprocess_hc3_cls,
+    ))
+
+register_dataset(
+    DatasetMeta(
+        ms_dataset_id='simpleai/HC3-Chinese',
+        hf_dataset_id='Hello-SimpleAI/HC3-Chinese',
+        subsets=hc3_subsets,
+        tags=['text-generation', 'classification', '🔥']))
+
+
 register_dataset(
     DatasetMeta(
         ms_dataset_id='simpleai/HC3-Chinese',
