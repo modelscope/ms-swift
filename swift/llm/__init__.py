963ece752303aa9d04ea9940df768149df5082fb
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2024-11-28 19:30:44 +0800

    update

--- swift/llm/__init__.py
+++ swift/llm/__init__.py
@@ -17,7 +17,7 @@ if TYPE_CHECKING:
                            ProcessorMixin)
     from .model import (MODEL_MAPPING, ModelType, get_model_tokenizer, safe_snapshot_download, HfConfigFactory,
                         ModelInfo, ModelMeta, ModelKeys, register_model_arch, MultiModelKeys, ModelArch, get_model_arch,
-                        MODEL_ARCH_MAPPING, get_model_info_meta)
+                        MODEL_ARCH_MAPPING, get_model_info_meta, get_model_name)
     from .dataset import (AlpacaPreprocessor, MessagesPreprocessor, AutoPreprocessor, DATASET_MAPPING, MediaResource,
                           register_dataset, register_dataset_info, EncodePreprocessor, LazyLLMDataset,
                           ConstantLengthDataset, standard_keys, load_dataset, DATASET_TYPE, sample_dataset,
@@ -47,7 +47,7 @@ else:
         'model': [
             'MODEL_MAPPING', 'ModelType', 'get_model_tokenizer', 'safe_snapshot_download', 'HfConfigFactory',
             'ModelInfo', 'ModelMeta', 'ModelKeys', 'register_model_arch', 'MultiModelKeys', 'ModelArch',
-            'MODEL_ARCH_MAPPING', 'get_model_arch', 'get_model_info_meta'
+            'MODEL_ARCH_MAPPING', 'get_model_arch', 'get_model_info_meta', 'get_model_name'
         ],
         'dataset': [
             'AlpacaPreprocessor', 'ClsPreprocessor', 'ComposePreprocessor', 'MessagesPreprocessor', 'DATASET_MAPPING',
