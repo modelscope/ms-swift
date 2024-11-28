6950d42522cfdfd7adcbb48842db0078cee22dd1
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2024-11-28 16:35:07 +0800

    update

--- swift/llm/template/__init__.py
+++ swift/llm/template/__init__.py
@@ -10,6 +10,6 @@ from .agent import get_tools_prompt, split_action_action_input
 from .base import Template
 from .constant import TemplateType
 from .register import TEMPLATE_MAPPING, get_template, get_template_meta, register_template
-from .template_inputs import InferRequest, Messages, TemplateInputs, Tool
+from .template_inputs import InferRequest, Message, Messages, TemplateInputs, Tool
 from .template_meta import TemplateMeta
 from .utils import Processor, ProcessorMixin, Word
