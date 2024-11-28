0d8075107f66785c99f5359880f5278aaa96bdc0
yuze.zyz <yuze.zyz@alibaba-inc.com>
2024-11-28 10:54:28 +0800

    wip

--- examples/train/plugins/train_cls.sh
+++ examples/train/plugins/train_cls.sh
@@ -1,16 +1,14 @@
-# 17GiB
-CUDA_VISIBLE_DEVICES=2 \
+# uncomment custom_trainer
+CUDA_VISIBLE_DEVICES=0 \
 swift sft \
     --model Qwen/Qwen2-7B-Instruct \
-    --train_type boft \
-    --dataset swift/self-cognition#1000 \
-    --num_train_epochs 50 \
+    --train_type ia3 \
+    --dataset simpleai/HC3-Chinese:baike_cls#1000 \
+    --num_train_epochs 1 \
     --per_device_train_batch_size 1 \
     --learning_rate 1e-4 \
     --gradient_accumulation_steps 16 \
     --eval_steps 100 \
     --save_steps 100 \
     --save_total_limit 2 \
-    --logging_steps 5 \
-    --model_author swift \
-    --model_name swift-robot
+    --logging_steps 5
