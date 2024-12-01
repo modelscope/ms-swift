6b69493efd7344e4a0efd732d7929774710fcdee
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2024-12-01 11:18:23 +0800

    update

--- tests/infer/test_lora.py
+++ tests/infer/test_lora.py
@@ -14,14 +14,14 @@ def test_llm_pt():
         request_config=RequestConfig(),
         lora_request=LoRARequest(
             lora_name='lora',
-            lora_local_path=
-            '/mnt/nas2/huangjintao.hjt/work/modelscope_swift/tutorial/output/Qwen2-7B-Instruct/checkpoint-93'))
+            lora_path='/mnt/nas2/huangjintao.hjt/work/modelscope_swift/tutorial/output/Qwen2-7B-Instruct/checkpoint-93'
+        ))
     print(response_list[0].choices[0].message)
 
 
 def test_llm_vllm():
     from swift.llm import VllmEngine, RequestConfig, LoRARequest
-    engine = PtEngine('qwen/Qwen2-7B-Instruct', max_batch_size=16)
+    engine = VllmEngine('qwen/Qwen2-7B-Instruct', enable_lora=True)
 
     response_list = engine.infer([{'messages': [{'role': 'user', 'content': '你是谁'}]}], request_config=RequestConfig())
     print(response_list[0].choices[0].message)
@@ -35,10 +35,11 @@ def test_llm_vllm():
         request_config=RequestConfig(),
         lora_request=LoRARequest(
             lora_name='lora',
-            lora_local_path=
-            '/mnt/nas2/huangjintao.hjt/work/modelscope_swift/tutorial/output/Qwen2-7B-Instruct/checkpoint-93'))
+            lora_path='/mnt/nas2/huangjintao.hjt/work/modelscope_swift/tutorial/output/Qwen2-7B-Instruct/checkpoint-93'
+        ))
     print(response_list[0].choices[0].message)
 
 
 if __name__ == '__main__':
-    test_llm_pt()
+    # test_llm_pt()
+    test_llm_vllm()
