dec801837dc6b5f1a8ab730436c48ef20c067a8a
yuze.zyz <yuze.zyz@alibaba-inc.com>
2024-12-03 19:52:25 +0800

    add docs

---
+++ examples/train/agent/train.sh
@@ -0,0 +1,23 @@
+nproc_per_node=4
+
+NPROC_PER_NODE=$nproc_per_node \
+CUDA_VISIBLE_DEVICES=0,1,2,3 \
+swift pt \
+    --model Qwen/Qwen-7B-Chat \
+    --train_type lora \
+    --dataset iic/ms_agent \
+    --loss_scale react \
+    --tools_prompt react_zh \
+    --torch_dtype bfloat16 \
+    --num_train_epochs 1 \
+    --per_device_train_batch_size 1 \
+    --learning_rate 1e-4 \
+    --gradient_accumulation_steps $(expr 32 / $nproc_per_node) \
+    --warmup_ratio 0.03 \
+    --eval_steps 500 \
+    --save_steps 500 \
+    --save_total_limit 2 \
+    --logging_steps 5 \
+    --deepspeed zero3 \
+    --max_length 1024 \
+    --max_steps 1000
