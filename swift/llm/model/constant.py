9e250940a449946b5940a871f492e013bece2594
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2024-12-15 02:52:06 +0800

    support Shanghai_AI_Laboratory/internlm-xcomposer2d5-ol-7b:audio

--- swift/llm/model/constant.py
+++ swift/llm/model/constant.py
@@ -115,6 +115,7 @@ class MLLMModelType:
     xcomposer2 = 'xcomposer2'
     xcomposer2_4khd = 'xcomposer2_4khd'
     xcomposer2_5 = 'xcomposer2_5'
+    xcomposer2_5_ol_audio = 'xcomposer2_5_ol_audio'
 
     llama3_2_vision = 'llama3_2_vision'
     llama3_1_omni = 'llama3_1_omni'
