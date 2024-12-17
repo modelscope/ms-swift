924b3eab2d6a9ca2e05c881bdb6018324ea90cfb
Jintao <huangjintao.hjt@alibaba-inc.com>
2024-12-17 17:17:47 +0800

    fix deepseek_vl2 (#2675)

--- swift/llm/model/model/deepseek.py
+++ swift/llm/model/model/deepseek.py
@@ -195,7 +195,7 @@ def get_model_tokenizer_deepseek_vl2(model_dir: str, *args, **kwargs):
     if not local_repo_path:
         local_repo_path = git_clone_github('https://github.com/deepseek-ai/DeepSeek-VL2')
     sys.path.append(os.path.join(local_repo_path))
-    from deepseek_vl.models import DeepseekVLV2Processor, DeepseekVLV2ForCausalLM
+    from deepseek_vl2.models import DeepseekVLV2Processor, DeepseekVLV2ForCausalLM
     processor: DeepseekVLV2Processor = DeepseekVLV2Processor.from_pretrained(model_dir)
     return _get_deepseek_vl(processor, 'language', model_dir, *args, **kwargs)
 
