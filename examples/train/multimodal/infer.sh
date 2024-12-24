cb8d2bbb72de8dd213ab8b9bfa9f477d02464bbe
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2024-12-24 20:43:00 +0800

    update

---
+++ examples/train/multimodal/infer.sh
@@ -0,0 +1,6 @@
+# Perform inference using the validation set from the training phase.
+# CUDA_VISIBLE_DEVICES=0,1 \
+swift infer \
+    --adapters output/vx-xxx/checkpoint-xxx \
+    --stream true \
+    --load_data_args true
