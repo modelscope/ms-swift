1f60fb2f2935dda7623bd443e58a7d68a337d773
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2024-12-27 17:01:14 +0800

    fix

--- swift/llm/argument/base_args/base_args.py
+++ swift/llm/argument/base_args/base_args.py
@@ -171,7 +171,7 @@ class BaseArguments(CompatArguments, GenerationArguments, QuantizeArguments, Dat
         return self
 
     def _init_ckpt_dir(self, adapters=None):
-        model_dirs = adapters or self.adapters.copy()
+        model_dirs = (adapters or self.adapters).copy()
         if self.model:
             model_dirs.append(self.model)
         self.ckpt_dir = None
