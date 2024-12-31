1fa04e81921e9414826ca83fb6b164a075864fdc
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2024-12-31 13:52:23 +0800

    update shell dataloader_num_workers

--- examples/train/multimodal/dpo.sh
+++ examples/train/multimodal/dpo.sh
@@ -9,15 +9,24 @@ MAX_PIXELS=1003520 \
 swift rlhf \
     --rlhf_type dpo \
     --model Qwen/Qwen2-VL-7B-Instruct \
-    --train_type lora \
     --dataset swift/RLAIF-V-Dataset \
+    --train_type lora \
+    --torch_dtype bfloat16 \
     --num_train_epochs 1 \
+    --per_device_train_batch_size 1 \
+    --per_device_eval_batch_size 1 \
     --learning_rate 1e-4 \
     --lora_rank 8 \
     --lora_alpha 32 \
+    --target_modules all-linear \
+    --freeze_vit true \
     --gradient_accumulation_steps $(expr 16 / $nproc_per_node) \
     --eval_steps 100 \
     --save_steps 100 \
-    --save_total_limit 2 \
+    --save_total_limit 5 \
     --deepspeed zero3 \
-    --logging_steps 5
+    --logging_steps 5 \
+    --max_length 2048 \
+    --output_dir output \
+    --warmup_ratio 0.05 \
+    --dataloader_num_workers 4
