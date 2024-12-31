58be39ef7787f173b120fcaad2ab89c98bf909b5
Jintao <huangjintao.hjt@alibaba-inc.com>
2024-12-31 13:41:21 +0800

    fix citest (#2812)

--- tests/llm/test_template.py
+++ tests/llm/test_template.py
@@ -37,7 +37,7 @@ def _infer_model(pt_engine, system=None, messages=None):
 class TestTemplate(unittest.TestCase):
 
     def test_template(self):
-        pt_engine = PtEngine('Qwen/Qwen2.5-3B-Instruct')
+        pt_engine = PtEngine('Qwen/Qwen2.5-3B-Instruct-GPTQ-Int4')
         response = _infer_model(pt_engine)
         pt_engine.default_template.template_backend = 'jinja'
         response2 = _infer_model(pt_engine)
