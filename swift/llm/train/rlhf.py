2f045c97c4fb8a8c55dd660027995158768d7e30
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2025-01-06 14:21:39 +0800

    update

--- swift/llm/train/rlhf.py
+++ swift/llm/train/rlhf.py
@@ -1,10 +1,12 @@
 # Copyright (c) Alibaba, Inc. and its affiliates.
 from typing import List, Union
 
-from swift.utils import patch_getattr
+from swift.utils import get_logger, get_model_parameter_info, patch_getattr
 from ..argument import RLHFArguments
 from .kto import prepare_kto_dataset
 from .sft import SwiftSft
+
+logger = get_logger()
 
 
 class SwiftRLHF(SwiftSft):
