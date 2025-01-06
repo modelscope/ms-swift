89dbe794dc111fa0f190b275e9b0c3510cb65fd8
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2025-01-06 16:47:47 +0800

    fix

--- swift/trainers/rlhf_trainer/ppo_trainer.py
+++ swift/trainers/rlhf_trainer/ppo_trainer.py
@@ -5,7 +5,9 @@ from torch.utils.data import DataLoader
 from transformers import PreTrainedModel
 from trl import PPOv2Trainer as HFPPOv2Trainer
 
+from swift.utils import patch_getattr
 from ..mixin import SwiftMixin
+from .rlhf_mixin import RLHFTrainerMixin
 
 
 class PPOTrainer(SwiftMixin, HFPPOv2Trainer):
@@ -14,11 +16,11 @@ class PPOTrainer(SwiftMixin, HFPPOv2Trainer):
 
     @staticmethod
     @contextmanager
-    def _patch_dataloader(data_collator):
+    def _patch_dataloader(collate_fn):
         __init__ = DataLoader.__init__
 
         def __new_init__(self, *args, **kwargs):
-            kwargs['data_collator'] = data_collator
+            kwargs['collate_fn'] = collate_fn
             __init__(self, *args, **kwargs)
 
         DataLoader.__init__ = __new_init__
@@ -31,9 +33,11 @@ class PPOTrainer(SwiftMixin, HFPPOv2Trainer):
             new_kwargs = {
                 k: v
                 for k, v in kwargs.items()
-                if k in ['train_dataset', 'data_collator', 'reward_model', 'val_model', 'eval_dataset', 'tokenizer']
+                if k in ['train_dataset', 'data_collator', 'reward_model', 'value_model', 'eval_dataset']
             }
-            self.ppo_trainer_init(config=kwargs['args'], policy=model, ref_policy=ref_model, **new_kwargs)
+            self.ppo_trainer_init(
+                config=kwargs['args'], tokenizer=self.tokenizer, policy=model, ref_policy=ref_model, **new_kwargs)
+        patch_getattr(self.model.__class__, 'policy')
 
     def train(self, *args, **kwargs):
         # remove args that are not needed for the HFPPOTrainer
