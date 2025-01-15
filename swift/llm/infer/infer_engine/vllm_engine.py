64952553cf2acbf0b93defed238f45846a836247
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2025-01-16 00:17:31 +0800

    fix vllm  tp block

--- swift/llm/infer/infer_engine/vllm_engine.py
+++ swift/llm/infer/infer_engine/vllm_engine.py
@@ -331,6 +331,13 @@ class VllmEngine(InferEngine):
             choices.append(choice)
         return ChatCompletionResponse(model=self.model_name, choices=choices, usage=usage_info, id=request_id)
 
+    def _batch_infer_stream(self,
+                            tasks,
+                            stream: bool = True,
+                            use_tqdm: bool = True) -> Iterator[List[Optional[ChatCompletionStreamResponse]]]:
+        self.engine.engine.model_executor.parallel_worker_tasks = None
+        return super()._batch_infer_stream(tasks, stream, use_tqdm)
+
     def infer(
         self,
         infer_requests: List[InferRequest],
@@ -341,7 +348,6 @@ class VllmEngine(InferEngine):
         use_tqdm: Optional[bool] = None,
         adapter_request: Optional[AdapterRequest] = None
     ) -> Union[List[ChatCompletionResponse], Iterator[List[Optional[ChatCompletionStreamResponse]]]]:
-        self.engine.engine.model_executor.parallel_worker_tasks = None
         return super().infer(
             infer_requests,
             request_config,
