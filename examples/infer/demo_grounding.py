f882aa397440d9bbda3fa106c82e0ab7911a8f69
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2025-01-27 23:30:13 +0800

    lint

--- examples/infer/demo_grounding.py
+++ examples/infer/demo_grounding.py
@@ -1,25 +1,28 @@
 import os
-from typing import Literal
-from swift.llm import load_image
 import re
+from typing import Literal
+
+from swift.llm import load_image
+
 os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 
 
-
 def draw_bbox(image, response):
-    matchs = re.findall(r'<\|object_ref_start\|>(.*?)<\|object_ref_end\|><\|box_start\|>\((\d+),(\d+)\),\((\d+),(\d+)\)<\|box_end\|>', response)
-    
+    matchs = re.findall(
+        r'<\|object_ref_start\|>(.*?)<\|object_ref_end\|><\|box_start\|>\((\d+),(\d+)\),\((\d+),(\d+)\)<\|box_end\|>',
+        response)
+
 
 def infer_grounding():
     from swift.llm import (PtEngine, RequestConfig, AdapterRequest, get_template, BaseArguments, InferRequest,
                            safe_snapshot_download, get_model_tokenizer)
     from swift.tuners import Swift
     image = load_image('http://modelscope-open.oss-cn-hangzhou.aliyuncs.com/images/animal.png')
-    infer_request = InferRequest(messages=[{'role': 'user', 'content': 'Task: Object Detection'}], 
-                                 images=[image])
+    infer_request = InferRequest(messages=[{'role': 'user', 'content': 'Task: Object Detection'}], images=[image])
 
     request_config = RequestConfig(max_tokens=512, temperature=0)
-    adapter_path = safe_snapshot_download('/mnt/nas2/huangjintao.hjt/work/llmscope/output/v92-20250126-173609/checkpoint-1237')
+    adapter_path = safe_snapshot_download(
+        '/mnt/nas2/huangjintao.hjt/work/llmscope/output/v92-20250126-173609/checkpoint-1237')
     args = BaseArguments.from_pretrained(adapter_path)
 
     engine = PtEngine(args.model, adapters=[adapter_path])
