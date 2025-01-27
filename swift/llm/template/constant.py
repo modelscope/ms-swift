5a2187f8a34a10a073e25a319f0e59e6e77fed95
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2025-01-27 20:43:17 +0800

    fix qwen2_5 vl

--- swift/llm/template/constant.py
+++ swift/llm/template/constant.py
@@ -85,6 +85,7 @@ class MLLMTemplateType:
     qwen_vl = 'qwen_vl'
     qwen_audio = 'qwen_audio'
     qwen2_vl = 'qwen2_vl'
+    qwen2_5_vl = 'qwen2_5_vl'
     qwen2_audio = 'qwen2_audio'
     qvq = 'qvq'
     ovis1_6 = 'ovis1_6'
