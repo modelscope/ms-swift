832e6751302382df77c92901af587a73c2547a3a
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2025-02-02 22:45:13 +0800

    update readme

--- examples/export/push_to_hub.sh
+++ examples/export/push_to_hub.sh
@@ -1,6 +1,6 @@
 CUDA_VISIBLE_DEVICES=0 swift export \
     --adapters output/vx-xxx/checkpoint-xxx \
     --push_to_hub true \
-    --hub_model_id '<your-model-id>' \
-    --hub_token '<your-sdk-token>' \
+    --hub_model_id '<model-id>' \
+    --hub_token '<sdk-token>' \
     --use_hf false
