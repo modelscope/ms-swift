2bac89a3bfded05de1c30896a048bfdad6d91a34
hjh <hujinghan.hjh@alibaba-inc.com>
2025-02-11 01:01:54 +0800

    fix

--- docs/source_en/Instruction/GRPO.md
+++ docs/source_en/Instruction/GRPO.md
@@ -43,7 +43,7 @@ swift rlhf \
     --num_train_epochs 1 \
     --per_device_train_batch_size 2 \
     --per_device_eval_batch_size 2 \
-    --learning_rate 2e-5 \
+    --learning_rate 2e-6 \
     --gradient_accumulation_steps 16 \
     --save_total_limit 2 \
     --logging_steps 5 \
@@ -69,7 +69,7 @@ swift rlhf \
     --num_train_epochs 1 \
     --per_device_train_batch_size 2 \
     --per_device_eval_batch_size 2 \
-    --learning_rate 2e-5 \
+    --learning_rate 2e-6 \
     --gradient_accumulation_steps 16 \
     --save_total_limit 2 \
     --logging_steps 5 \
--- swift/trainers/rlhf_trainer/grpo_trainer.py
+++ swift/trainers/rlhf_trainer/grpo_trainer.py
@@ -2,7 +2,7 @@
 # Part of the implementation is borrowed from huggingface/trl.
 import inspect
 from collections import defaultdict
-from typing import Any, Dict, List, Optional, Union
+from typing import Any, Dict, List, Optional, Union, Callable
 from unittest.mock import patch
 
 import torch
@@ -33,7 +33,7 @@ class GRPOTrainer(RLHFTrainerMixin, SwiftMixin, HFGRPOTrainer):
                  model: Optional[Union[PreTrainedModel, nn.Module]] = None,
                  ref_model: Optional[Union[PreTrainedModel, nn.Module]] = None,
                  reward_model: Optional[Union[PreTrainedModel, nn.Module]] = None,
-                 reward_funcs: List[str, callable] = None,
+                 reward_funcs: Optional[List[Union[str, Callable]]] = None
                  *_args,
                  **kwargs):
 
@@ -239,7 +239,7 @@ class GRPOTrainer(RLHFTrainerMixin, SwiftMixin, HFGRPOTrainer):
             if isinstance(reward_func, nn.Module):  # Module instead of PretrainedModel for compat with compiled models
                 reward_func_name = reward_func.config._name_or_path.split('/')[-1]
             else:
-                if isinstance(reward_func, callable):
+                if callable(reward_func):
                     reward_func_name = reward_func.__name__  # function
                 else:
                     reward_func_name = reward_func.__class__.__name__  # object
