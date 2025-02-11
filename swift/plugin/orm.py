1f0cc0b179f530ba0c6ee732e39c13fda0ae7072
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2025-02-11 23:13:00 +0800

    fix

--- swift/plugin/orm.py
+++ swift/plugin/orm.py
@@ -364,4 +364,13 @@ class MathFormat(ORM):
         ]
 
 
-orms = {'toolbench': ReactORM, 'math': MathORM, 'dummy': DummyORM, 'accuracy': MathAccuracy, 'format': MathFormat}
+class SoftMathFormat(MathFormat):
+    def __call__(self, completions, **kwargs):
+        """Reward function that checks if the completion has a specific format."""
+        pattern = r'^<think>.*?</think>\s*<answer>.*?</answer>$'
+        matches = [re.match(pattern, content, re.DOTALL | re.MULTILINE) for content in completions]
+        return [1.0 if match else 0.0 for match in matches]
+
+
+
+orms = {'toolbench': ReactORM, 'math': MathORM, 'dummy': DummyORM, 'accuracy': MathAccuracy, 'format': MathFormat, 'soft_format': SoftMathFormat}
