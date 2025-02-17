280ba1d42c9a05aa47d281f1da85ed67d55e47e0
tastelikefeet <58414341+tastelikefeet@users.noreply.github.com>
2025-02-17 19:52:53 +0800

    Support swanlab (#3142)

--- swift/ui/llm_eval/llm_eval.py
+++ swift/ui/llm_eval/llm_eval.py
@@ -11,6 +11,7 @@ import gradio as gr
 import json
 import torch
 from json import JSONDecodeError
+from transformers.utils import is_torch_cuda_available, is_torch_npu_available
 
 from swift.llm import EvalArguments
 from swift.ui.base import BaseUI
@@ -157,7 +158,12 @@ class LLMEval(BaseUI):
         gpus = ','.join(devices)
         cuda_param = ''
         if gpus != 'cpu':
-            cuda_param = f'CUDA_VISIBLE_DEVICES={gpus}'
+            if is_torch_npu_available():
+                cuda_param = f'ASCEND_RT_VISIBLE_DEVICES={gpus}'
+            elif is_torch_cuda_available():
+                cuda_param = f'CUDA_VISIBLE_DEVICES={gpus}'
+            else:
+                cuda_param = ''
         now = datetime.now()
         time_str = f'{now.year}{now.month}{now.day}{now.hour}{now.minute}{now.second}'
         file_path = f'output/{eval_args.model_type}-{time_str}'
