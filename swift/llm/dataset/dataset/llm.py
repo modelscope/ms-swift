cd36488d5385a94bf1e3a36f59459a14ed37041d
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2025-02-20 00:39:48 +0800

    lint pass

--- swift/llm/dataset/dataset/llm.py
+++ swift/llm/dataset/dataset/llm.py
@@ -429,11 +429,7 @@ register_dataset(
     DatasetMeta(ms_dataset_id='modelscope/MathR', subsets=['default', 'clean'], split=['train'], tags=['qa', 'math']))
 
 register_dataset(
-    DatasetMeta(
-        ms_dataset_id='modelscope/MathR-32B-Distill',
-        subsets=['data'],
-        split=['train'],
-        tags=['qa', 'math']))
+    DatasetMeta(ms_dataset_id='modelscope/MathR-32B-Distill', subsets=['data'], split=['train'], tags=['qa', 'math']))
 
 
 class HC3Preprocessor(ResponsePreprocessor):
