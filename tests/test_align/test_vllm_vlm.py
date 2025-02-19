2df9110eac9c0b84a05d2552e5031abf6fc6c6f4
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2025-02-19 17:09:22 +0800

    fix

--- tests/test_align/test_vllm_vlm.py
+++ tests/test_align/test_vllm_vlm.py
@@ -30,6 +30,7 @@ def _infer_image(model, use_chat_template: bool = True, max_model_len=8192, syst
                              RequestConfig(temperature=0, max_tokens=64, repetition_penalty=1.))
     return resp_list[0].choices[0].message.content
 
+
 def test_qwen2_audio():
     response = _infer_audio('Qwen/Qwen2-Audio-7B-Instruct')
     assert response == "The audio is a man speaking in Mandarin saying '今天天气真好呀'."
@@ -42,6 +43,7 @@ def test_qwen2_vl():
         'expressive blue eyes and is looking directly at the camera. Its ears are perked up, and it has a '
         'small red mark on its left ear. The background is blurred, focusing attention on the kitten. The overall')
 
+
 def test_qwen2_5_vl():
     response = _infer_image('Qwen/Qwen2.5-VL-3B-Instruct')
     assert response == (
@@ -53,18 +55,18 @@ def test_qwen2_5_vl():
 
 def test_deepseek_vl_v2():
     response = _infer_image('deepseek-ai/deepseek-vl2-tiny', max_model_len=4096)
-    assert response == (
-        'The image depicts a close-up of a adorable kitten with large, expressive eyes. The kitten has '
-        'a mix of white and gray fur with distinct black stripes, giving it a tabby-like appearance. '
-        'Its ears are perked up, and its whiskers are prominently visible. The background is blurred, '
-        'focusing attention on the kitten')
+    assert response == ('The image depicts a close-up of a adorable kitten with large, expressive eyes. The kitten has '
+                        'a mix of white and gray fur with distinct black stripes, giving it a tabby-like appearance. '
+                        'Its ears are perked up, and its whiskers are prominently visible. The background is blurred, '
+                        'focusing attention on the kitten')
+
 
 def test_internvl2():
     response = _infer_image('OpenGVLab/InternVL2-2B', max_model_len=4096, system='')
-    assert response == (
-        'The image features a kitten with striking blue eyes and a mix of white and black fur. '
-        'The kitten has large, expressive eyes and a small, pink nose. Its ears are perked up, '
-        'and it appears to be looking directly at the camera. The fur is soft and fluffy, with a mix')
+    assert response == ('The image features a kitten with striking blue eyes and a mix of white and black fur. '
+                        'The kitten has large, expressive eyes and a small, pink nose. Its ears are perked up, '
+                        'and it appears to be looking directly at the camera. The fur is soft and fluffy, with a mix')
+
 
 if __name__ == '__main__':
     from swift.llm import VllmEngine, InferRequest, RequestConfig
