7ad68764f75eb478349dc604fe51586b4314d6e7
mi804 <1576993271@qq.com>
2025-02-25 16:05:54 +0800

    minor fix

--- swift/llm/dataset/dataset/llm.py
+++ swift/llm/dataset/dataset/llm.py
@@ -423,12 +423,10 @@ class CoundownTaskPreprocessor(ResponsePreprocessor):
     def preprocess(self, row: Dict[str, Any]) -> Dict[str, Any]:
         numbers = row['nums']
         target = row.pop('response', None)
-        query = f"""
-        Using the numbers {numbers}, create an equation that equals {target}.
+        query = f"""Using the numbers {numbers}, create an equation that equals {target}.
         You can use basic arithmetic operations (+, -, *, /) and each number can only be used once.
         Show your work in <think> </think> tags. And return the final equation and answer in <answer> </answer> tags,
-        for example <answer> (1 + 2) / 3 * 4 = 4 </answer>.
-        """
+        for example <answer> (1 + 2) / 3 * 4 = 4 </answer>."""
         row.update({'target': target, 'query': query})
         return super().preprocess(row)
 
