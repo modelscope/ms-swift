9d6151b970e82db976a71c21ffda5f0bd79ee510
mi804 <1576993271@qq.com>
2025-02-25 16:31:31 +0800

    minor fix

--- swift/llm/dataset/dataset/llm.py
+++ swift/llm/dataset/dataset/llm.py
@@ -424,9 +424,9 @@ class CoundownTaskPreprocessor(ResponsePreprocessor):
         numbers = row['nums']
         target = row.pop('response', None)
         query = f"""Using the numbers {numbers}, create an equation that equals {target}.
-        You can use basic arithmetic operations (+, -, *, /) and each number can only be used once.
-        Show your work in <think> </think> tags. And return the final equation and answer in <answer> </answer> tags,
-        for example <answer> (1 + 2) / 3 * 4 = 4 </answer>."""
+You can use basic arithmetic operations (+, -, *, /) and each number can only be used once.
+Show your work in <think> </think> tags. And return the final equation and answer in <answer> </answer> tags,
+for example <answer> (1 + 2) / 3 * 4 = 4 </answer>."""
         row.update({'target': target, 'query': query})
         return super().preprocess(row)
 
