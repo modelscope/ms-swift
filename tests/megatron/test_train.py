8d5c9a86bfdde6386c936adc906635bf4ccf942b
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2025-03-11 22:35:20 +0800

    update

--- tests/megatron/test_train.py
+++ tests/megatron/test_train.py
@@ -7,8 +7,11 @@ def test_train():
     from swift.megatron import megatron_sft_main, MegatronTrainArguments
     megatron_sft_main(
         MegatronTrainArguments(
-            megatron_model='Qwen2-7B-Instruct-megatron', dataset=['AI-ModelScope/alpaca-gpt4-data-zh#500'], 
-            tensor_model_parallel_size=2, train_iters=200, eval_iters=5))
+            megatron_model='Qwen2-7B-Instruct-megatron',
+            dataset=['AI-ModelScope/alpaca-gpt4-data-zh#500'],
+            tensor_model_parallel_size=2,
+            train_iters=200,
+            eval_iters=5))
 
 
 if __name__ == '__main__':
