a699b76e835e2b74ab8e5d0ad56f2064d5317a8c
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2025-03-12 23:30:59 +0800

    update

--- tests/train/test_packing.py
+++ tests/train/test_packing.py
@@ -5,7 +5,7 @@ os.environ['CUDA_VISIBLE_DEVICES'] = '0,1'
 kwargs = {
     'per_device_train_batch_size': 2,
     'save_steps': 50,
-    'gradient_accumulation_steps': 4,
+    'gradient_accumulation_steps': 1,
     'num_train_epochs': 3,
 }
 
@@ -19,6 +19,7 @@ def test_llm():
             packing=True,
             max_length=4096,
             attn_impl='flash_attn',
+            logging_steps=1,
             **kwargs))
     last_model_checkpoint = result['last_model_checkpoint']
     infer_main(InferArguments(adapters=last_model_checkpoint, load_data_args=True, merge_lora=True))
@@ -31,7 +32,7 @@ def test_streaming():
             model='Qwen/Qwen2-7B-Instruct',
             dataset=['AI-ModelScope/alpaca-gpt4-data-zh#10000'],
             packing=True,
-            max_length=8192,
+            max_length=4096,
             streaming=True,
             attn_impl='flash_attn',
             **kwargs))
