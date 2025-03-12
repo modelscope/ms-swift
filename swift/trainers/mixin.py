97b022e73c1da7fce3a8a80604b74c297e0c9152
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2025-03-13 00:18:23 +0800

    update

--- swift/trainers/mixin.py
+++ swift/trainers/mixin.py
@@ -260,12 +260,12 @@ class SwiftMixin:
         origin_clip_grad_norm_ = Accelerator.clip_grad_norm_
 
         def clip_grad_norm_(self, parameters, *args, **kwargs):
+            # If NaN occurs, ignore weight updates.
             parameters = list(parameters)
             grad_norm = origin_clip_grad_norm_(self, parameters, *args, **kwargs)
             if grad_norm.isnan().item():
                 for p in parameters:
-                    if p.grad is not None:
-                        p.grad.data.zero_()
+                    p.grad = None
             return grad_norm
 
         Accelerator.clip_grad_norm_ = clip_grad_norm_
