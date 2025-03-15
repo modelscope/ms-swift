5534d7f1e0230d404f6910359c4fa272a71278b3
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2025-03-15 13:50:25 +0800

    update

--- tests/megatron/test_train.py
+++ tests/megatron/test_train.py
@@ -1,6 +1,6 @@
 import os
 
-os.environ['CUDA_VISIBLE_DEVICES'] = '2,3'
+os.environ['CUDA_VISIBLE_DEVICES'] = '0,1,2,3'
 
 
 def test_train():
@@ -9,7 +9,7 @@ def test_train():
         MegatronTrainArguments(
             megatron_model='Qwen2-7B-Instruct-megatron',
             dataset=['AI-ModelScope/alpaca-gpt4-data-zh#500'],
-            tensor_model_parallel_size=2,
+            tensor_model_parallel_size=4,
             train_iters=200,
             eval_iters=5))
 
