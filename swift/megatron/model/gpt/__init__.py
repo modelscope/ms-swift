04fe85a3409f8954911506a2e9130a71541e866d
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2025-03-16 18:40:31 +0800

    update

--- swift/megatron/model/gpt/__init__.py
+++ swift/megatron/model/gpt/__init__.py
@@ -8,5 +8,6 @@ from .mcore2hf import convert_mcore2hf
 from .model import model_provider
 
 register_megatron_model(
-    MegatronModelMeta(MegatronModelType.gpt, [ModelType.qwen, ModelType.qwen2, ModelType.qwen2_5], model_provider,
-                      load_qwen_config, convert_mcore2hf, convert_hf2mcore))
+    MegatronModelMeta(MegatronModelType.gpt, [
+        ModelType.qwen, ModelType.qwen2, ModelType.qwen2_5, ModelType.qwq, ModelType.qwq_preview, ModelType.qwen2_5_math
+    ], model_provider, load_qwen_config, convert_mcore2hf, convert_hf2mcore))
