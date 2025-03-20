c3fb9dbd0c827889821345dc8189b2ba0a2212a1
tastelikefeet <58414341+tastelikefeet@users.noreply.github.com>
2025-03-20 14:02:10 +0800

    Fix lmdeploy 0.7.3 (#3584)

--- swift/llm/infer/infer_engine/utils.py
+++ swift/llm/infer/infer_engine/utils.py
@@ -265,8 +265,12 @@ def patch_lmdeploy(load_weights=False):
             if not load_weights:
                 for _ in e.map(self.model_comm.process_weight, self.gpu_list, ranks):
                     pass
-            for _ in e.map(self.model_comm.create_engine, self.gpu_list, ranks, repeat(self.nccl_params)):
-                pass
+            if version.parse(lmdeploy.__version__) < version.parse('0.7.2'):
+                for _ in e.map(self.model_comm.create_engine, self.gpu_list, ranks, repeat(self.nccl_params)):
+                    pass
+            else:
+                for _ in e.map(self.model_comm.create_engine, self.gpu_list, ranks):
+                    pass
 
     def _create_weight(self, model_comm):
         """Allocate weight buffer, load params if from_workspace."""
@@ -274,7 +278,8 @@ def patch_lmdeploy(load_weights=False):
         # TODO: support mpi
         self.node_id = 0
         self.node_num = 1
-        self.nccl_params = model_comm.create_nccl_params(self.node_id)
+        if version.parse(lmdeploy.__version__) < version.parse('0.7.2'):
+            self.nccl_params = model_comm.create_nccl_params(self.node_id)
         torch.cuda.synchronize()
 
         # create weight
