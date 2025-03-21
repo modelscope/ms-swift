739df79b30cb340bb899331260c9e53fb97ded7d
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2025-03-21 09:25:48 +0800

    update

--- swift/megatron/utils/patcher.py
+++ swift/megatron/utils/patcher.py
@@ -1,6 +1,6 @@
 # Copyright (c) Alibaba, Inc. and its affiliates.
 from megatron.training import get_args, global_vars, initialize, training
-
+from megatron.core.dist_checkpointing.strategies.torch import TorchDistSaveShardedStrategy
 from swift.utils import get_logger
 
 logger = get_logger()
@@ -13,3 +13,14 @@ def patch_megatron_tokenizer(tokenizer):
         return tokenizer
 
     global_vars.build_tokenizer = build_tokenizer
+
+
+def patch_torch_dist_shard(thread_count):
+    __init__ = TorchDistSaveShardedStrategy.__init__
+
+    def __new_init__(*args, **kwargs):
+        if 'thread_count' not in kwargs:
+            kwargs['thread_count'] = thread_count
+        return __init__(*args, **kwargs)
+
+    TorchDistSaveShardedStrategy.__init__ = __new_init__
