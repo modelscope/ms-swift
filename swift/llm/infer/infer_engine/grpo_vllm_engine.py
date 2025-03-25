0ee9bbf2d63c3367322830663a392bdac1239ef5
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2025-03-25 19:30:23 +0800

    update

--- swift/llm/infer/infer_engine/grpo_vllm_engine.py
+++ swift/llm/infer/infer_engine/grpo_vllm_engine.py
@@ -14,7 +14,7 @@ from .utils import AdapterRequest, patch_vllm_memory_leak
 
 try:
     # After setting the environment variables, import vllm. This way of writing allows lint to pass.
-    os.environ['VLLM_USE_V1'] = '1'
+    os.environ['VLLM_USE_V1'] = '0'
     os.environ['VLLM_WORKER_MULTIPROC_METHOD'] = 'spawn'
     os.environ['VLLM_ENGINE_ITERATION_TIMEOUT_S'] = '3600'
     import vllm
