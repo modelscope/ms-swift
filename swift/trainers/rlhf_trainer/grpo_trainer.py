a77d171accec6288bd7e6608c7a8c26e924714bb
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2025-03-25 14:06:45 +0800

    fix grpo pt ddp

--- swift/trainers/rlhf_trainer/grpo_trainer.py
+++ swift/trainers/rlhf_trainer/grpo_trainer.py
@@ -698,6 +698,8 @@ class GRPOTrainer(RLHFTrainerMixin, SwiftMixin, HFGRPOTrainer):
                     _input['messages'].append({'role': 'assistant', 'content': choice.message.content})
                     _choices.append(_input['messages'])
                 outputs.append(_choices)
+            if isinstance(outputs[0][0], list):
+                outputs = [output[0] for output in outputs]
             assert len(outputs) == prompt_lens
             assert all([len(o) == self.args.tensor_parallel_size for o in outputs])
             return outputs
