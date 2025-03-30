f65a1c52e2935d8619e88a5f2a72678b877fdb2f
hjh <hujinghan.hjh@alibaba-inc.com>
2025-03-31 00:18:14 +0800

    fix

--- swift/trainers/rlhf_trainer/__init__.py
+++ swift/trainers/rlhf_trainer/__init__.py
@@ -7,4 +7,4 @@ from .orpo_trainer import ORPOTrainer
 from .ppo_trainer import PPOTrainer
 from .reward_trainer import RewardTrainer
 from .rlhf_mixin import RLHFTrainerMixin
-from .utils import patch_lora_merge, round_robin, patch_lora_unmerge
+from .utils import patch_lora_merge, patch_lora_unmerge, round_robin
