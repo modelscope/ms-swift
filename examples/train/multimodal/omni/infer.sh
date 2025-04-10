fcf4215f8e3e0baa8e33e4bdcaf9e526de2ded05
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2025-04-10 18:46:59 +0800

    update

--- examples/train/multimodal/omni/infer.sh
+++ examples/train/multimodal/omni/infer.sh
@@ -2,6 +2,7 @@ CUDA_VISIBLE_DEVICES=0 \
 VIDEO_MAX_PIXELS=50176 \
 FPS_MAX_FRAMES=12 \
 MAX_PIXELS=1003520 \
+ENABLE_AUDIO_OUTPUT=0 \
 swift infer \
     --adapters output/vx-xxx/checkpoint-xxx \
     --stream true \
