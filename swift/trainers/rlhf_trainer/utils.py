490874b21d76137a24b7c95f893ea232c66c3b58
hjh <hujinghan.hjh@alibaba-inc.com>
2025-04-12 13:13:48 +0800

    rm nanstd

--- swift/trainers/rlhf_trainer/utils.py
+++ swift/trainers/rlhf_trainer/utils.py
@@ -153,21 +153,3 @@ def _split_into_mini_batches(batch: List, mini_batch_size: int) -> List[List]:
         mini_batch = batch[i:i + mini_batch_size]
         mini_batches.append(mini_batch)
     return mini_batches
-
-
-def nanstd(tensor: torch.Tensor) -> torch.Tensor:
-    """
-    Compute the standard deviation of a tensor, ignoring NaNs. This function only supports 1D tensors.
-
-    Args:
-        tensor (`torch.Tensor`):
-            Input tensor of shape `(N,)`.
-
-    Returns:
-        `torch.Tensor`:
-            Standard deviation of the tensor, ignoring NaNs.
-    """
-    variance = torch.nanmean((tensor - torch.nanmean(tensor, keepdim=True))**2)  # Compute variance ignoring NaNs
-    count = torch.sum(~torch.isnan(tensor))  # Count of non-NaN values
-    variance *= count / (count - 1)  # Bessel's correction
-    return torch.sqrt(variance)
