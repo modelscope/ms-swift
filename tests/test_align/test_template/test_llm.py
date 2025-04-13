ee9f7e77651066b6ab99a582e696fc5202db4a5f
hjh <hujinghan.hjh@alibaba-inc.com>
2025-04-13 23:28:04 +0800

    update

--- tests/test_align/test_template/test_llm.py
+++ tests/test_align/test_template/test_llm.py
@@ -75,6 +75,17 @@ def test_glm4():
     _infer_model(pt_engine)
 
 
+def test_glm4_0414():
+    # The Jinja prompt is missing \n.
+    models = ['ZhipuAI/GLM-4-Z1-9B-0414', 'ZhipuAI/GLM-4-Z1-32B-0414']
+    for model in models:
+        pt_engine = PtEngine('ZhipuAI/glm-4-9b-chat')
+        response1 = _infer_model(pt_engine)
+        pt_engine.default_template.template_backend = 'jinja'
+        response2 = _infer_model(pt_engine)
+        assert response1 == response2
+
+
 def test_qwq():
     pt_engine = PtEngine('Qwen/QwQ-32B-Preview')
     response = _infer_model(pt_engine)
@@ -397,4 +408,5 @@ if __name__ == '__main__':
     # test_baichuan_m1()
     # test_moonlight()
     # test_ling()
-    test_gemma3()
+    # test_gemma3()
+    test_glm4_0414()
