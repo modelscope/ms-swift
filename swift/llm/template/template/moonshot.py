96b04c11c93f317a66076c012082a0f469e8982b
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2025-04-15 17:37:47 +0800

    update

--- swift/llm/template/template/moonshot.py
+++ swift/llm/template/template/moonshot.py
@@ -9,7 +9,7 @@ from ..base import Template
 from ..constant import LLMTemplateType, MLLMTemplateType
 from ..register import TemplateMeta, register_template
 from ..template_inputs import StdTemplateInputs
-from ..utils import Context, Prompt
+from ..utils import Context, Prompt, findall
 
 
 @dataclass
@@ -28,6 +28,7 @@ register_template(MoonlightTemplateMeta(LLMTemplateType.moonlight))
 
 
 class KimiVLTemplate(Template):
+    placeholder_tokens = ['<|media_pad|>']
 
     def replace_tag(self, media_type: Literal['image', 'video', 'audio'], index: int,
                     inputs: StdTemplateInputs) -> List[Context]:
@@ -36,12 +37,30 @@ class KimiVLTemplate(Template):
 
     def _encode(self, inputs: StdTemplateInputs) -> Dict[str, Any]:
         encoded = super()._encode(inputs)
+        input_ids = encoded['input_ids']
+        labels = encoded['labels']
+        media_token = self._tokenize('<|media_pad|>')[0]
+        idx_list = findall(input_ids, media_token)
+        if inputs.images:
+            image_processor = self.processor.image_processor
+            image_inputs = image_processor(inputs.images, return_tensors='pt')
+            image_grid_hws = image_inputs['image_grid_hws']
+            merge_length = image_processor.merge_kernel_size[0] * image_processor.merge_kernel_size[1]
+
+            def _get_new_tokens(i):
+                token_len = (image_grid_hws[i].prod() // merge_length)
+                return [media_token] * token_len
+
+            input_ids, labels = self._extend_tokens(input_ids, labels, idx_list, _get_new_tokens)
+            encoded['input_ids'] = input_ids
+            encoded['labels'] = labels
+            encoded.update(image_inputs)
         return encoded
 
     def _data_collator_mm_data(self, batch: List[Dict[str, Any]]) -> Dict[str, Any]:
         res = super()._data_collator_mm_data(batch)
         image_grid_hws = self.concat_tensor(batch, 'image_grid_hws', 0)
-        if image_grid_hws:
+        if image_grid_hws is not None:
             res['image_grid_hws'] = image_grid_hws
         return res
 
