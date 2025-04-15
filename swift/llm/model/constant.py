5daf54737ae81227087fae5efead6156ebd8f935
jinghanhu <hjhcs@zju.edu.cn>
2025-04-15 10:08:34 +0800

    support glm4-z1 (#3862)
    * update
    
    * fix
    
    * wip
    
    * template
    
    * fix
    
    * update template
    
    * revert template
    
    * update
    
    * more model
    
    * fix
    
    * fix
    
    * rumination
    
    * rename models
    
    * models
    
    ---------
    
    Co-authored-by: hjh <hujinghan.hjh@alibaba-inc.com>

--- swift/llm/model/constant.py
+++ swift/llm/model/constant.py
@@ -38,6 +38,9 @@ class LLMModelType:
     chatglm2 = 'chatglm2'
     chatglm3 = 'chatglm3'
     glm4 = 'glm4'
+    glm4_z1 = 'glm4_z1'
+    glm4_z1_rumination = 'glm4_z1_rumination'
+
     glm_edge = 'glm_edge'
     codefuse_codegeex2 = 'codefuse_codegeex2'
     codegeex4 = 'codegeex4'
--- swift/llm/template/constant.py
+++ swift/llm/template/constant.py
@@ -35,6 +35,8 @@ class LLMTemplateType:
 
     chatglm2 = 'chatglm2'
     glm4 = 'glm4'
+    glm4_z1 = 'glm4_z1'
+    glm4_z1_rumination = 'glm4_z1_rumination'
     codegeex4 = 'codegeex4'
     longwriter_llama = 'longwriter_llama'
 
