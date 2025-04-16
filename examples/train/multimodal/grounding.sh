e3011120d6276f94eb7b4918290ffef10e700709
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2025-04-16 15:10:46 +0800

    update

--- examples/train/multimodal/grounding.sh
+++ examples/train/multimodal/grounding.sh
@@ -3,7 +3,7 @@
 CUDA_VISIBLE_DEVICES=0 \
 MAX_PIXELS=1003520 \
 swift sft \
-    --model Qwen/Qwen2.5-VL-7B-Instruct \
+    --model Qwen/Qwen2-VL-7B-Instruct \
     --dataset 'AI-ModelScope/coco#20000' \
     --train_type lora \
     --torch_dtype bfloat16 \
