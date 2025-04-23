fb306eac633ec3396e1c4b612d066b93cdf54df5
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2025-04-23 12:57:01 +0800

    update

--- examples/deploy/agent/server.sh
+++ examples/deploy/agent/server.sh
@@ -4,4 +4,5 @@ CUDA_VISIBLE_DEVICES=0 swift deploy \
     --gpu_memory_utilization 0.9 \
     --max_model_len 8192 \
     --max_new_tokens 2048 \
+    --agent_template hermes \
     --served_model_name Qwen2.5-7B-Instruct
