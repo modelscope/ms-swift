ad516596dd7ccfb691e164bf74d0828aa126bfe9
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2025-04-23 18:21:35 +0800

    update

--- swift/llm/template/base.py
+++ swift/llm/template/base.py
@@ -948,8 +948,8 @@ class Template(ProcessorMixin):
             query_role, query = query_message['role'], query_message['content']
             response_role, response = response_message['role'], response_message['content']
             # TODO: Optimize the Template mechanism.
-            assert query_role in {'user', 'tool'}
-            assert response_role in {'assistant'}
+            assert query_role in {'user', 'tool'}, f'query_role: {query_role}'
+            assert response_role in {'assistant'}, f'response_role: {response_role}'
             if query_role == 'tool':
                 prompt = query
                 query = ''
