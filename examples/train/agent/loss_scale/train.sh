f39f987459ad10e2c319c03b6a2792e3ec72c8c6
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2025-04-23 17:03:03 +0800

    update

--- examples/train/agent/loss_scale/train.sh
+++ examples/train/agent/loss_scale/train.sh
@@ -1,10 +1,9 @@
-# 24GB
+# 20GB
 CUDA_VISIBLE_DEVICES=0 \
 swift sft \
     --model Qwen/Qwen2.5-3B \
-    --template default \
     --train_type lora \
-    --dataset AI-ModelScope/function-calling-chatml \
+    --dataset AI-ModelScope/function-calling-chatml#10000 \
     --loss_scale hermes \
     --agent_template hermes \
     --torch_dtype bfloat16 \
@@ -12,19 +11,17 @@ swift sft \
     --per_device_train_batch_size 1 \
     --per_device_eval_batch_size 1 \
     --learning_rate 1e-4 \
---lora_rank 8 \
+    --lora_rank 8 \
     --lora_alpha 32 \
-    --target_modules all-linear \
-    --gradient_accumulation_steps 8 \
+    --target_modules all-linear all-embedding lm_head \
+    --gradient_accumulation_steps 16 \
     --eval_steps 100 \
     --save_steps 100 \
     --save_total_limit 2 \
     --logging_steps 5 \
-    --max_length 8192 \
-    --packing true \
+    --max_length 2048 \
     --use_liger_kernel true \
     --output_dir output \
     --warmup_ratio 0.05 \
-    --attn_impl flash_attn \
     --dataloader_num_workers 4 \
     --dataset_num_proc 16
