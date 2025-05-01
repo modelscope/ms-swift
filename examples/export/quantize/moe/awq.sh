1b00459b528efc1bdb8d34ae44596f474a0d6498
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2025-05-01 22:44:03 +0800

    update

--- examples/export/quantize/moe/awq.sh
+++ examples/export/quantize/moe/awq.sh
@@ -6,7 +6,7 @@ swift export \
     --model Qwen/Qwen3-30B-A3B \
     --dataset 'swift/Qwen3-SFT-Mixin' \
     --device_map auto \
-    --quant_n_samples 32 \
+    --quant_n_samples 64 \
     --quant_batch_size -1 \
     --max_length 8192 \
     --quant_method awq \
