6806521531df3fc974aa06b8eab2f2d3752a38c9
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2025-05-13 21:36:45 +0800

    update

--- tests/megatron/test_rlhf.py
+++ tests/megatron/test_rlhf.py
@@ -2,14 +2,13 @@ import os
 
 os.environ['CUDA_VISIBLE_DEVICES'] = '0,1'
 
+
 def test_dpo():
     from swift.megatron import megatron_rlhf_main, MegatronRLHFArguments
     megatron_rlhf_main(
         MegatronRLHFArguments(
             load='Qwen2.5-3B-Instruct-mcore',
-            dataset=[
-                'hjh0119/shareAI-Llama3-DPO-zh-en-emoji#1000'
-            ],
+            dataset=['hjh0119/shareAI-Llama3-DPO-zh-en-emoji#1000'],
             tensor_model_parallel_size=2,
             train_iters=100,
             eval_iters=5,
