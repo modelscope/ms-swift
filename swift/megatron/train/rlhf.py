3a21dcaac8909573c397673381021c9dd066d984
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2025-05-13 16:23:33 +0800

    update

--- swift/megatron/train/rlhf.py
+++ swift/megatron/train/rlhf.py
@@ -1,16 +1,8 @@
 # Copyright (c) Alibaba, Inc. and its affiliates.
-import os
 from typing import List, Union
-
-from megatron.core.enums import ModelType
-from megatron.training import pretrain
-
-from swift.utils import get_logger, is_master, plot_images
+from swift.utils import get_logger
 from ..argument import MegatronRLHFArguments
-from ..utils import patch_megatron_tokenizer
-from .patcher import patch_megatron_data_collator, patch_training_log
 from .sft import MegatronSft
-from .utils import build_streaming_dataloader, forward_step, get_swift_datasets_provider
 
 logger = get_logger()
 
