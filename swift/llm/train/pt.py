42bad995e4811797841e400b2dfcf4e9eabd9db3
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2025-05-15 19:02:12 +0800

    update

--- swift/llm/train/pt.py
+++ swift/llm/train/pt.py
@@ -1,8 +1,11 @@
 # Copyright (c) Alibaba, Inc. and its affiliates.
 from typing import List, Union
 
+from swift.utils import get_logger
 from ..argument import TrainArguments
 from .sft import SwiftSft
+
+logger = get_logger()
 
 
 class SwiftPt(SwiftSft):
@@ -12,6 +15,8 @@ class SwiftPt(SwiftSft):
     def _prepare_template(self) -> None:
         self.args.use_chat_template = False
         self.args.loss_scale = 'all'
+        logger.info('Setting args.use_chat_template: False')
+        logger.info("Setting args.loss_scale: 'all'")
         super()._prepare_template()
 
 
