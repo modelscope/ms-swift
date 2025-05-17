8636c674e95909026b483cc88769a05ee7cffee0
hjh0119 <hujinghan.hjh@alibaba-inc.com>
2025-05-17 22:37:08 +0800

    set default engine

--- swift/llm/infer/infer_engine/vllm_engine.py
+++ swift/llm/infer/infer_engine/vllm_engine.py
@@ -23,7 +23,6 @@ from .utils import AdapterRequest, InferStreamer, patch_npu_vllm
 
 try:
     # After setting the environment variables, import vllm. This way of writing allows lint to pass.
-    os.environ['VLLM_USE_V1'] = os.environ.get('VLLM_USE_V1', '0')
     os.environ['VLLM_WORKER_MULTIPROC_METHOD'] = 'spawn'
     os.environ['VLLM_ENGINE_ITERATION_TIMEOUT_S'] = '3600'
     import vllm
