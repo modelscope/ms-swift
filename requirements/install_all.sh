ec6043b8db978a765586a2ed12d521d5e253bfa0
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2025-05-24 19:04:09 +0800

    fix deepspeed

--- requirements/install_all.sh
+++ requirements/install_all.sh
@@ -6,7 +6,7 @@ pip install autoawq -U --no-deps
 pip install auto_gptq optimum bitsandbytes -U
 pip install git+https://github.com/modelscope/ms-swift.git
 pip install timm -U
-pip install "deepspeed==0.14.5"
+pip install deepspeed -U
 pip install qwen_vl_utils qwen_omni_utils decord librosa icecream soundfile -U
 pip install liger_kernel nvitop pre-commit -U
 # flash-attn: https://github.com/Dao-AILab/flash-attention/releases
