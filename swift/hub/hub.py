dcc3276c2c8f25446f7c357f85d78945d936212a
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2025-05-30 15:40:26 +0800

    update

--- swift/hub/hub.py
+++ swift/hub/hub.py
@@ -1,4 +1,5 @@
 # Copyright (c) Alibaba, Inc. and its affiliates.
+import logging
 import os
 import tempfile
 from contextlib import contextmanager
@@ -11,11 +12,11 @@ from huggingface_hub import RepoUrl
 from huggingface_hub.hf_api import api, future_compatible
 from requests.exceptions import HTTPError
 from transformers import trainer
-from transformers.utils import logging, strtobool
+from transformers.utils import strtobool
 
-from swift.utils import ms_logger_context, use_hf_hub
+from swift.utils import get_logger, ms_logger_context, use_hf_hub
 
-logger = logging.get_logger(__name__)
+logger = get_logger()
 
 
 class HubOperation:
