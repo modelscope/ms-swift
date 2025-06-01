6d29ab5c45119e131a0072421d3257838b233e90
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2025-06-01 16:30:54 +0800

    fix qwen2_5_vl_awq

--- swift/llm/model/model/qwen.py
+++ swift/llm/model/model/qwen.py
@@ -583,13 +583,14 @@ def get_model_tokenizer_qwen2_vl(*args, **kwargs):
     kwargs['automodel_class'] = kwargs['automodel_class'] or Qwen2VLForConditionalGeneration
     model, tokenizer = get_model_tokenizer_multimodal(*args, **kwargs)
     if model is not None:
-        if hasattr(model.model, 'embed_tokens'):
-            embed_tokens = model.model.embed_tokens
+        base_model = model.model if 'AWQ' in model.__class__.__name__ else model
+        if hasattr(base_model.model, 'embed_tokens'):
+            embed_tokens = base_model.model.embed_tokens
         else:
-            embed_tokens = model.model.language_model.embed_tokens
+            embed_tokens = base_model.model.language_model.embed_tokens
         patch_output_clone(embed_tokens)
         patch_output_to_input_device(embed_tokens)
-        patch_get_input_embeddings(model.visual, 'patch_embed')
+        patch_get_input_embeddings(base_model.visual, 'patch_embed')
 
     from qwen_vl_utils import vision_process
     patch_qwen_vl_utils(vision_process)
