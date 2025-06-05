17cdc1e228e009af74358cf9f8c46a8cfaaf9a6d
tastelikefeet <58414341+tastelikefeet@users.noreply.github.com>
2025-06-05 15:23:13 +0800

    Fix multi modal bugs in ulysses (#4484)
    ---------
    
    Co-authored-by: tastelikefeet <yuze.zyz@alibaab-inc.com>

--- swift/trainers/sequence_parallel/ulysses.py
+++ swift/trainers/sequence_parallel/ulysses.py
@@ -651,14 +651,12 @@ class Ulysses(SequenceParallel):
             inputs_embeds = kwargs.get('inputs_embeds', None)
             position_ids = kwargs['position_ids']
             attention_mask = kwargs.get('attention_mask', None)
+            if hasattr(_self, 'language_model'):
+                embed_tokens = getattr(_self.language_model, 'embed_tokens', None)
+            else:
+                embed_tokens = getattr(_self, 'embed_tokens', None)
             _input_ids, inputs_embeds, _, position_ids, attention_mask, _ = self.pad_and_split_inputs(
-                input_ids,
-                inputs_embeds,
-                None,
-                position_ids,
-                attention_mask,
-                None,
-                embed_tokens=getattr(_self, 'embed_tokens', None))
+                input_ids, inputs_embeds, None, position_ids, attention_mask, None, embed_tokens=embed_tokens)
             kwargs['input_ids'] = _input_ids
             kwargs['inputs_embeds'] = inputs_embeds
             kwargs['position_ids'] = position_ids
@@ -824,15 +822,16 @@ class Ulysses(SequenceParallel):
             raise ValueError('Trainer: training requires a train_dataset.')
 
         trainer.ulysses = self
-        if trainer.__class__.__name__ in ('Seq2SeqTrainer', 'DPOTrainer'):
+        if trainer.__class__.__name__ == 'Seq2SeqTrainer':
             trainer.compute_loss_func = partial(loss_scale_sp_func, ulysses=self)
-            if trainer.__class__.__name__ == 'DPOTrainer':
-                trainer.get_per_token_logps = MethodType(partial(get_per_token_logps, ulysses=self), trainer)
 
-                def rlhf_loss_scale_sp_func(_, *args, **kwargs):
-                    return loss_scale_sp_func(*args, ulysses=self, **kwargs)
+        elif trainer.__class__.__name__ == 'DPOTrainer':
+            trainer.get_per_token_logps = MethodType(partial(get_per_token_logps, ulysses=self), trainer)
 
-                trainer.get_nll_loss = MethodType(rlhf_loss_scale_sp_func, trainer)
+            def rlhf_loss_scale_sp_func(_, *args, **kwargs):
+                return loss_scale_sp_func(*args, ulysses=self, **kwargs)
+
+            trainer.get_nll_loss = MethodType(rlhf_loss_scale_sp_func, trainer)
 
         elif trainer.__class__.__name__ == 'GRPOTrainer':
             assert version.parse(trl.__version__) >= version.parse('0.18.0')
