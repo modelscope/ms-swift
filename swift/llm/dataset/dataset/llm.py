518061b5a2b3a99641f789c50ea1f2cf35a315b2
tastelikefeet <yuze.zyz@alibaab-inc.com>
2025-06-06 14:42:00 +0800

    add stsb positive subset

--- swift/llm/dataset/dataset/llm.py
+++ swift/llm/dataset/dataset/llm.py
@@ -325,13 +325,20 @@ register_dataset(
 
 class StsbPreprocessor(ResponsePreprocessor):
 
+    def __init__(self, sim_threshold: Optional[float] = None):
+        self.sim_threshold = sim_threshold
+        super().__init__()
+
     def preprocess(self, row: Dict[str, Any]) -> Dict[str, Any]:
         row = {
             'query': row['sentence1'],
             'response': row['sentence2'],
             'label': row['score'],
         }
-        return super().preprocess(row)
+        if self.sim_threshold is None or float(row['label']) >= self.sim_threshold:
+            return super().preprocess(row)
+        else:
+            return None
 
 
 class StsbGeneratePreprocessor(ResponsePreprocessor):
@@ -364,6 +371,7 @@ register_dataset(
         hf_dataset_id='sentence-transformers/stsb',
         subsets=[
             SubsetDataset('default', preprocess_func=StsbPreprocessor()),  # embedding
+            SubsetDataset('positive', preprocess_func=StsbPreprocessor(sim_threshold=0.75)),  # infonce
             SubsetDataset('generate', preprocess_func=StsbGeneratePreprocessor()),
             SubsetDataset('reg', preprocess_func=StsbRegressionPreprocessor()),
         ],
