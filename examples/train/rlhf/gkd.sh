c92c7ac77b8ddb5cfaa5c3a5d02ae4a6fdb0342c
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2025-06-17 15:20:43 +0800

    update

--- examples/train/rlhf/gkd.sh
+++ examples/train/rlhf/gkd.sh
@@ -1,12 +1,11 @@
-# 4 * 80GiB
+# 4 * 65GiB
 NPROC_PER_NODE=4 \
 PYTORCH_CUDA_ALLOC_CONF='expandable_segments:True' \
 CUDA_VISIBLE_DEVICES=0,1,2,3 \
 swift rlhf \
     --rlhf_type gkd \
-    --model Qwen/Qwen3-4B-Base \
-    --teacher_model Qwen/Qwen3-8B \
-    --lmbda 0 \
+    --model Qwen/Qwen2.5-7B \
+    --teacher_model Qwen/Qwen2.5-14B-Instruct \
     --train_type full \
     --dataset 'AI-ModelScope/alpaca-gpt4-data-en#2000' 'AI-ModelScope/alpaca-gpt4-data-zh#2000' \
     --seq_kd true \
@@ -20,12 +19,12 @@ swift rlhf \
     --save_steps 50 \
     --save_total_limit 2 \
     --logging_steps 5 \
-    --max_length 4096 \
-    --max_completion_length 2048 \
+    --max_length 2048 \
+    --max_completion_length 512 \
     --output_dir output \
     --warmup_ratio 0.05 \
     --save_only_model true \
     --dataloader_num_workers 4 \
     --dataset_num_proc 4 \
-    --deepspeed zero2 \
+    --deepspeed zero3 \
     --attn_impl flash_attn
