0fa72f26b8be97ebaced0e46e64952efecfeff69
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2025-06-19 13:42:36 +0800

    update

--- examples/infer/sglang/tp.sh
+++ examples/infer/sglang/tp.sh
@@ -2,7 +2,8 @@ CUDA_VISIBLE_DEVICES=0,1 \
 swift infer \
     --model Qwen/Qwen3-8B \
     --infer_backend sglang \
-    --stream true \
+    --val_dataset AI-ModelScope/alpaca-gpt4-data-zh#2000 \
     --max_new_tokens 2048 \
     --sglang_context_length 8192 \
-    --sglang_tp_size 2
+    --sglang_tp_size 2 \
+    --write_batch_size 1000
