f3eb95b010cc522d075494b05c8ec4ab0ef20dad
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2025-06-21 19:26:41 +0800

    update readme & shell

--- docs/source_en/Instruction/Command-line-parameters.md
+++ docs/source_en/Instruction/Command-line-parameters.md
@@ -417,7 +417,7 @@ RLHF arguments inherit from the [training arguments](#training-arguments).
 - undesirable_weight: Loss weight $\lambda_U$ for undesirable response in the KTO algorithm, default is `1.`.
 - loss_scale: Override template arguments, default is 'last_round'.
 - temperature: Default is 0.9; this parameter will be used in PPO, GRPO and GKD.
-- lmbda: Default is 0.5. This parameter is used in GKD. It is the lambda parameter that controls the student data fraction (i.e., the proportion of on-policy student-generated outputs).
+- lmbda: Default is 0.5. This parameter is used in GKD. It is the lambda parameter that controls the student data fraction (i.e., the proportion of on-policy student-generated outputs). If lambda is 0, the student-generated data is not used at all.
 - seq_kd: Default is False. This parameter is used in GKD. It is the `seq_kd` parameter that controls whether to perform Sequence-Level KD (can be viewed as supervised fine-tuning on teacher-generated output).
   - Note: You can perform inference on the dataset using the teacher model in advance (accelerated by inference engines such as vLLM, SGLang, or lmdeploy), and set `seq_kd` to False during training. Alternatively, you can set `seq_kd` to True, which will use the teacher model to generate sequences during training (ensuring different generated data across multiple epochs, but at a slower efficiency).
 
--- examples/train/rlhf/gkd/fast.sh
+++ examples/train/rlhf/gkd/fast.sh
@@ -1,14 +1,29 @@
-# 4 * 65GiB
+
+export teacher_model='Qwen/Qwen2.5-14B-Instruct'
+
+NPROC_PER_NODE=4 \
+CUDA_VISIBLE_DEVICES=0,1,2,3 \
+swift infer \
+    --model $teacher_model \
+    --infer_backend vllm \
+    --val_dataset 'AI-ModelScope/alpaca-gpt4-data-en#5000' 'AI-ModelScope/alpaca-gpt4-data-zh#5000' \
+    --gpu_memory_utilization 0.9 \
+    --max_model_len 8192 \
+    --max_new_tokens 2048 \
+    --write_batch_size 10000 \
+    --result_path new_dataset.jsonl
+
+
+# 4 * 52GiB, 3.81s/it
 NPROC_PER_NODE=4 \
 PYTORCH_CUDA_ALLOC_CONF='expandable_segments:True' \
 CUDA_VISIBLE_DEVICES=0,1,2,3 \
 swift rlhf \
     --rlhf_type gkd \
     --model Qwen/Qwen2.5-7B \
-    --teacher_model Qwen/Qwen2.5-14B-Instruct \
+    --teacher_model $teacher_model \
     --train_type full \
-    --dataset 'AI-ModelScope/alpaca-gpt4-data-en#2000' 'AI-ModelScope/alpaca-gpt4-data-zh#2000' \
-    --seq_kd true \
+    --dataset 'new_dataset.jsonl' \
     --torch_dtype bfloat16 \
     --num_train_epochs 1 \
     --per_device_train_batch_size 4 \
@@ -20,11 +35,11 @@ swift rlhf \
     --save_total_limit 2 \
     --logging_steps 5 \
     --max_length 2048 \
-    --max_completion_length 512 \
     --output_dir output \
     --warmup_ratio 0.05 \
     --save_only_model true \
     --dataloader_num_workers 4 \
     --dataset_num_proc 4 \
     --deepspeed zero3 \
-    --attn_impl flash_attn
+    --attn_impl flash_attn \
+    --lmbda 0
