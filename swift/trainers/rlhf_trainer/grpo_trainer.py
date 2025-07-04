7290409d2f140b1d249993f837c07aa06278aea6
aacedar <149222459+aacedar@users.noreply.github.com>
2025-07-04 17:18:43 +0800

    Update grpo_trainer.py for special token encode error for deepseek model

--- swift/trainers/rlhf_trainer/grpo_trainer.py
+++ swift/trainers/rlhf_trainer/grpo_trainer.py
@@ -1094,7 +1094,19 @@ class GRPOTrainer(RLHFTrainerMixin, SwiftMixin, HFGRPOTrainer):
             InferRequest.remove_response(messages)
             template_inputs, _ = StdTemplateInputs.from_dict({'messages': messages})
             res_context_list, _, _ = self.template._swift_encode(template_inputs)
-            prompts_text.append(''.join(res_context_list))
+            # 类型检查和转换
+            processed_context = []
+            for context in res_context_list:
+                if isinstance(context, str):
+                    processed_context.append(context)
+                elif isinstance(context, list) and all(isinstance(x, int) for x in context):
+                    # 将token ID列表解码为字符串
+                    decoded_text = self.template.tokenizer.decode(context)
+                    processed_context.append(decoded_text)
+                else:
+                    # 其他类型，转换为字符串
+                    processed_context.append(str(context))
+            prompts_text.append(''.join(processed_context))
         return prompts_text
 
     @profiling_decorator
