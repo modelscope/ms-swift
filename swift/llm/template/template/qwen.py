57c4742a9de4636d1415e877ef5b18d4775e931a
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2025-07-07 20:16:19 +0800

    fix qwen3 remove think

--- swift/llm/template/template/qwen.py
+++ swift/llm/template/template/qwen.py
@@ -50,9 +50,13 @@ class ThinkingTemplate(Template):
 
     def _swift_prepare_messages(self, messages):
         super()._swift_prepare_messages(messages)
-        for i, message in enumerate(messages):
-            if message['role'] == 'assistant' and isinstance(message['content'], str) and i != len(messages) - 1:
-                message['content'] = message['content'].split('</think>')[-1].strip()
+        # Only during inference or training, and only if the loss_scale is set to 'last_round',
+        # will the previous 'think' entries be deleted.
+        if not self.is_training or self.loss_scale.name == 'last_round':
+            for i, message in enumerate(messages):
+                # Delete the content before '</think>' in all assistant turns except the last round.
+                if message['role'] == 'assistant' and isinstance(message['content'], str) and i != len(messages) - 1:
+                    message['content'] = message['content'].split('</think>')[-1].strip()
 
 
 register_template(
