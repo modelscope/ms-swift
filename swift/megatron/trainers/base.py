72d208a2487505766ab7652492d54c2d92a509ee
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2025-07-10 22:15:10 +0800

    lint pass

--- swift/megatron/trainers/base.py
+++ swift/megatron/trainers/base.py
@@ -10,12 +10,12 @@ import torch.distributed as dist
 import torch.nn
 from megatron.core import mpu
 from megatron.core.enums import ModelType
-from megatron.training.checkpointing import load_checkpoint
 from megatron.core.num_microbatches_calculator import get_num_microbatches
 from megatron.core.pipeline_parallel import get_forward_backward_func
 from megatron.core.rerun_state_machine import RerunMode, get_rerun_state_machine
 from megatron.core.utils import StragglerDetector
 from megatron.training import ft_integration, get_args, get_timers, is_last_rank, pretrain, print_rank_0, training
+from megatron.training.checkpointing import load_checkpoint
 from packaging import version
 
 from swift.utils import get_logger
@@ -158,7 +158,8 @@ class BaseMegatronTrainer(ABC):
             return model
 
         with self._patch_load_state_dict():
-            model, optimizer, opt_param_scheduler = self._origin_setup_model_and_optimizer(new_model_provider_func, model_type, *_args, **kwargs)
+            model, optimizer, opt_param_scheduler = self._origin_setup_model_and_optimizer(
+                new_model_provider_func, model_type, *_args, **kwargs)
         args = get_args()
         if args.adapter_load is not None:
             with adapter_state_dict_context():
