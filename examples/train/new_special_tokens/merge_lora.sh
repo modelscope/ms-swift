4deec6ff60b2c10c9b7830aca610bf63679b2712
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2025-07-14 16:01:29 +0800

    update

--- examples/train/new_special_tokens/merge_lora.sh
+++ examples/train/new_special_tokens/merge_lora.sh
@@ -9,4 +9,5 @@ CUDA_VISIBLE_DEVICES=0 \
 swift infer \
     --adapters output/vx-xxx/checkpoint-xxx-merged \
     --max_batch_size 16 \
-    --load_data_args true
+    --load_data_args true \
+    --temperature 0
