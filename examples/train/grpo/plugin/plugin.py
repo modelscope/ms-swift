e32ebcee297a9ebef2a07ea9fb1d2f333fd25fc8
jinghanhu <hjhcs@zju.edu.cn>
2025-07-16 14:40:00 +0800

    [doc] update grpo document (#4971)

--- examples/train/grpo/plugin/plugin.py
+++ examples/train/grpo/plugin/plugin.py
@@ -28,7 +28,7 @@ TO CUSTOMIZE REWARD FUNCTION:
 
     Step 3: Configure the Arguments
         Run the script with:
-        --plugin /path/to/plugin.py \
+        --external_plugins /path/to/plugin.py \
         --reward_funcs my_reward_function
 """
 
@@ -721,7 +721,7 @@ TO CUSTOMIZE REWARD MODEL:
 
     Step 3: Configure the Arguments
         Run the script with:
-        --plugin /path/to/plugin.py \
+        --external_plugins /path/to/plugin.py \
         --reward_model_plugin my_rm_plugin
 
 For GenRM you can refer to swift/llm/plugin/rm_plugin/GenRMPlugin
@@ -883,7 +883,7 @@ TO CUSTOMIZE MULTITURN SCHEDULER:
 
     Step 3: Configure the Arguments
         Run the script with:
-        --plugin /path/to/plugin.py \
+        --external_plugins /path/to/plugin.py \
         --multi_turn_scheduler my_scheduler
 """
 
