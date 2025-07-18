291aab438c0b094000162da15484708bf01043c0
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2025-07-18 14:08:24 +0800

    lint pass

--- swift/llm/train/sft.py
+++ swift/llm/train/sft.py
@@ -221,7 +221,7 @@ class SwiftSft(SwiftPipeline, TunerMixin):
         datasets = [train_dataset, val_dataset]
         if not is_grpo:
             origin_template_model = template.model
-            template.model = None   # Avoid serializing the model.
+            template.model = None  # Avoid serializing the model.
             lazy_tokenize = args.lazy_tokenize and not args.packing
             for i, dataset in enumerate(datasets):
                 if dataset is None:
@@ -249,7 +249,7 @@ class SwiftSft(SwiftPipeline, TunerMixin):
                     if args.model_meta.is_multimodal:
                         dataset = LazyLLMDataset(dataset, template.encode)
                 datasets[i] = dataset
-            template.model = origin_template_model 
+            template.model = origin_template_model
             train_dataset, val_dataset = datasets
             if is_master():
                 inputs = train_dataset[0] if hasattr(train_dataset, '__len__') else next(iter(train_dataset))
