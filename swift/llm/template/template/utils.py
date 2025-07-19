ae3328ed1765206a1a61b2142287f202dad9c450
Jintao <huangjintao.hjt@alibaba-inc.com>
2025-07-19 18:58:28 +0800

    [model] support glm4_5_moe (#5015)

--- swift/llm/template/template/utils.py
+++ swift/llm/template/template/utils.py
@@ -2,6 +2,7 @@
 from dataclasses import dataclass, field
 from typing import Optional
 
+from ..base import Template
 from ..constant import LLMTemplateType
 from ..register import TemplateMeta, register_template
 from ..utils import Prompt
@@ -29,3 +30,16 @@ class EmptyTemplateMeta(TemplateMeta):
 
 register_template(ChatmlTemplateMeta(LLMTemplateType.chatml))
 register_template(EmptyTemplateMeta(LLMTemplateType.dummy))
+
+
+class ThinkingTemplate(Template):
+
+    def _swift_prepare_messages(self, messages):
+        super()._swift_prepare_messages(messages)
+        # Only during inference or training, and only if the loss_scale is set to 'last_round',
+        # will the previous 'think' entries be deleted.
+        if not self.is_training or self.loss_scale.name == 'last_round':
+            for i, message in enumerate(messages):
+                # Delete the content before '</think>' in all assistant turns except the last round.
+                if message['role'] == 'assistant' and isinstance(message['content'], str) and i != len(messages) - 1:
+                    message['content'] = message['content'].split('</think>')[-1].strip()
