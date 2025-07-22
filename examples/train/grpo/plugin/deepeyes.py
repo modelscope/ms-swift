27ca49d5ac5b7ef7d52bb3a09982c2bc06fdcd53
hjh0119 <hujinghan.hjh@alibaba-inc.com>
2025-07-22 11:01:22 +0800

    init

---
+++ examples/train/grpo/plugin/deepeyes.py
@@ -0,0 +1,8 @@
+
+"""
+3 dataset file
+
+    1. data_thinklite_reasoning_acc:  data_source == 'thinklite_eureka'
+    2. data_0.1.2_visual_toolbox_v2.parquet : data_source == 'thinklite_eureka'
+    3. 
+"""
