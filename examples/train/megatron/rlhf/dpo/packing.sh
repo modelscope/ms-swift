cefcb35433878823d517522c9110e92258508f38
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2025-08-13 14:54:31 +0800

    update

--- examples/train/megatron/rlhf/dpo/packing.sh
+++ examples/train/megatron/rlhf/dpo/packing.sh
@@ -1,19 +1,14 @@
-# 8 * 75GiB 10.5s/it
+# 4 * 62GiB
 PYTORCH_CUDA_ALLOC_CONF='expandable_segments:True' \
-NPROC_PER_NODE=8 \
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 \
+NPROC_PER_NODE=4 \
+CUDA_VISIBLE_DEVICES=0,1,2,3 \
 megatron rlhf \
     --rlhf_type dpo \
-    --load Qwen3-30B-A3B-Base-mcore \
-    --dataset AI-ModelScope/orpo-dpo-mix-40k \
+    --load Qwen3-8B-Base-mcore \
+    --dataset 'hjh0119/shareAI-Llama3-DPO-zh-en-emoji#20000' \
     --split_dataset_ratio 0.01 \
-    --tensor_model_parallel_size 2 \
-    --expert_tensor_parallel_size 1 \
-    --expert_model_parallel_size 8 \
-    --moe_permute_fusion true \
-    --moe_grouped_gemm true \
-    --moe_shared_expert_overlap true \
-    --moe_aux_loss_coeff 1e-3 \
+    --tensor_model_parallel_size 4 \
+    --packing true \
     --micro_batch_size 1 \
     --global_batch_size 4 \
     --recompute_granularity full \
@@ -25,20 +20,16 @@ megatron rlhf \
     --lr 1e-5 \
     --lr_warmup_fraction 0.05 \
     --min_lr 1e-6 \
-    --save megatron_output/Qwen3-30B-A3B-Base \
+    --save megatron_output/Qwen3-8B-Base \
     --eval_interval 200 \
     --save_interval 200 \
     --max_length 8192 \
-    --packing true \
     --num_workers 8 \
     --dataset_num_proc 8 \
     --no_save_optim true \
     --no_save_rng true \
     --sequence_parallel true \
     --attention_backend flash \
-    --optimizer_cpu_offload true \
-    --use_precision_aware_optimizer true \
-    --optimizer_offload_fraction 1 \
     --beta 0.1 \
     --rpo_alpha 1 \
     --loss_type sigmoid
