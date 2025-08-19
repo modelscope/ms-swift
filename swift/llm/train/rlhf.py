3545f056b43023647ad369573512b048c8c83c27
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2025-08-19 14:38:00 +0800

    fix

--- swift/llm/train/rlhf.py
+++ swift/llm/train/rlhf.py
@@ -159,8 +159,7 @@ class SwiftRLHF(SwiftSft):
             else:
                 tuner = Swift
             assert not args.adapters or len(args.adapters) == 1, f'args.adapters: {args.adapters}'
-            model = tuner.from_pretrained(
-                model, args.resume_from_checkpoint or args.adapters[0], is_trainable=True, adapter_name='ref_model')
+            model = tuner.from_pretrained(model, args.adapters[0], adapter_name='ref_model')
             assert args.rlhf_type in {'dpo', 'kto',
                                       'grpo'}, 'Currently, only DPO、KTO and GRPO support `ref_adapter_name`.'
             args.training_args.ref_adapter_name = 'ref_model'
