42cc6e7e92bdf9541aa2db4524c6d59d366ceb5b
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2025-08-29 13:55:22 +0800

    fix

--- swift/llm/template/vision_utils.py
+++ swift/llm/template/vision_utils.py
@@ -113,7 +113,7 @@ def load_file(path: Union[str, bytes, _T]) -> Union[BytesIO, _T]:
             res = BytesIO(content)
         elif os.path.exists(path) or (not path.startswith('data:') and len(path) <= 200):
             ROOT_IMAGE_DIR = get_env_args('ROOT_IMAGE_DIR', str, None)
-            if not os.path.exists(path) and ROOT_IMAGE_DIR is not None:
+            if ROOT_IMAGE_DIR is not None and not os.path.exists(path):
                 path = os.path.join(ROOT_IMAGE_DIR, path)
             path = os.path.abspath(os.path.expanduser(path))
             with open(path, 'rb') as f:
