6a431aa7a1665c2b2cd53b6cae177e9b0bb2d606
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2025-09-02 18:14:56 +0800

    update

--- examples/megatron/multimodal/lora.sh
+++ examples/megatron/multimodal/lora.sh
@@ -1,4 +1,4 @@
-# 2 * 72GiB; 4.1s/it
+# 2 * 15GiB; 3.8s/it
 PYTORCH_CUDA_ALLOC_CONF='expandable_segments:True' \
 NPROC_PER_NODE=2 \
 MAX_PIXELS=1003520 \
@@ -6,12 +6,16 @@ CUDA_VISIBLE_DEVICES=0,1 \
 megatron sft \
     --load Qwen2.5-VL-7B-Instruct-mcore \
     --dataset 'AI-ModelScope/LaTeX_OCR' \
+    --train_type lora \
+    --lora_rank 8 \
+    --lora_alpha 32 \
+    --target_modules all-linear \
     --tensor_model_parallel_size 2 \
     --sequence_parallel true \
-    --packing true \
     --freeze_llm false \
     --freeze_vit true \
     --freeze_aligner true \
+    --packing true \
     --split_dataset_ratio 0.01 \
     --micro_batch_size 1 \
     --global_batch_size 4 \
@@ -20,9 +24,9 @@ megatron sft \
     --recompute_num_layers 1 \
     --finetune true \
     --cross_entropy_loss_fusion true \
-    --lr 1e-5 \
+    --lr 1e-4 \
     --lr_warmup_fraction 0.05 \
-    --min_lr 1e-6 \
+    --min_lr 1e-5 \
     --max_epochs 1 \
     --save megatron_output/Qwen2.5-VL-7B-Instruct \
     --save_interval 200 \
