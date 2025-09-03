5f5009bd9e86ffe917e02b5cdb7b8550c2c07b1b
hjh0119 <hujinghan.hjh@alibaba-inc.com>
2025-09-03 20:20:05 +0800

    update

--- swift/trainers/rlhf_trainer/grpo_trainer.py
+++ swift/trainers/rlhf_trainer/grpo_trainer.py
@@ -2441,7 +2441,7 @@ class GRPOTrainer(RLHFTrainerMixin, SwiftMixin, HFGRPOTrainer):
             return []
 
         # Define core metadata fields required for all requests
-        REQUEST_METADATA_FIELDS = ['messages', 'images', 'audios', 'videos', 'objects', 'uuid', 'request_id']
+        REQUEST_METADATA_FIELDS = ['messages', 'images', 'audios', 'videos', 'objects', 'uuid']
         requests_dicts = []
 
         for data in inputs:
