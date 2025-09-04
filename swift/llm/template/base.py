bee677132a8bcf97bc5041bb939e73fa775f234e
Jintao <huangjintao.hjt@alibaba-inc.com>
2025-09-04 17:36:58 +0800

    [bugfix] fix padding_free & truncate (#5665)

--- swift/llm/template/base.py
+++ swift/llm/template/base.py
@@ -1203,6 +1203,17 @@ class Template(ProcessorMixin):
             loss_mask = torch.tensor(loss_mask)[protected].tolist()
         return input_ids, labels, loss_mask
 
+    @staticmethod
+    def _get_length(input_ids, labels):
+        # input_ids might be a tensor.
+        lengths = [0]
+        if input_ids is not None:
+            lengths.append(len(input_ids))
+        if labels is not None:
+            lengths.append(len(labels))
+        length = max(lengths)
+        return length
+
     def _encode_truncated(self, inputs: StdTemplateInputs):
         self._preprocess_inputs(inputs)
         if self.mode in {'vllm', 'lmdeploy', 'sglang'}:
@@ -1220,23 +1231,16 @@ class Template(ProcessorMixin):
         input_ids = encoded.get('input_ids')
         labels = encoded.get('labels')
         loss_scale = encoded.get('loss_scale')
-
-        # input_ids might be a tensor.
-        lengths = [0]
-        if input_ids is not None:
-            lengths.append(len(input_ids))
-        if labels is not None:
-            lengths.append(len(labels))
-        length = max(lengths)
-        encoded['length'] = length
-
+        length = self._get_length(input_ids, labels)
         if self.max_length is not None and length > self.max_length:
             if self.truncation_strategy in {'right', 'left'}:
                 input_ids, labels, loss_scale = self._truncate(
                     input_ids, labels, loss_scale, truncation_strategy=self.truncation_strategy)
+                length = self._get_length(input_ids, labels)
             elif self.truncation_strategy == 'raise':
                 raise MaxLengthError(f'Current length of row({length}) is larger'
                                      f' than the max_length({self.max_length}).')
+        encoded['length'] = length
         encoded['input_ids'] = input_ids
         encoded['labels'] = labels
         encoded['loss_scale'] = loss_scale
