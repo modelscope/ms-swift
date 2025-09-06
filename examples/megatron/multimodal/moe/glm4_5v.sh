b01c8dabf3ef481ad4e2240c8fc7820db31618c7
Jintao <huangjintao.hjt@alibaba-inc.com>
2025-09-06 21:52:08 +0800

    [model] support moonshotai/Kimi-K2-Instruct-0905 (#5689)

--- examples/megatron/multimodal/moe/glm4_5v.sh
+++ examples/megatron/multimodal/moe/glm4_5v.sh
@@ -1,9 +1,9 @@
-# 2 * 43GiB, 8s/it
+# 4 * 66GiB, 6.4s/it
 PYTORCH_CUDA_ALLOC_CONF='expandable_segments:True' \
-NPROC_PER_NODE=2 \
-CUDA_VISIBLE_DEVICES=0,1 \
+NPROC_PER_NODE=4 \
+CUDA_VISIBLE_DEVICES=0,1,2,3 \
 megatron sft \
-    --load InternVL3_5-30B-A3B-mcore \
+    --load GLM-4.5V-mcore \
     --dataset 'AI-ModelScope/LaTeX_OCR:human_handwrite#5000' \
     --train_type lora \
     --lora_rank 8 \
@@ -15,13 +15,15 @@ megatron sft \
     --freeze_aligner true \
     --packing true \
     --split_dataset_ratio 0.01 \
-    --expert_model_parallel_size 2 \
+    --tensor_model_parallel_size 4 \
+    --expert_tensor_parallel_size 1 \
+    --expert_model_parallel_size 4 \
     --moe_permute_fusion true \
     --moe_grouped_gemm true \
     --moe_shared_expert_overlap true \
     --moe_aux_loss_coeff 1e-3 \
     --micro_batch_size 1 \
-    --global_batch_size 4 \
+    --global_batch_size 2 \
     --recompute_granularity full \
     --recompute_method uniform \
     --recompute_num_layers 1 \
@@ -31,7 +33,7 @@ megatron sft \
     --lr_warmup_fraction 0.05 \
     --min_lr 1e-5 \
     --max_epochs 1 \
-    --save megatron_output/InternVL3_5-30B-A3B \
+    --save megatron_output/GLM-4.5V-mcore \
     --eval_interval 200 \
     --save_interval 200 \
     --vit_gradient_checkpointing true \
