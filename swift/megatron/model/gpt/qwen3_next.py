52301b6db826f644a26cc4f41235923f0c82d6d9
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2025-09-10 15:17:56 +0800

    update

--- swift/megatron/model/gpt/qwen3_next.py
+++ swift/megatron/model/gpt/qwen3_next.py
@@ -1,5 +1,36 @@
+from megatron.core.models.gpt.gpt_layer_specs import (
+    get_gpt_layer_with_transformer_engine_spec,
+)
+from megatron.core.extensions.transformer_engine import TENorm
+from megatron.core.transformer.module import MegatronModule
+from megatron.core.transformer.spec_utils import ModuleSpec
 
+class Qwen3NextRMSNormGated(MegatronModule):
+    # code borrowed from huggingface/transformers
+    def __init__(self, config, **kwargs):
+        super().__init__()
+        self.weight = nn.Parameter(torch.ones(config.hidden_size))
+        self.variance_epsilon = config.layernorm_epsilon
+
+    def forward(self, hidden_states, gate=None):
+        input_dtype = hidden_states.dtype
+        hidden_states = hidden_states.to(torch.float32)
+        variance = hidden_states.pow(2).mean(-1, keepdim=True)
+        # Norm before gate
+        hidden_states = hidden_states * torch.rsqrt(variance + self.variance_epsilon)
+        hidden_states = self.weight * hidden_states.to(input_dtype)
+        hidden_states = hidden_states * F.silu(gate.to(torch.float32))
+
+        return hidden_states.to(input_dtype)
 
 
 def get_qwen3_next_transformer_layer_spec(config):
-    pass
+    layer_norm_impl = TENorm
+    moe_layer_spec = get_gpt_layer_with_transformer_engine_spec(
+        num_experts=config.num_moe_experts,
+        moe_grouped_gemm=config.moe_grouped_gemm,
+        qk_layernorm=config.qk_layernorm,
+        multi_latent_attention=config.multi_latent_attention,
+        moe_use_legacy_grouped_gemm=config.moe_use_legacy_grouped_gemm,
+        use_kitchen=config.use_kitchen,
+    )
