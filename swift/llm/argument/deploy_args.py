6069888484be49f938ea699c102e6891bb4b7564
hjh0119 <hujinghan.hjh@alibaba-inc.com>
2025-09-12 16:57:48 +0800

    add args for lora script

--- swift/llm/argument/deploy_args.py
+++ swift/llm/argument/deploy_args.py
@@ -86,7 +86,6 @@ class RolloutArguments(DeployArguments):
     # only for GRPO rollout with AsyncEngine, see details in swift/plugin/multi_turn
     multi_turn_scheduler: Optional[str] = None
     max_turns: Optional[int] = None
-    # lora, TODO: modify example script for lora
     vllm_enable_lora: bool = False
     vllm_max_lora_rank: int = 16
     # GYM env
