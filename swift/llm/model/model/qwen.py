00f41e89a9e5e8ae607306259a5695973d3ba9c7
Jintao <huangjintao.hjt@alibaba-inc.com>
2025-09-17 23:35:27 +0800

    [model] support tongyi-deep-research (#5854)

--- swift/llm/model/model/qwen.py
+++ swift/llm/model/model/qwen.py
@@ -552,6 +552,9 @@ register_model(
                 Model('swift/Qwen3-30B-A3B-AWQ', 'cognitivecomputations/Qwen3-30B-A3B-AWQ'),
                 Model('swift/Qwen3-235B-A22B-AWQ', 'cognitivecomputations/Qwen3-235B-A22B-AWQ'),
             ]),
+            ModelGroup([
+                Model('iic/Tongyi-DeepResearch-30B-A3B', 'Alibaba-NLP/Tongyi-DeepResearch-30B-A3B'),
+            ])
         ],
         TemplateType.qwen3,
         get_model_tokenizer_with_flash_attn,
