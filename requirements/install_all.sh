d7389b2b1c383dd3fdec9f25210d0b79284381f4
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2025-09-18 14:54:49 +0800

    update

--- requirements/install_all.sh
+++ requirements/install_all.sh
@@ -1,7 +1,7 @@
 # please use python=3.10/3.11, cuda12.*
 # sh requirements/install_all.sh
-pip install "sglang[all]" -U
-pip install "vllm>=0.5.1" -U
+pip install "sglang[all]<0.5" -U
+pip install "vllm>=0.5.1,<0.10.2" -U
 pip install "lmdeploy>=0.5" -U
 pip install "transformers<4.57" "trl<0.21" peft -U
 pip install autoawq -U --no-deps
