c97db6044eea0ef64139fe78f881b1b376f06a72
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2025-09-21 19:52:25 +0800

    fix

--- swift/megatron/utils/convert.py
+++ swift/megatron/utils/convert.py
@@ -268,7 +268,7 @@ def convert_mcore2hf(args: ExportArguments) -> None:
     extra_config = MegatronArguments.load_args_config(adapter_load or args.mcore_model)
     extra_config['adapter_load'] = adapter_load
     if args.mcore_model is not None:
-        extra_config['mcore_model'] = args.mcore_model
+        extra_config['load'] = args.mcore_model
     megatron_args = MegatronArguments(
         **kwargs,
         **current_convert_kwargs,
