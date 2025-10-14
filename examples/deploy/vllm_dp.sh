c4c8d37dc8a9a97fd04fba3fbdd5887917970411
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2025-10-14 21:18:21 +0800

    lint pass

--- examples/deploy/vllm_dp.sh
+++ examples/deploy/vllm_dp.sh
@@ -8,15 +8,15 @@ CUDA_VISIBLE_DEVICES=0,1 swift deploy \
 
 # After the server-side deployment above is successful, use the command below to perform a client call test.
 
-curl http://localhost:8000/v1/chat/completions \
--H "Content-Type: application/json" \
--d '{
-"model": "Qwen2.5-VL-7B-Instruct",
-"messages": [{"role": "user", "content": [
-    {"type": "image", "image": "http://modelscope-open.oss-cn-hangzhou.aliyuncs.com/images/cat.png"},
-    {"type": "image", "image": "http://modelscope-open.oss-cn-hangzhou.aliyuncs.com/images/animal.png"},
-    {"type": "text", "text": "What is the difference between the two images?"}
-]}],
-"max_tokens": 256,
-"temperature": 0
-}'
+# curl http://localhost:8000/v1/chat/completions \
+# -H "Content-Type: application/json" \
+# -d '{
+# "model": "Qwen2.5-VL-7B-Instruct",
+# "messages": [{"role": "user", "content": [
+#     {"type": "image", "image": "http://modelscope-open.oss-cn-hangzhou.aliyuncs.com/images/cat.png"},
+#     {"type": "image", "image": "http://modelscope-open.oss-cn-hangzhou.aliyuncs.com/images/animal.png"},
+#     {"type": "text", "text": "What is the difference between the two images?"}
+# ]}],
+# "max_tokens": 256,
+# "temperature": 0
+# }'
