70e0a180bf0a12fa2e6da71bb9af3fe7dfafe310
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2025-10-14 20:13:25 +0800

    add_retry

--- swift/llm/template/vision_utils.py
+++ swift/llm/template/vision_utils.py
@@ -10,7 +10,8 @@ import numpy as np
 import requests
 import torch
 from PIL import Image
-
+from requests.adapters import HTTPAdapter
+from urllib3.util.retry import Retry
 from swift.utils import get_env_args
 
 # >>> internvl
@@ -105,12 +106,26 @@ def load_file(path: Union[str, bytes, _T]) -> Union[BytesIO, _T]:
     if isinstance(path, str):
         path = path.strip()
         if path.startswith('http'):
-            request_kwargs = {}
-            timeout = float(os.getenv('TIMEOUT', '300'))
-            if timeout > 0:
-                request_kwargs['timeout'] = timeout
-            content = requests.get(path, **request_kwargs).content
-            res = BytesIO(content)
+            retries = Retry(
+                total=3,
+                backoff_factor=1,
+                allowed_methods=['GET']
+            )
+            session = requests.Session()
+            session.mount("http://", HTTPAdapter(max_retries=retries))
+            session.mount("https://", HTTPAdapter(max_retries=retries))
+
+            timeout = float(os.getenv('SWIFT_TIMEOUT', '20'))
+            request_kwargs = {'timeout': timeout} if timeout > 0 else {}
+
+            try:
+                response = session.get(path, **request_kwargs)
+                response.raise_for_status()
+                content = response.content
+                res = BytesIO(content)
+            finally:
+                session.close()
+
         elif os.path.exists(path) or (not path.startswith('data:') and len(path) <= 200):
             ROOT_IMAGE_DIR = get_env_args('ROOT_IMAGE_DIR', str, None)
             if ROOT_IMAGE_DIR is not None and not os.path.exists(path):
