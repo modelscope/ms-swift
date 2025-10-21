e5ec9caec8e7c6c044bacd1374955a464b1efe06
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2025-10-21 21:27:50 +0800

    fix

--- requirements/install_all.sh
+++ requirements/install_all.sh
@@ -4,10 +4,9 @@ pip install "sglang[all]" -U
 pip install "vllm>=0.5.1" -U
 pip install "lmdeploy>=0.5" -U
 pip install "transformers<4.58" "trl<0.24" peft -U
-pip install autoawq -U --no-deps
 pip install auto_gptq optimum bitsandbytes "gradio<5.33" -U
 pip install git+https://github.com/modelscope/ms-swift.git#egg=ms-swift[all]
-pip install timm deepspeed -U
+pip install timm "deepspeed<0.18s" -U
 pip install qwen_vl_utils qwen_omni_utils keye_vl_utils -U
 pip install decord librosa icecream soundfile -U
 pip install liger_kernel nvitop pre-commit math_verify==0.5.2 py-spy wandb swanlab -U
