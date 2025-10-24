2f7284706f2f3d194be56883047ed78d5c2a97e8
slin000111 <zhaoshilin2015@sina.com>
2025-10-24 11:07:08 +0800

    support llava-onevision-1.5

--- swift/llm/model/constant.py
+++ swift/llm/model/constant.py
@@ -222,6 +222,7 @@ class MLLMModelType:
     llava1_6_yi = 'llava1_6_yi'
     llava_next_qwen = 'llava_next_qwen'
     llama3_llava_next = 'llama3_llava_next'
+    llava_onevision1_5 = 'llava_onevision1_5'
 
     deepseek_vl = 'deepseek_vl'
     deepseek_vl2 = 'deepseek_vl2'
