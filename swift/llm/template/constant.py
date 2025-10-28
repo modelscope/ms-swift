23619093ed5699379e0403201ddb3568d5c5ed39
slin000111 <127832064+slin000111@users.noreply.github.com>
2025-10-28 14:59:09 +0800

    [model] support LLaVA-OneVision-1.5 (#6284)
    * update faq
    
    * Fixed the inconsistencies between the Chinese and English FAQ documentation.
    
    * Update link to sequence parallel example
    
    * support llava-onevision-1.5
    
    * update model list
    
    * update model list
    
    * add test
    
    * Update test_vision.py

--- swift/llm/template/constant.py
+++ swift/llm/template/constant.py
@@ -153,6 +153,7 @@ class MLLMTemplateType:
     llava1_6_yi = 'llava1_6_yi'
     llava_next_qwen = 'llava_next_qwen'
     llama3_llava_next = 'llama3_llava_next'
+    llava_onevision1_5 = 'llava_onevision1_5'
 
     yi_vl = 'yi_vl'
 
