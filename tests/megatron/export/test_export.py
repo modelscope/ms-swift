ea276a1e0c53fde9496d6761ecf0afb16922cb61
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2025-11-02 21:08:36 +0800

    update

--- tests/megatron/export/test_export.py
+++ tests/megatron/export/test_export.py
@@ -15,13 +15,43 @@ def test_to_mcore():
 def test_to_hf():
     megatron_export_main(
         MegatronExportArguments(
-            load='Qwen2.5-7B-Instruct-mcore',
+            load='Qwen3-30B-A3B-mcore',
             to_hf=True,
             exist_ok=True,
             tensor_model_parallel_size=2,
+            pipeline_model_parallel_size=2,
+            expert_model_parallel_size=2,
+            test_convert_precision=True))
+
+
+def test_peft_to_mcore():
+    megatron_export_main(
+        MegatronExportArguments(
+            model='Qwen/Qwen3-30B-A3B',
+            adapters=['megatron_output/Qwen3-30B-A3B/vx-xxx-hf'],
+            merge_lora=False,
+            to_mcore=True,
+            exist_ok=True,
+            tensor_model_parallel_size=2,
+            expert_model_parallel_size=4,
+            test_convert_precision=True))
+
+
+def test_peft_to_hf():
+    megatron_export_main(
+        MegatronExportArguments(
+            load='Qwen3-30B-A3B-mcore',
+            adapter_load='megatron_output/Qwen3-30B-A3B/vx-xxx',
+            merge_lora=False,
+            to_hf=True,
+            exist_ok=True,
+            tensor_model_parallel_size=2,
+            expert_model_parallel_size=2,
             test_convert_precision=True))
 
 
 if __name__ == '__main__':
     # test_to_mcore()
     test_to_hf()
+    # test_peft_to_mcore()
+    # test_peft_to_hf()
