add7fc9e5f5bf892e8884ab8bbf1897717c6b5a8
tastelikefeet <yuze.zyz@alibaba-inc.com>
2025-11-07 18:26:04 +0800

    fix

--- swift/cli/pt.py
+++ swift/cli/pt.py
@@ -3,7 +3,5 @@
 if __name__ == '__main__':
     from swift.cli.utils import fix_ppu
     fix_ppu()
-    from swift.ray import try_init_ray
-    try_init_ray()
     from swift.llm import pt_main
     pt_main()
