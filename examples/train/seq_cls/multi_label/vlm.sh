bd10e5c0a2d3244dc2813ca1aab517a017b61e4f
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2025-11-09 00:02:03 +0800

    update

--- examples/train/seq_cls/multi_label/vlm.sh
+++ examples/train/seq_cls/multi_label/vlm.sh
@@ -1,4 +1,7 @@
-CUDA_VISIBLE_DEVICES=1 \
+CUDA_VISIBLE_DEVICES=0 \
+IMAGE_MAX_TOKEN_NUM=1024 \
+VIDEO_MAX_TOKEN_NUM=128 \
+FPS_MAX_FRAMES=16 \
 swift sft \
     --model Qwen/Qwen3-VL-4B-Instruct \
     --train_type lora \
@@ -26,6 +29,3 @@ swift sft \
     --num_labels 20 \
     --task_type seq_cls \
     --problem_type multi_label_classification
-
-
-
