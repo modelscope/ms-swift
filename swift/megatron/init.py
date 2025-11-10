ccb406a81a8555bb01d872ec15d3c1a5f87550dd
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2025-11-10 17:15:58 +0800

    update

--- swift/megatron/init.py
+++ swift/megatron/init.py
@@ -643,9 +643,17 @@ def _patch_mrope():
             cp_size = cp_group.size()
             cu_seqlens_for_batched = cu_seqlens // cp_size
             use_batched_rope = (freqs.dim() >= 1 and freqs.shape[0] == cu_seqlens_for_batched[-1]).item()
-
-        # use batched RoPE for acceleration
-        assert use_batched_rope, f'freqs.shape: {freqs.shape}, cu_seqlens_for_batched: {cu_seqlens_for_batched}'
+        if not use_batched_rope:
+            logger.warning_once('Using non-batched RoPE, which may affect performance.')
+            return _origin_apply_rotary_pos_emb_thd(
+                t,
+                cu_seqlens,
+                freqs,
+                rotary_interleaved=rotary_interleaved,
+                multi_latent_attention=multi_latent_attention,
+                mscale=mscale,
+                cp_group=cp_group,
+            )
         if cp_group is None:
             raise ValueError('cp_group must be provided for THD format RoPE')
 
