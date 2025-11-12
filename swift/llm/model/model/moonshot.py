26e5f9584e0c6e28a919d2a7b1f931e1e8d30831
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2025-11-12 15:10:39 +0800

    update

--- swift/llm/model/model/moonshot.py
+++ swift/llm/model/model/moonshot.py
@@ -4,7 +4,7 @@ from transformers.dynamic_module_utils import get_class_from_dynamic_module
 from swift.llm import TemplateType
 from ..constant import LLMModelType, MLLMModelType
 from ..model_arch import ModelArch
-from ..patcher import patch_get_input_embeddings, patch_output_clone
+from ..patcher import patch_get_input_embeddings
 from ..register import (Model, ModelGroup, ModelMeta, get_model_tokenizer_multimodal,
                         get_model_tokenizer_with_flash_attn, register_model)
 
