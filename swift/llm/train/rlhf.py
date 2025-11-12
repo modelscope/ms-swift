e27554797e55cd7891d6f6ace5ac1134f88b59df
hjh0119 <hujinghan.hjh@alibaba-inc.com>
2025-11-12 17:46:01 +0800

    rm try catch

--- swift/llm/train/rlhf.py
+++ swift/llm/train/rlhf.py
@@ -55,13 +55,12 @@ class SwiftRLHF(SwiftSft):
         model_id_or_path = getattr(args, f'{key}_model')
         if model_id_or_path is None:
             return
+
         if model_type is None:
-            try:
-                from swift.llm.model.register import get_model_info_meta
-                model_info, _ = get_model_info_meta(model_id_or_path)
-                model_type = model_info.model_type
-            except Exception:
-                pass
+            from swift.llm.model.register import get_model_info_meta
+            model_info, _ = get_model_info_meta(model_id_or_path)
+            model_type = model_info.model_type
+
         if isinstance(model_id_or_path, list):
             # value model in PPO
             model_id_or_path = model_id_or_path[0]
