0b6aee8639a7438e763912684ec702d41f2bba3f
Jintao <huangjintao.hjt@alibaba-inc.com>
2025-11-18 22:00:44 +0800

    [bugfix] fix chatml chat template (#6656)

--- swift/llm/template/template/utils.py
+++ swift/llm/template/template/utils.py
@@ -15,7 +15,7 @@ class ChatmlTemplateMeta(TemplateMeta):
     prefix: Prompt = field(default_factory=list)
     prompt: Prompt = field(default_factory=lambda: ['<|im_start|>user\n{{QUERY}}<|im_end|>\n<|im_start|>assistant\n'])
     chat_sep: Optional[Prompt] = field(default_factory=lambda: ['<|im_end|>\n'])
-    suffix: Prompt = field(default_factory=lambda: ['<|im_end|>'])
+    suffix: Prompt = field(default_factory=lambda: ['<|im_end|>\n'])
     system_prefix: Optional[Prompt] = field(default_factory=lambda: ['<|im_start|>system\n{{SYSTEM}}<|im_end|>\n'])
     auto_add_bos: bool = True
 
