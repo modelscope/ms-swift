d4de6199c7867ed8513cd6c9ca717b999ba23b55
hzji210@gmail.com <hzji210@gmail.com>
2025-11-20 10:32:42 +0800

    upate vl-moe.sh

--- vl-moe.sh
+++ vl-moe.sh
@@ -1,6 +1,6 @@
 PYTORCH_NPU_ALLOC_CONF='expandable_segments:True' \
-NPROC_PER_NODE=2 \
-ASCEND_RT_VISIBLE_DEVICES=0,1 \
+NPROC_PER_NODE=4 \
+ASCEND_RT_VISIBLE_DEVICES=0,1,2,3 \
 megatron sft \
     --model Qwen/Qwen3-VL-30B-A3B-Instruct \
     --load_safetensors true \
@@ -15,6 +15,8 @@ megatron sft \
     --tensor_model_parallel_size 2 \
     --sequence_parallel true \
     --expert_model_parallel_size 2 \
+    --pipeline_model_parallel_size 2 \
+    --decoder_first_pipeline_num_layers 5 \
     --moe_aux_loss_coeff 1e-3 \
     --micro_batch_size 1 \
     --global_batch_size 4 \
@@ -32,10 +34,12 @@ megatron sft \
     --dataset_num_proc 8 \
     --no_save_optim true \
     --no_save_rng true \
-    --padding_free false \
     --moe_grouped_gemm true \
     --moe_shared_expert_overlap true \
+    --packing true \
+    --cross_entropy_loss_fusion true \
+    --recompute_granularity full \
+    --recompute_method uniform \
+    --recompute_num_layers 1 \
     --attention_backend flash
-    # --packing true \
     # --moe_permute_fusion true \
-    # --cross_entropy_loss_fusion true \
