0331aabec75b636403099cbb0549f9ba4cfc8365
slin000111 <127832064+slin000111@users.noreply.github.com>
2025-12-04 15:29:17 +0800

    [model] support Ministral3 2512 (#6891)
    * support mistral_2512
    
    * fix test mistral2512
    
    * delete unnecessary printouts

--- swift/llm/template/constant.py
+++ swift/llm/template/constant.py
@@ -230,6 +230,8 @@ class MLLMTemplateType:
     gemma3n = 'gemma3n'
     mistral_2503 = 'mistral_2503'
     mistral_2506 = 'mistral_2506'
+    mistral_2512 = 'mistral_2512'
+    mistral_2512_thinking = 'mistral_2512_thinking'
     paddle_ocr = 'paddle_ocr'
 
 
