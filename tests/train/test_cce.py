73773fcf1aefcd0026b0cf3ac4c9e2b6a649022d
w1ida <gxunda@gmail.com>
2025-12-09 09:27:13 +0000

    [misc] add use_cce test

--- tests/train/test_cce.py
+++ tests/train/test_cce.py
@@ -1,8 +1,10 @@
 import os
 
 os.environ['CUDA_VISIBLE_DEVICES'] = '0,1'
+os.environ['HF_ENDPOINT'] = 'https://hf-mirror.com'
+
 kwargs = {
-    'per_device_train_batch_size': 2,
+    'per_device_train_batch_size': 64,
     'save_steps': 30,
     'gradient_accumulation_steps': 2,
     'num_train_epochs': 1,
@@ -14,9 +16,10 @@ def test_sft():
     result = sft_main(
         TrainArguments(
             model='Qwen/Qwen2.5-0.5B-Instruct',
-            dataset=['swift/self-cognition#200'],
+            dataset=['gsm8k#1024'],
             split_dataset_ratio=0.01,
             use_cce=True,
+            # use_liger_kernel=True,
             **kwargs))
     last_model_checkpoint = result['last_model_checkpoint']
     infer_main(InferArguments(adapters=last_model_checkpoint, load_data_args=True))
