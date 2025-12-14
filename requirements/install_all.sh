21193c6e6c0ee2611b3d912744e7258a85c76ece
Jintao <huangjintao.hjt@alibaba-inc.com>
2025-12-14 16:04:49 +0800

    [docs] update readme (#7033)

--- requirements/install_all.sh
+++ requirements/install_all.sh
@@ -2,8 +2,8 @@
 # sh requirements/install_all.sh
 pip install "sglang<0.5.6" -U
 pip install "vllm>=0.5.1,<0.11.1" -U
-pip install "lmdeploy>=0.5" -U
-pip install "transformers<4.58" "trl<0.24" peft -U
+pip install "lmdeploy>=0.5,<0.10.2" -U
+pip install "transformers<4.58" "trl<0.25" peft -U
 pip install auto_gptq optimum bitsandbytes "gradio<5.33" -U
 pip install git+https://github.com/modelscope/ms-swift.git#egg=ms-swift[all]
 pip install timm "deepspeed<0.18" -U
