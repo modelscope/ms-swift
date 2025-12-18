3beb3bbf02a82d73683673b2a88745c4200cfac1
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2025-12-18 17:29:52 +0800

    fix

--- examples/models/qwen3_next/mtp.sh
+++ examples/models/qwen3_next/mtp.sh
@@ -51,7 +51,6 @@ megatron sft \
 #     --vllm_tensor_parallel_size 4 \
 #     --infer_backend vllm \
 #     --vllm_max_model_len 8192 \
-#     --val_dataset AI-ModelScope/alpaca-gpt4-data-zh#100 \
 #     --vllm_gpu_memory_utilization 0.9 \
 #     --vllm_speculative_config '{"method":"qwen3_next_mtp","num_speculative_tokens":2}' \
 #     --max_new_tokens 2048
