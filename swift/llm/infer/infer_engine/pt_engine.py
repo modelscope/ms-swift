360161f0b422d54736aa0cd4edaed8e2f8c841dd
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2026-01-09 11:44:37 +0800

    fix

--- swift/llm/infer/infer_engine/pt_engine.py
+++ swift/llm/infer/infer_engine/pt_engine.py
@@ -346,7 +346,7 @@ class PtEngine(InferEngine):
         elif template.task_type in ('reranker', 'generative_reranker'):
             if template.task_type == 'generative_reranker':
                 # Qwen3-reranker like
-                logits = get_generative_reranker_logits(logits)
+                logits = get_generative_reranker_logits(template.tokenizer, logits, attention_mask=inputs.get('attention_mask'))
             preds = logits.float()
             if self.reranker_use_activation:
                 preds = F.sigmoid(preds)
