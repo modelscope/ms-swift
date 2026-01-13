1f65b1ee260e41f98df00ba36f2b978b171ab836
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2026-01-13 11:39:52 +0800

    update pretrain

--- swift/megatron/pipelines/train/pretrain.py
+++ swift/megatron/pipelines/train/pretrain.py
@@ -12,13 +12,6 @@ class MegatronPretrain(MegatronSft):
     args_class = MegatronPretrainArguments
     args: args_class
 
-    def _prepare_template(self) -> None:
-        self.args.use_chat_template = False
-        self.args.loss_scale = 'all'
-        logger.info('Setting args.use_chat_template: False')
-        logger.info("Setting args.loss_scale: 'all'")
-        super()._prepare_template()
-
 
 def megatron_pretrain_main(args: Optional[Union[List[str], MegatronPretrainArguments]] = None):
     return MegatronPretrain(args).main()
