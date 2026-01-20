205125b3a75521f108036ed8b3cc0751e05b2f5e
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2026-01-20 11:40:33 +0800

    update

--- swift/megatron/trainers/reranker_trainer.py
+++ swift/megatron/trainers/reranker_trainer.py
@@ -1,7 +1,10 @@
 # Copyright (c) ModelScope Contributors. All rights reserved.
+from functools import partial
+
 import torch.nn
 from megatron.training import get_args, get_timers
 
+from swift.loss import loss_map
 from swift.utils import get_logger
 from .base import BaseMegatronTrainer
 
@@ -10,8 +13,19 @@ logger = get_logger()
 
 class MegatronRerankerTrainer(BaseMegatronTrainer):
 
+    def __init__(self, args, template):
+        super().__init__(args, template)
+        if not args.padding_free:
+            raise ValueError('Currently, task_type reranker/generative_reranker only supports padding_free.')
+        self._loss_func = loss_map[self.args.loss_type](args, self)
+
     def loss_func(self, output_tensor: torch.Tensor, *, labels: torch.Tensor, packed_seq_params=None):
-        pass
+        args = self.args
+        last_hidden_state = self.get_last_tokens(output_tensor, packed_seq_params)
+        loss = self._loss_func({'last_hidden_state': last_hidden_state}, labels)
+        metric = {'loss': loss.detach().clone()}
+        metric = self._all_reduce_metric(metric)
+        return loss, metric
 
     def forward_step(self, data_iterator, model):
         timers = get_timers()
@@ -28,3 +42,5 @@ class MegatronRerankerTrainer(BaseMegatronTrainer):
         with self.stimer:
             output_tensor = model(**data)
         packed_seq_params = data.get('packed_seq_params')
+        loss_func = partial(self.loss_func, labels=labels, packed_seq_params=packed_seq_params)
+        return output_tensor, loss_func
