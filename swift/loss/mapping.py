32aecd8b174c1bdb90a1f77b7d83c27927d6878a
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2026-01-21 18:05:11 +0800

    update

--- swift/loss/mapping.py
+++ swift/loss/mapping.py
@@ -1,7 +1,7 @@
 # Copyright (c) ModelScope Contributors. All rights reserved.
 from .causal_lm import CustomCrossEntropyLoss
 from .embedding import ContrastiveLoss, CosineSimilarityLoss, InfonceLoss, OnlineContrastiveLoss
-from .reranker import ListwiseRerankerLoss, RerankerLoss
+from .reranker import ListwiseRerankerLoss, PointwiseRerankerLoss
 
 loss_map = {
     'cross_entropy': CustomCrossEntropyLoss,  # examples
@@ -11,6 +11,6 @@ loss_map = {
     'online_contrastive': OnlineContrastiveLoss,
     'infonce': InfonceLoss,
     # # reranker
-    'pointwise_reranker': RerankerLoss,
+    'pointwise_reranker': PointwiseRerankerLoss,
     'listwise_reranker': ListwiseRerankerLoss,
 }
