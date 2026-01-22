17395eca58cde9841fe61bb32d1933deb58d11bf
meichangsu1 <1484603386@qq.com>
2026-01-22 10:17:20 +0800

    feat: add activation CPU offload callback support for FSDP training
    - Add ActivationCpuOffloadCallBack import and registration in callbacks mapping
    - Automatically append activation_cpu_offload callback when FSDP config has activation_cpu_offload enabled
    - Enables memory-efficient training by offloading activations to CPU during FSDP forward pass

--- swift/callbacks/mapping.py
+++ swift/callbacks/mapping.py
@@ -1,10 +1,12 @@
 # Copyright (c) ModelScope Contributors. All rights reserved.
+from swift.activation_cpu_offload import ActivationCpuOffloadCallBack
 from .adalora import AdaloraCallback
 from .early_stop import EarlyStopCallback
 from .lisa import LISACallback
 from .perf_log import PerfMetricsLogCallback
 
 callbacks_map = {
+    'activation_cpu_offload': ActivationCpuOffloadCallBack,
     'adalora': AdaloraCallback,
     'early_stop': EarlyStopCallback,
     'lisa': LISACallback,
