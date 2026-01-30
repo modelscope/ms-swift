15a8a0a37a27aaad7679832ab04180fe65c0af5b
hjh0119 <hujinghan.hjh@alibaba-inc.com>
2026-01-30 10:56:54 +0800

    revert

--- swift/megatron/arguments/megatron_args.py
+++ swift/megatron/arguments/megatron_args.py
@@ -700,7 +700,6 @@ class MegatronArguments(ExtraMegatronArguments):
         if self.apply_query_key_layer_scaling:
             os.environ['NVTE_APPLY_QK_LAYER_SCALING'] = '1'
 
-    @staticmethod
     def _init_moe(self):
         if self.moe_router_dtype.lower() == 'none':
             self.moe_router_dtype = None
@@ -775,7 +774,7 @@ class MegatronArguments(ExtraMegatronArguments):
                 logger.info('Setting args.tuner_type: lora')
         if self.adapters:
             self._load_adapter_config()
-        MegatronArguments._init_moe(self)
+        self._init_moe()
         self._init_mixed_precision()
 
         self.megatron_extra_kwargs = json_parse_to_dict(self.megatron_extra_kwargs)
