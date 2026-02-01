c40d0fce33ac3f5e5130962b5231612d6151d3ef
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2026-02-01 16:25:46 +0800

    update

--- swift/megatron/tuners/lora.py
+++ swift/megatron/tuners/lora.py
@@ -200,14 +200,10 @@ class LoraParallelLinear(MegatronModule, LoraLayer):
                     **kwargs,
                 )
                 lora_b.parallel_mode = self.base_layer.parallel_mode  # fix moe_shared_expert_overlap
-        for lora in [lora_a, lora_b]:
+        for lora in [lora_a, lora_b] and hasattr(lora, 'weight'):  # TODO: experts
             if getattr(lora, 'parallel_mode', None) is None:
                 sequence_parallel = True if isinstance(self.base_layer, TopKRouter) else self.sequence_parallel
-                if hasattr(lora, 'weight'):
-                    lora.weight.sequence_parallel = sequence_parallel
-                else:
-                    for i in range(self.base_layer.num_gemms):
-                        getattr(lora, f'weight{i}').sequence_parallel = sequence_parallel
+                lora.weight.sequence_parallel = sequence_parallel
         self.lora_A[adapter_name] = lora_a
         self.lora_B[adapter_name] = lora_b
         if hasattr(self, 'lora_bias'):
