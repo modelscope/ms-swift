b37a63e6b21aa22879d46526c181bee324c0901d
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2026-02-09 19:46:36 +0800

    bump version

--- swift/version.py
+++ swift/version.py
@@ -1,5 +1,5 @@
 # Make sure to modify __release_datetime__ to release time when making official release.
-__version__ = '3.12.4'
+__version__ = '3.12.5'
 # default release datetime for branches under active development is set
 # to be a time far-far-away-into-the-future
-__release_datetime__ = '2026-02-03 23:59:59'
+__release_datetime__ = '2026-02-14 23:59:59'
