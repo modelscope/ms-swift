c475772bdff2dc803856c8d96b8074a1d73bb13a
鸡腿 <16110130+gitoor@user.noreply.gitee.com>
2026-02-13 16:14:28 +0800

    improve code format

--- swift/callbacks/qat.py
+++ swift/callbacks/qat.py
@@ -1,9 +1,10 @@
 # Copyright (c) ModelScope Contributors. All rights reserved.
 import os
 
+from transformers.utils import is_torchao_available
+
 from swift.utils import get_logger
 from .base import TrainerCallback
-from transformers.utils import is_torchao_available
 
 logger = get_logger()
 
@@ -14,7 +15,7 @@ class QatCallback(TrainerCallback):
     def __init__(self, args, trainer):
         super().__init__(args, trainer)
         # check dependency
-        assert is_torchao_available("0.15.0"), 'Version of torchao should be 0.15.0 or higher'
+        assert is_torchao_available('0.15.0'), 'Version of torchao should be 0.15.0 or higher'
 
         # defined torchao quantization config e.g. Int4WeightOnlyConfig, Int8DynamicActivationInt8WeightConfig
         from torchao.quantization import Int4WeightOnlyConfig
@@ -26,17 +27,16 @@ class QatCallback(TrainerCallback):
         # insert fake quantizer
         logger.info(f'Initializing QAT with config: [{self.quant_config}]')
         train_model = self.trainer.model
-        quantize_(train_model.model, QATConfig(self.quant_config, step="prepare"))
-        logger.info(f"QAT Model: {train_model}")
+        quantize_(train_model.model, QATConfig(self.quant_config, step='prepare'))
+        logger.info(f'QAT Model: {train_model}')
 
     def on_train_end(self, args, state, control, **kwargs):
         from transformers.trainer_utils import PREFIX_CHECKPOINT_DIR
         from transformers import AutoModel, TorchAoConfig
-        checkpoint_dir = os.path.join(args.output_dir, f"{PREFIX_CHECKPOINT_DIR}-{state.max_steps}")
+        checkpoint_dir = os.path.join(args.output_dir, f'{PREFIX_CHECKPOINT_DIR}-{state.max_steps}')
         quantization_config = TorchAoConfig(quant_type=self.quant_config)
-        pqt_model = AutoModel.from_pretrained(checkpoint_dir, dtype="auto",
-                                              quantization_config=quantization_config)
-        quantized_output_dir = os.path.join(args.output_dir, "quantized_model")
+        pqt_model = AutoModel.from_pretrained(checkpoint_dir, dtype='auto', quantization_config=quantization_config)
+        quantized_output_dir = os.path.join(args.output_dir, 'quantized_model')
         pqt_model.save_pretrained(quantized_output_dir)
         self.trainer.tokenizer.save_pretrained(quantized_output_dir)
-        logger.info(f"Quantized model saved to: {quantized_output_dir}")
+        logger.info(f'Quantized model saved to: {quantized_output_dir}')
