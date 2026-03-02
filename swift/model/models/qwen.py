7545ab2abb19dc7642357167e6e929ae02f4c09f
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2026-03-02 22:21:45 +0800

    update models

--- swift/model/models/qwen.py
+++ swift/model/models/qwen.py
@@ -1152,9 +1152,18 @@ register_model(
         [
             ModelGroup(
                 [
+                    Model('Qwen/Qwen3.5-0.8B', 'Qwen/Qwen3.5-0.8B'),
+                    Model('Qwen/Qwen3.5-2B', 'Qwen/Qwen3.5-2B'),
+                    Model('Qwen/Qwen3.5-4B', 'Qwen/Qwen3.5-4B'),
+                    Model('Qwen/Qwen3.5-9B', 'Qwen/Qwen3.5-9B'),
                     Model('Qwen/Qwen3.5-27B', 'Qwen/Qwen3.5-27B'),
                     # FP8
                     Model('Qwen/Qwen3.5-27B-FP8', 'Qwen/Qwen3.5-27B-FP8'),
+                    # base
+                    Model('Qwen/Qwen3.5-0.8B-Base', 'Qwen/Qwen3.5-0.8B-Base'),
+                    Model('Qwen/Qwen3.5-2B-Base', 'Qwen/Qwen3.5-2B-Base'),
+                    Model('Qwen/Qwen3.5-4B-Base', 'Qwen/Qwen3.5-4B-Base'),
+                    Model('Qwen/Qwen3.5-9B-Base', 'Qwen/Qwen3.5-9B-Base'),
                 ],
                 TemplateType.qwen3_5),
         ],
