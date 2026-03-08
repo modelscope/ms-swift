f596ab0450a6d5d6e76953c5e3811f53533f5701
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2026-03-08 15:45:35 +0800

    fix kimi_k2

--- swift/megatron/model/model_config.py
+++ swift/megatron/model/model_config.py
@@ -417,6 +417,7 @@ def convert_hf_config(config) -> Dict[str, Any]:
             'deepseek',
             'deepseek_v2',
             'deepseek_v3',
+            'kimi_k2',
             'deepseek_v32',
             'dots1',
     } or hf_model_type == 'kimi_vl':
