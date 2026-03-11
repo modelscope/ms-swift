ec80b87fce0c91d2cdddddd087e5cbea527949c6
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2026-03-11 18:23:22 +0800

    fix

--- swift/megatron/model/modules/gated_self_attention.py
+++ swift/megatron/model/modules/gated_self_attention.py
@@ -1,5 +1,7 @@
 # Copyright (c) ModelScope Contributors. All rights reserved.
 import torch
+from megatron.core.parallel_state import get_tensor_model_parallel_rank
+from megatron.core.tensor_parallel import all_gather_last_dim_from_tensor_parallel_region
 from megatron.core.transformer.attention import SelfAttention
 
 try:
@@ -16,6 +18,24 @@ class GatedSelfAttention(SelfAttention):
         """
         # Attention heads [sq, b, h] --> [sq, b, ng * (np/ng + 2) * hn)]
         mixed_qkv, _ = self.linear_qkv(hidden_states)
+
+        assert self.config.num_query_groups is not None
+        if getattr(self, 'world_size', None) is not None and self.config.num_query_groups < self.world_size:
+            # Note that weights are interleaved in the following manner:
+            # q1 q2 k1 v1 | q3 q4 k2 v2 | q5 q6 k3 v3 | ...
+            # When tp_size > num_kv_heads, we split "q1 q2 k1 v1" over multiple
+            # ranks, so a rank does not have a clean partitioning of just the q_heads
+            # it needs. Instead, we perform the following steps:
+            # 1. Assemble the full "q1 q2 k1 v1 | q3 q4 k2 v2 | q5 q6 k3 v3 | ..."
+            #    through an AG.
+            # 2. Pull out the right slice (e.g., "q1 q2 k1 v1" or "q3 q4 k2 v2").
+            # 3. Split q_heads (e.g., q1, q2), k_heads (e.g., k1), v_heads (e.g., v1).
+            # 4. Further index into query to get only the q_heads that this rank is
+            #    responsible for (e.g., q1).
+            mixed_qkv = all_gather_last_dim_from_tensor_parallel_region(mixed_qkv)
+            idx = get_tensor_model_parallel_rank() // (self.world_size // self.config.num_query_groups)
+            size = mixed_qkv.size()[-1] // self.config.num_query_groups
+            mixed_qkv = mixed_qkv[:, :, idx * size:(idx + 1) * size]
 
         new_tensor_shape = mixed_qkv.size()[:-1] + (
             self.num_query_groups_per_partition,
@@ -43,6 +63,13 @@ class GatedSelfAttention(SelfAttention):
 
         # [sq, b, ng, np/ng * hn] -> [sq, b, np, hn]
         query = query.reshape(query.size(0), query.size(1), -1, self.hidden_size_per_attention_head)
+        if getattr(self, 'world_size', None) is not None and self.config.num_query_groups < self.world_size:
+            # query above corresponds to (num_q_heads / num_kv_heads) q_heads.
+            # Index appropriately into query to get (num_q_heads / tp_size) q_heads.
+            # This is step 4 in the list of steps above.
+            idx = get_tensor_model_parallel_rank() % (self.world_size // self.config.num_query_groups)
+            size = query.shape[2] // (self.world_size // self.config.num_query_groups)
+            query = query[:, :, idx * size:(idx + 1) * size, :]
         query, gate = query[:, :, ::2], query[:, :, 1::2]
         if self.q_layernorm is not None:
             query = self.q_layernorm(query)
