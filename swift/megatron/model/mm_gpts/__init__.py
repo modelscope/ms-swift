b4415b5f33c146d35f513e88ab31059a8be89bf7
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2026-03-11 17:00:07 +0800

    update

--- swift/megatron/model/mm_gpts/__init__.py
+++ swift/megatron/model/mm_gpts/__init__.py
@@ -1,2 +1,9 @@
 # Copyright (c) ModelScope Contributors. All rights reserved.
-from . import glm, internvl, kimi_vl, llama4, qwen, qwen3_5, qwen3_vl
+from swift.utils import get_env_args
+from . import glm, internvl, kimi_vl, llama4, qwen, qwen3_vl
+
+use_mcore_gdn = get_env_args('SWIFT_USE_MCORE_GDN', bool, False)
+if use_mcore_gdn:
+    from . import qwen3_5_gdn
+else:
+    from . import qwen3_5
