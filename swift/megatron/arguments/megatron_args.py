729d6a6f57a90e304a73e1ee9e5e576683c76e65
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2026-03-20 16:15:25 +0800

    fix

--- swift/megatron/arguments/megatron_args.py
+++ swift/megatron/arguments/megatron_args.py
@@ -672,6 +672,8 @@ class MegatronArguments(RLHFMegatronArgumentsMixin, MegatronTunerMixin):
         if self.merge_lora is None:
             self.merge_lora = self.save_safetensors
         if self.tuner_type == 'lora_llm':
+            if not self.is_multimodal:
+                raise ValueError('`tuner_type="lora_llm"` is only supported for multimodal models.')
             if not self.merge_lora:
                 raise ValueError('`merge_lora` must be True when using `--tuner_type lora_llm`')
             if not self.no_save_optim:
