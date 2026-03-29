ef7314eda8b6f42b099990e97bf985b19e2b89d7
Jintao <huangjintao.hjt@alibaba-inc.com>
2026-03-29 13:29:17 +0800

    fix experts_impl (#8463)

--- swift/model/register.py
+++ swift/model/register.py
@@ -184,7 +184,10 @@ class ModelLoader(BaseModelLoader):
         self.attn_impl_keys = None
         experts_impl = experts_impl or kwargs.get('experts_implementation')
         if experts_impl is not None and not transformers_5:
-            raise ValueError('experts_impl is only supported in "transformers>=5.0".')
+            if experts_impl == 'eager':
+                experts_impl = None
+            else:
+                raise ValueError('experts_impl is only supported in "transformers>=5.0".')
         self.experts_impl = experts_impl
         self.rope_scaling = rope_scaling
         self.max_model_len = max_model_len
@@ -208,7 +211,6 @@ class ModelLoader(BaseModelLoader):
         else:
             model_kwargs['torch_dtype'] = self.torch_dtype
         _patch_awq_compat(model_info)
-        logger.info(f'model_kwargs: {model_kwargs}')
 
     def _postprocess_config(self, config):
         # fix prediction_step (internvl2, ovis, ...)
@@ -267,6 +269,7 @@ class ModelLoader(BaseModelLoader):
                   model_kwargs) -> PreTrainedModel:
         if self.experts_impl is not None:
             model_kwargs['experts_implementation'] = self.experts_impl
+        logger.info(f'model_kwargs: {model_kwargs}')
         model_info = self.model_info
         model_meta = self.model_meta
         auto_model_cls = self.auto_model_cls
