556c202ccd5f445d419f0fd47f919997ea6ffd0a
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2026-04-01 17:57:20 +0800

    update

--- swift/megatron/arguments/megatron_args.py
+++ swift/megatron/arguments/megatron_args.py
@@ -644,7 +644,7 @@ class MegatronArguments(RLHFMegatronArgumentsMixin, MegatronTunerMixin):
         self.model_type = self.model_info.model_type
         self.model_dir = self.model_info.model_dir
         self.is_multimodal = self.model_meta.is_multimodal
-        self.megatron_model_meta = get_model_meta(self._get_mcore_model_type(self.model_type))
+        self.megatron_model_meta = get_model_meta(self._get_mcore_model_type(self.model_meta))
         if self.megatron_model_meta is None:
             raise ValueError(f'Model: {self.model} is not supported.')
         self._init_teacher_model()
@@ -724,10 +724,10 @@ class MegatronArguments(RLHFMegatronArgumentsMixin, MegatronTunerMixin):
                              f'micro_batch_size: {self.micro_batch_size}.')
         self._check_muon()
 
-    def _get_mcore_model_type(self, model_type):
-        if self.model_type in {'qwen3_emb'}:
-            mcore_model_type = model_type
-        else:
+    def _get_mcore_model_type(self, model_meta):
+        model_type = model_meta.model_type
+        mcore_model_type = self.model_meta.mcore_model_type
+        if mcore_model_type is None:
             mcore_model_type = get_mcore_model_type(model_type)
         return mcore_model_type
 
@@ -752,7 +752,7 @@ class MegatronArguments(RLHFMegatronArgumentsMixin, MegatronTunerMixin):
             self.teacher_model, model_type=self.teacher_model_type, use_hf=self.use_hf, hub_token=self.hub_token)
         self.teacher_model_type = self.teacher_model_info.model_type
         self.teacher_model_dir = self.teacher_model_info.model_dir
-        self.teacher_megatron_model_meta = get_model_meta(self._get_mcore_model_type(self.teacher_model_type))
+        self.teacher_megatron_model_meta = get_model_meta(self._get_mcore_model_type(self.teacher_model_meta))
         if self.teacher_megatron_model_meta is None:
             raise ValueError(f'Model: {self.teacher_model} is not supported.')
 
