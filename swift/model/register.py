0bb1be0626d4b0d7d5bba1ba6d8a29acc5043a86
Jintao <huangjintao.hjt@alibaba-inc.com>
2026-04-13 19:33:03 +0800

    [bugfix] fix bge-m3 reranker (#9091)

--- swift/model/register.py
+++ swift/model/register.py
@@ -277,7 +277,9 @@ class ModelLoader(BaseModelLoader):
         model = None
         if model_info.task_type in {'seq_cls', 'reranker'}:
             HfConfigFactory.set_config_attr(config, 'tie_word_embeddings', False)
-        if model_info.task_type in {'seq_cls', 'reranker'} and auto_model_cls is None and not self.return_dummy_model:
+        if model_info.task_type in {'seq_cls', 'reranker'} and auto_model_cls in {
+                None, AutoModelForSequenceClassification
+        } and not self.return_dummy_model:
             with patch_automodel_for_sequence_classification(model_config=config, patch_from_pretrained=False):
                 try:
                     model = AutoModelForSequenceClassification.from_pretrained(
@@ -449,8 +451,9 @@ class ModelLoader(BaseModelLoader):
     def _init_generation_config(self, model, model_dir):
         # generation_config
         generation_config_path = os.path.join(model_dir, 'generation_config.json')
-        if not hasattr(model, 'generation_config') and os.path.isfile(generation_config_path):
-            model.generation_config = GenerationConfig.from_pretrained(model_dir)
+        if getattr(model, 'generation_config', None) is None:
+            model.generation_config = GenerationConfig.from_pretrained(model_dir) if os.path.isfile(
+                generation_config_path) else None
         # fix llama2 warning
         if getattr(model, 'generation_config', None):
             fix_do_sample_warning(model.generation_config)
