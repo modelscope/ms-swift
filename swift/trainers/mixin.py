b5b1b7e908527b4f3a5f3fcf38895bf28c84bfec
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2026-04-16 16:19:01 +0800

    fix

--- swift/trainers/mixin.py
+++ swift/trainers/mixin.py
@@ -889,7 +889,8 @@ class SwiftMixin:
 
         # gradient_checkpointing
         gradient_checkpointing = self.args.gradient_checkpointing
-        self._prepare_gradient_checkpointing(self.accelerator.unwrap_model(self.model))
+        base_model = self.template.get_base_model(self.accelerator.unwrap_model(self.model))  # fix peftmodel
+        self._prepare_gradient_checkpointing(base_model)
         with self.hub.patch_hub(), self._fix_grad_norm_nan(), self._patch_skip_first_batches(
         ), self._patch_deepspeed_load_checkpoint():
             res = super().train(*args, **kwargs)
