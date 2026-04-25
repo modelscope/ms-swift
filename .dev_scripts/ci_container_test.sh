482ee6473460ea3cac2351a1374937e4eed08a30
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2026-04-25 15:06:11 +0800

    fix ci

--- .dev_scripts/ci_container_test.sh
+++ .dev_scripts/ci_container_test.sh
@@ -22,10 +22,10 @@ if [ "$MODELSCOPE_SDK_DEBUG" == "True" ]; then
 
     pip install -r requirements/framework.txt -U -i https://mirrors.aliyun.com/pypi/simple/
     pip install decord einops -U -i https://mirrors.aliyun.com/pypi/simple/
-    pip uninstall autoawq auto-gptq -y
+    pip uninstall autoawq -y
     pip install optimum
     pip install diffusers
-    pip install "transformers<5.0"
+    pip install "transformers<5.0" "peft<0.19"
     # pip install autoawq -U --no-deps
 
     # test with install
