45f532e7e1163a9f88dd714a3480094f3217c992
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2026-04-26 23:41:29 +0800

    fix

--- swift/template/templates/llm.py
+++ swift/template/templates/llm.py
@@ -342,6 +342,7 @@ class Hy3Template(Template):
     def init_env_args(self):
         super().init_env_args()
         # reasoning_effort: "no_think", "low", "high" (deep chain-of-thought)
+        # TODO: sample level
         self.reasoning_effort = get_env_args('reasoning_effort', str, None)
         if self.reasoning_effort is None:
             self.reasoning_effort = 'high' if self.enable_thinking else 'no_think'
