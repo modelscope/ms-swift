0a334d1d19900014e2f43250f5c7e9e0874e515f
jinghanhu <hujinghan.hjh@alibaba-inc.com>
2026-05-11 19:43:50 +0800

    megatron grpo data_shuffle (#9310)

--- swift/megatron/arguments/megatron_args.py
+++ swift/megatron/arguments/megatron_args.py
@@ -303,8 +303,6 @@ class RLHFMegatronArgumentsMixin:
                 raise ValueError('async_generate is not supported for Megatron GRPO right now')
             if self.sync_ref_model:
                 raise ValueError('sync_ref_model is not supported for Megatron GRPO right now')
-            if not self.dataset_shuffle:
-                raise ValueError('dataset_shuffle false is not supported for Megatron GRPO')
             if self.multi_turn_scheduler:
                 raise ValueError('multi_turn_scheduler is not supported for Megatron GRPO right now')
             if self.num_iterations > 1:
@@ -316,6 +314,8 @@ class RLHFMegatronArgumentsMixin:
                      f'is a multiple of num_generations ({self.num_generations}). Please adjust your batch parameters.')
 
         _check_not_supported()
+        if self.dataset_shuffle is not None:
+            self.train_dataloader_shuffle = self.dataset_shuffle
         self._init_generation_batch_params()
         self.remove_unused_columns = False
         logger.info(f'Setting args.remove_unused_columns: {self.remove_unused_columns}')
