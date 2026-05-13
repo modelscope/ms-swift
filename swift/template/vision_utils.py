030e65cec913a87ae6e11b9c76e7f4f7649bf1e3
Tohrusky <65994850+Tohrusky@users.noreply.github.com>
2026-05-13 12:36:21 +0800

    [fix] support video as audio input

--- swift/template/vision_utils.py
+++ swift/template/vision_utils.py
@@ -298,14 +298,19 @@ def load_video_minicpmv_mplug_owl3(video: Union[str, bytes], max_num_frames):
 
 def load_audio(audio: Union[str, bytes], sampling_rate: int, return_sr: bool = False):
     import librosa
-    if isinstance(audio, bytes):
-        audio_io = BytesIO(audio)
-    elif isinstance(audio, str) and (audio.startswith('http://') or audio.startswith('https://')):
-        import audioread
-        audio_io = audioread.ffdec.FFmpegAudioFile(audio)
-    else:
-        audio_io = audio
-    res = librosa.load(audio_io, sr=sampling_rate)
+    try:
+        audio = load_file(audio)
+        res = librosa.load(audio, sr=sampling_rate)
+    except Exception:
+        if isinstance(audio, str):
+            if audio.startswith(('http://', 'https://')):
+                import audioread
+                audio = audioread.ffdec.FFmpegAudioFile(audio)
+            else:
+                audio = _check_path(audio) or audio
+            res = librosa.load(audio, sr=sampling_rate)
+        else:
+            raise ValueError(f'Unsupported audio type: {type(audio)} for {audio}')
     return res if return_sr else res[0]
 
 
