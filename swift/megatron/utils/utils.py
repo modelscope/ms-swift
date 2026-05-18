0de71a030b2cf6c2080bd0372a59bf7df94d88c1
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2026-05-19 01:58:22 +0800

    update

--- swift/megatron/utils/utils.py
+++ swift/megatron/utils/utils.py
@@ -220,10 +220,11 @@ def get_padding_to(args):
         padding_to = (padding_to or 1) * args.context_parallel_size
     origin_padding_to = padding_to
     fp8_format = getattr(args, 'fp8_format', None) or getattr(args, 'fp8', None)
+    fp4_format = getattr(args, 'fp4_format', None) or getattr(args, 'fp4', None)
     if args.fp8_recipe == 'blockwise':
         padding_to = (padding_to or 1) * 128
-    elif fp8_format is not None:
-        padding_to = max((padding_to or 1) * 8, 16)
+    elif fp8_format is not None or fp4_format is not None:
+        padding_to = (padding_to or 1) * 16
     if args.attention_backend == 'fused':
         padding_to = max(padding_to or 1, ((origin_padding_to) or 1) * 64)
     return padding_to
