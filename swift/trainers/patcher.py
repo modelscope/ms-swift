2de84852219d95bfa9395e8615e5222558d9bf62
qq1243196045 <1243196045@qq.com>
2026-05-27 18:27:41 +0800

    support logging MFU

--- swift/trainers/patcher.py
+++ swift/trainers/patcher.py
@@ -7,10 +7,8 @@ from transformers import trainer
 from transformers.trainer_callback import (DefaultFlowCallback, PrinterCallback, ProgressCallback, TrainerControl,
                                            TrainerState)
 from transformers.trainer_utils import IntervalStrategy, has_length
-
-from swift.utils import append_to_jsonl, format_time, get_logger, get_max_reserved_memory, is_pai_training_job
+from swift.utils import get_env_args, get_device_count, append_to_jsonl, format_time, get_logger, get_max_reserved_memory, is_pai_training_job
 from .arguments import TrainingArguments
-
 logger = get_logger()
 
 
@@ -29,6 +27,59 @@ def add_train_message(logs, state, start_time, start_step) -> None:
         logs['memory(GiB)'] = round(state.max_memory, 2)
     logs['train_speed(s/it)'] = round(train_speed, 6)
 
+
+class ProgressCallbackNewWithMFU(ProgressCallback):
+    def __init__(self):
+        super().__init__()
+        self.device_tflops = None
+        self.elapsed = 0.0
+        self.step_start_time = None
+        
+    def on_init_end(self, args: 'TrainingArguments', state: TrainerState, control: TrainerControl, **kwargs):
+
+        # Top priority. Specify by ENV
+        tflops = get_env_args('DEVICE_TFLOPS', float, None)
+        device_count = max(get_device_count(), 1)
+        self.device_tflops = tflops * device_count
+        super().on_init_end(args, state, control, **kwargs)
+    def on_step_begin(self, args: 'TrainingArguments', state: TrainerState, control: TrainerControl, **kwargs):
+        self.step_start_time = time.time()
+        super().on_step_begin(args, state, control, **kwargs)
+        
+    def on_step_end(self, args: 'TrainingArguments', state: TrainerState, control: TrainerControl, **kwargs):
+        self.elapsed += time.time() - self.step_start_time
+        super().on_step_end(args, state, control, **kwargs)
+
+    def on_train_begin(self, args, state, control, **kwargs):
+        if state.is_world_process_zero:
+            self.training_bar = tqdm(desc='Train', total=state.max_steps, dynamic_ncols=True)
+        self.start_step = state.global_step
+        self.current_step = 0
+        self.start_time = time.time()
+
+    def on_prediction_step(self, args, state: TrainerState, control, eval_dataloader=None, **kwargs):
+        if state.is_world_process_zero and has_length(eval_dataloader):
+            if self.prediction_bar is None:
+                if self.training_bar is not None:
+                    self.training_bar.fp.write('\n')
+                self.prediction_bar = tqdm(
+                    desc='Val', total=len(eval_dataloader), leave=True, dynamic_ncols=True, position=0)
+            self.prediction_bar.update()
+
+    def on_log(self, args: TrainingArguments, state: TrainerState, control, logs=None, **kwargs):
+        add_train_message(logs, state, self.start_time, self.start_step)
+        total_flos = getattr(state, 'total_flos', 0)
+        actual_flops = total_flos / self.elapsed
+        theoretical_max_flops = self.device_tflops * 1e12
+        mfu = actual_flops / theoretical_max_flops
+        logger.debug(f'Total_flos[{total_flos}] elapsed_time[{self.elapsed}]sec Average MFU[{mfu}]')
+        logs['MFU'] = round(mfu, 6)
+        if not is_pai_training_job() and state.is_world_process_zero:
+            jsonl_path = os.path.join(args.output_dir, 'logging.jsonl')
+            append_to_jsonl(jsonl_path, logs)
+        super().on_log(args, state, control, logs, **kwargs)
+        if state.is_world_process_zero and self.training_bar is not None:
+            self.training_bar.refresh()
 
 class ProgressCallbackNew(ProgressCallback):
 
@@ -100,9 +151,11 @@ class PrinterCallbackNew(PrinterCallback):
         _ = logs.pop('total_flos', None)
         if state.is_world_process_zero:
             print(logs, flush=True)
-
-
 # monkey patching
-trainer.DEFAULT_PROGRESS_CALLBACK = ProgressCallbackNew
+tflops = get_env_args('DEVICE_TFLOPS', float, None)
+if tflops is not None:
+    trainer.DEFAULT_PROGRESS_CALLBACK = ProgressCallbackNewWithMFU
+else:
+    trainer.DEFAULT_PROGRESS_CALLBACK = ProgressCallbackNew
 trainer.DEFAULT_CALLBACKS = [DefaultFlowCallbackNew]
 trainer.PrinterCallback = PrinterCallbackNew
