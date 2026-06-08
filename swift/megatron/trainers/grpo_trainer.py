753996a7f24fc416ce28b38317b6cfd781e256f9
hjh0119 <hujinghan.hjh@alibaba-inc.com>
2026-06-08 17:48:54 +0800

    fix

--- swift/megatron/trainers/grpo_trainer.py
+++ swift/megatron/trainers/grpo_trainer.py
@@ -522,11 +522,11 @@ class MegatronGRPOTrainer(MegatronRolloutMixin, MegatronRLHFTrainer):
         # add prompt ids and system prompts
         batch = self._preprocess_inputs(batch)
         # Step 1: Wake up the engine if it's sleeping (vLLM colocate mode)
-        aggressive_empty_cache()
         if self.vllm_mode == 'colocate' and self.engine.inner_model_executor.is_sleeping:
             wake_up_params = inspect.signature(self.engine.engine.wake_up).parameters
             # Load weights only (faster and reduces memory peak)
             kwargs = {'tags': ['weights']} if 'tags' in wake_up_params else {}
+            aggressive_empty_cache()
             self.engine.engine.wake_up(**kwargs)
 
         # Step 2: Load model weights
