6f7c95c72914e33a348a15d10bdd9f6e436c63f6
Yinlei Sun <ginray0215@gmail.com>
2026-06-09 16:57:40 +0800

    Add ray to fix ModuleNotFoundError

--- .dev_scripts/ci_container_test.sh
+++ .dev_scripts/ci_container_test.sh
@@ -208,6 +208,7 @@ if [ "$MODELSCOPE_SDK_DEBUG" == "True" ]; then
     pip install diffusers
     if [ "$SWIFT_CI_USE_NPU" == "True" ]; then
         pip install math-verify -i "$NPU_PIP_INDEX"
+        pip install ray -i "$NPU_PIP_INDEX"
     fi
     pip install "transformers<5.0" "peft<0.19"
     # pip install autoawq -U --no-deps
