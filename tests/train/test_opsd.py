bc4e246f8d14f183c0214b1e5f6736a4e5ba5967
huangjintao.hjt <huangjintao.hjt@alibaba-inc.com>
2026-06-15 14:16:53 +0800

    update

--- tests/train/test_opsd.py
+++ tests/train/test_opsd.py
@@ -1,7 +1,7 @@
 import os
-from swift.utils import select_device
-select_device('0,1')
+os.environ['CUDA_VISIBLE_DEVICES'] = '0,1'
 os.environ['NPROC_PER_NODE'] = '2'
+
 
 def train():
     from swift import RLHFArguments, rlhf_main
