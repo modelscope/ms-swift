7ccf05897fd37f955ad165983dae823289be6888
Jintao <huangjintao.hjt@alibaba-inc.com>
2026-06-16 15:32:46 +0800

    fix minimax_m3 (#9568)

--- swift/model/register.py
+++ swift/model/register.py
@@ -44,7 +44,7 @@ def register_model(model_meta: ModelMeta, *, exist_ok: bool = False) -> None:
 
 def load_by_unsloth(args):
     """Load model by unsloth"""
-    assert is_unsloth_available(), 'please install unsloth if using `use_unsloth=True`: `pip install unsloth`'
+    assert is_unsloth_available(), 'please install unsloth if using `--tuner_backend unsloth`: `pip install unsloth`'
     os.environ['UNSLOTH_RETURN_LOGITS'] = '1'
     os.environ['UNSLOTH_DISABLE_STATISTICS'] = '1'
     model_info = args.model_info
