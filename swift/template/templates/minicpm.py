101e3134aa481ab547fe6540c5f392b9e9ba6028
Jintao Huang <huangjintao.hjt@alibaba-inc.com>
2026-07-07 14:23:07 +0800

    update

--- swift/template/templates/minicpm.py
+++ swift/template/templates/minicpm.py
@@ -619,6 +619,8 @@ class MiniCPMV4_6Template(Template):
         self.video_max_slice_nums = get_env_args('video_max_slice_nums', int, 1)
         self.max_num_frames = get_env_args('max_num_frames', int, 128)
         self.stack_frames = get_env_args('stack_frames', int, 1)
+        self.transformers_version = version.parse(transformers.__version__)
+        self.transformers_5_9 = self.transformers_version >= version.parse('5.9.0')
 
     def _preprocess_inputs(self, inputs: StdTemplateInputs) -> None:
         super()._preprocess_inputs(inputs)
@@ -695,13 +697,12 @@ class MiniCPMV4_6Template(Template):
             res.update(get_packed_seq_params(res['position_ids']))
         return res
 
-
     def _post_encode(self, model, inputs: Dict[str, Any]) -> Dict[str, Any]:
-        transformers_version = version.parse(transformers.__version__)
-        if (self.padding_free and self.sequence_parallel_size <= 1 and transformers_version < version.parse('5.9.0')):
+        if self.padding_free and self.sequence_parallel_size <= 1 and not self.transformers_5_9:
             raise RuntimeError('MiniCPM-V 4.6 packing/padding_free with sequence_parallel_size=1 requires '
-                               f'transformers>=5.9.0 (current: {transformers_version}). ')
+                               f'transformers>=5.9.0 (current: {self.transformers_version}). ')
         return super()._post_encode(model, inputs)
+
 
 register_template(
     ChatmlTemplateMeta(
