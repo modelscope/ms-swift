1378fb59e3d132a3da5ec1344dd91afd90868fb2
burm95 <65502872+burm95@users.noreply.github.com>
2026-08-04 11:22:27 +0800

    Refactor assertion for model weight dtype check

--- tests/tuners/test_peft.py
+++ tests/tuners/test_peft.py
@@ -139,8 +139,8 @@ class TestPeft(unittest.TestCase):
         model3 = copy.deepcopy(model)
         lora_config = LoraConfig(target_modules=['query', 'key', 'value'], lora_dtype='float16')
         model = Swift.prepare_model(model, lora_config)
-        self.assertTrue(model.base_model.bert.encoder.layer[0].attention.self.key.lora_A.default.weight.dtype ==
-                        torch.float16)
+        self.assertTrue(
+            model.base_model.bert.encoder.layer[0].attention.self.key.lora_A.default.weight.dtype == torch.float16)
         model.save_pretrained(self.tmp_dir, safe_serialization=False)
         self.assertTrue(os.path.exists(os.path.join(self.tmp_dir, 'additional_config.json')))
         model2 = Swift.from_pretrained(model2, self.tmp_dir)
