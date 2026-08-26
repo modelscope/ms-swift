24f2941caa7cf57914810038952614ef4f5d7df3
Excelius <57819425+Excelius-Wang@users.noreply.github.com>
2026-08-26 16:06:31 +0800

    fix(train): fail fast on incompatible DLRover flash checkpoint APIs (#9964)
    * fix(train): validate DLRover flash checkpoint API
    
    * refactor(train): move DLRover API validator to utils

--- swift/pipelines/train/sft.py
+++ swift/pipelines/train/sft.py
@@ -26,17 +26,20 @@ class SwiftSft(SwiftPipeline, TunerMixin):
     def __init__(self, args: Optional[Union[List[str], SftArguments]] = None) -> None:
         super().__init__(args)
         self.train_msg = {}
+        self._prepare_flash_ckpt()
         self._prepare_model_tokenizer()
         self._prepare_template()
-        self._prepare_flash_ckpt()
 
     @RayHelper.function(group='default')
     def _prepare_flash_ckpt(self):
         if self.args.use_flash_ckpt:
             try:
-                import dlrover.trainer.torch.flash_checkpoint.hf_trainer
+                from dlrover.trainer.torch.flash_checkpoint.engine import CheckpointEngine
+                from dlrover.trainer.torch.flash_checkpoint.hf_trainer import HfFlashCheckpointer
             except ImportError:
-                raise ValueError('Please install dlrover to use flash ckpt `pip install dlrover[k8s,torch]')
+                raise ValueError('Please install DLRover to use Flash Checkpoint: `pip install dlrover[k8s,torch]`.')
+            from swift.trainers.utils import _validate_dlrover_flash_checkpoint_api
+            _validate_dlrover_flash_checkpoint_api(HfFlashCheckpointer, CheckpointEngine)
 
     def _prepare_generation_config(self):
         args = self.args
---
+++ tests/utils/test_flash_checkpoint_compat.py
@@ -0,0 +1,44 @@
+# Copyright (c) ModelScope Contributors. All rights reserved.
+import unittest
+
+from swift.trainers.utils import _validate_dlrover_flash_checkpoint_api
+
+
+class DLRover061Engine:
+
+    def wait_latest_checkpoint(self, timeout=None):
+        pass
+
+
+class DLRoverMasterEngine:
+
+    def wait_latest_checkpoint(self, timeout=None, max_steps=None):
+        pass
+
+
+class DLRover061Checkpointer:
+
+    def save_checkpoint_to_storage(self, step):
+        pass
+
+
+class DLRoverMasterCheckpointer:
+
+    def save_checkpoint_to_storage(self, step, blocking=False):
+        pass
+
+
+class TestFlashCheckpointCompatibility(unittest.TestCase):
+
+    def test_dlrover_061_is_rejected_before_training(self):
+        with self.assertRaisesRegex(ValueError, 'blocking.*max_steps') as error:
+            _validate_dlrover_flash_checkpoint_api(DLRover061Checkpointer, DLRover061Engine)
+        install_command = 'pip install git+https://github.com/intelligent-machine-learning/dlrover.git'
+        self.assertIn(install_command, str(error.exception))
+
+    def test_new_dlrover_api_is_accepted(self):
+        _validate_dlrover_flash_checkpoint_api(DLRoverMasterCheckpointer, DLRoverMasterEngine)
+
+
+if __name__ == '__main__':
+    unittest.main()
